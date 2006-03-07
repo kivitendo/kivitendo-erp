43a19768b1c750bdc915136bcc50f0b2d5a21d58
Stephan Köhler <s.koehler@linet-services.de>
2006-03-07 16:59:16 +0000

    Version 2.2.2 - changelog aktualisiert

--- SL/Form.pm
+++ SL/Form.pm
@@ -140,7 +140,7 @@ sub new {
   $self->{action} = lc $self->{action};
   $self->{action} =~ s/( |-|,|#)/_/g;
 
-  $self->{version}   = "2.2.1";
+  $self->{version}   = "2.2.2";
   $self->{dbversion} = "2.2.0";
 
   $main::lxdebug->leave_sub();
