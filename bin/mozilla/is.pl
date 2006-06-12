1f7101253f475d7881a37230be321fb385462824
Stephan Köhler <s.koehler@linet-services.de>
2006-06-12 13:43:42 +0000

    Nachtag zu r1152, fehlerhafte Abfrage beim Bezahlen von Rechnungen "0,00" ist nicht false

--- bin/mozilla/is.pl
+++ bin/mozilla/is.pl
@@ -1061,7 +1061,7 @@ sub post {
     if ($form->{currency} ne $form->{defaultcurrency});
 
   for $i (1 .. $form->{paidaccounts}) {
-    if ($form->{"paid_$i"}) {
+    if ($form->parse_amount(\%myconfig, $form->{"paid_$i"})) {
       $datepaid = $form->datetonum($form->{"datepaid_$i"}, \%myconfig);
 
       $form->isblank("datepaid_$i", $locale->text('Payment date missing!'));
