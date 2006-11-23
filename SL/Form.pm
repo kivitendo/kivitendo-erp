e5abe3effc8d6f5e45b75648ee10342a814f3ff2
Moritz Bunkus <m.bunkus@linet-services.de>
2006-11-23 09:26:05 +0000

    Noch haben wir keine 2.4.0.

--- SL/Form.pm
+++ SL/Form.pm
@@ -146,7 +146,7 @@ sub new {
   $self->{action} = lc $self->{action};
   $self->{action} =~ s/( |-|,|\#)/_/g;
 
-  $self->{version}   = "2.4.0";
+  $self->{version}   = "2.3.9";
 
   $main::lxdebug->leave_sub();
 
