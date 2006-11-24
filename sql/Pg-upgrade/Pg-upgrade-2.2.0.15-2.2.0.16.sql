2071984a9447c94fdf42d91f7b27e87d535a3a37
Holger Lindemann <hli@lx-system.de>
2006-11-24 16:43:33 +0000

    Spalte id in shipto_id umbenannt

--- sql/Pg-upgrade/Pg-upgrade-2.2.0.15-2.2.0.16.sql
+++ sql/Pg-upgrade/Pg-upgrade-2.2.0.15-2.2.0.16.sql
@@ -6,9 +6,9 @@ CREATE TABLE "adr" (
 
 ALTER TABLE parts add column adr_id integer;
 
-alter table shipto add column id integer;
-alter table shipto alter column id set default nextval('id'::text);
-update shipto set id=nextval('id'::text);
+alter table shipto add column shipto_id integer;
+alter table shipto alter column shipto_id set default nextval('id'::text);
+update shipto set shipto_id=nextval('id'::text);
 
 alter table oe add column shipto_id integer;
 alter table ar add column shipto_id integer;
