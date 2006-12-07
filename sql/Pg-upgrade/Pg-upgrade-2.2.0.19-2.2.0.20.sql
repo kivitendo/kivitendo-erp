2259825299133759a5ba0febf2d88bda8539de7a
Moritz Bunkus <m.bunkus@linet-services.de>
2006-12-07 10:01:43 +0000

    Datenbankupgrades: Es gibt einige Probleme, wenn die CRM installiert ist, weil dann einige Spalten, die in den SQL-Scripten angelegt werden sollten, bereits existieren. Dieses wird nun ueber Perl-Upgradescripte abgefangen.

--- sql/Pg-upgrade/Pg-upgrade-2.2.0.19-2.2.0.20.sql
+++
@@ -1,9 +0,0 @@
-alter table contacts add column cp_fax text;
-alter table contacts add column cp_mobile1 text;
-alter table contacts add column cp_mobile2 text;
-alter table contacts add column cp_satphone text;
-alter table contacts add column cp_satfax text;
-alter table contacts add column cp_project text;
-alter table contacts add column cp_privatphone text;
-alter table contacts add column cp_privatemail text;
-alter table contacts add column cp_birthday text;
--- sql/Pg-upgrade/Pg-upgrade-2.2.0.20-2.2.0.21.sql
+++
@@ -1,3 +0,0 @@
-alter table contacts add column cp_abteilung text;
-update contacts set cp_abteilung=cp_department;
-alter table contacts drop column cp_department;
