fbafe8b06c63af273368d052adeeb5af86993059
Moritz Bunkus <m.bunkus@linet-services.de>
2006-12-13 15:22:46 +0000

    Debugcode

--- sql/Pg-upgrade/Pg-upgrade-2.2.0.36-2.2.0.37.pl
+++ sql/Pg-upgrade/Pg-upgrade-2.2.0.36-2.2.0.37.pl
@@ -7,7 +7,6 @@ die("This script cannot be run from the command line.") unless ($main::form);
 sub do_update {
   my $query = "ALTER TABLE orderitems DROP CONSTRAINT orderitems_pkey";
   if (!$dbh->do($query)) {
-    $main::lxdebug->message(0, "gab es nicht");
     $dbh->rollback();
     $dbh->begin_work();
   }
