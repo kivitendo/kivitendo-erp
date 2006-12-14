c90e49a20777b5181797088d3c27b5626a3b62b4
Philip Reetz <p.reetz@linet-services.de>
2006-12-14 16:37:01 +0000

    Fehler Sortierung Buchungsjournal behoben

--- SL/GL.pm
+++ SL/GL.pm
@@ -405,7 +405,7 @@ sub all_transactions {
 		 AND ac.chart_id = c.id
 		 AND a.vendor_id = ct.id
 		 AND a.id = ac.trans_id
-	         ORDER BY acoid, $sortorder transdate, trans_id, taxkey DESC|;
+	         ORDER BY $sortorder transdate,acoid, trans_id, taxkey DESC|;
 
   # Show all $query in Debuglevel LXDebug::QUERY
   $callingdetails = (caller (0))[3];
