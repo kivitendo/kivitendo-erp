f9504bdd4a868b62b545e42dc3ca374c988dc647
Moritz Bunkus <m.bunkus@linet-services.de>
2007-01-04 10:58:01 +0000

    Primär- und Fremdschlüssel für Tabellen tax und taxkeys. Problem bei Zuordnung von taxkeys zu tax behoben.

--- sql/Pg-upgrade2/tax_id_if_taxkey_is_0.sql
+++ sql/Pg-upgrade2/tax_id_if_taxkey_is_0.sql
@@ -2,3 +2,4 @@
 -- @description: Aktualisierung der Spalte tax.id, wenn tax.taxkey = 0 ist.
 -- @depends:
 UPDATE tax SET id = 0 WHERE taxkey = 0;
+UPDATE taxkeys SET tax_id = 0 WHERE taxkey_id = 0;
---
+++ sql/Pg-upgrade2/tax_primary_key_taxkeys_foreign_keys.sql
@@ -0,0 +1,5 @@
+-- @tag: tax_primary_key_taxkeys_foreign_keys
+-- @description: Legt in tax einen neuen Prim&auml;rschl&uuml;ssel und in taxkeys einen neuen Fremdschl&uuml;ssel auf tax an.
+-- @depends: tax_id_if_taxkey_is_0
+ALTER TABLE tax ADD PRIMARY KEY (id);
+ALTER TABLE taxkeys ADD FOREIGN KEY (tax_id) REFERENCES tax (id);
