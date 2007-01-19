ce901807ad227865ee8c73ae6b4b60029f8c9b9b
Moritz Bunkus <m.bunkus@linet-services.de>
2007-01-19 10:58:28 +0000

    Datenbankupgradescript für v2.4.1, das von den anderen Scripten abhängt.

---
+++ sql/Pg-upgrade2/release_2_4_1.sql
@@ -0,0 +1,3 @@
+-- @tag: release_2_4_1
+-- @description: Leeres Script, das von allen bis zum Release 2.4.1 hinzugef&uuml;gten Upgradescripten abh&auml;ngt, um ein fest definiertes Schema f&uuml;r 2.4.1 zu definieren.
+-- @depends: chart_names rename_buchungsgruppen_accounts_16_19_percent language_output_formatting units_translations_and_singular_plural_distinction invalid_taxkesy customer_vendor_ustid_length
