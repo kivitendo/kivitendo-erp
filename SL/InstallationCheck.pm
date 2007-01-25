62a8aa81ec93edf284b3ffcc065120b3fe303fb5
Moritz Bunkus <m.bunkus@linet-services.de>
2007-01-25 16:32:28 +0000

    Installations-Check-Modul. Das Script installation_check.pl nutzt es.

---
+++ SL/InstallationCheck.pm
@@ -0,0 +1,36 @@
+package SL::InstallationCheck;
+
+use vars qw(@required_modules);
+
+@required_modules = (
+  { "name" => "Class::Accessor", "url" => "http://search.cpan.org/~kasei/" },
+  { "name" => "CGI", "url" => "http://search.cpan.org/~lds/" },
+  { "name" => "CGI::Ajax", "url" => "http://search.cpan.org/~bct/" },
+  { "name" => "DBI", "url" => "http://search.cpan.org/~timb/" },
+  { "name" => "DBD::Pg", "url" => "http://search.cpan.org/~dbdpg/" },
+  { "name" => "HTML::Template", "url" => "http://search.cpan.org/~samtregar/" },
+  { "name" => "Archive::Zip", "url" => "http://search.cpan.org/~adamk/" },
+  { "name" => "Text::Iconv", "url" => "http://search.cpan.org/~mpiotr/" },
+  { "name" => "Klaus", "url" => "http://dum.my/" },
+  );
+
+sub module_available {
+  my ($module) = @_;
+
+  if (!defined(eval("require $module;"))) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+sub test_all_modules {
+  my @missing_modules;
+
+  map({ push(@missing_modules, $_) unless (module_available($_)); }
+      @required_modules);
+
+  return @missing_modules;
+}
+
+1;
--- scripts/installation_check.pl
+++ scripts/installation_check.pl
@@ -1,35 +1,16 @@
 #!/usr/bin/perl -w
 
+use SL::InstallationCheck;
+
 $| = 1;
 
-my @required_modules = (
-  { "name" => "Class::Accessor", "url" => "http://search.cpan.org/~kasei/" },
-  { "name" => "CGI", "url" => "http://search.cpan.org/~lds/" },
-  { "name" => "CGI::Ajax", "url" => "http://search.cpan.org/~bct/" },
-  { "name" => "DBI", "url" => "http://search.cpan.org/~timb/" },
-  { "name" => "DBD::Pg", "url" => "http://search.cpan.org/~dbdpg/" },
-  { "name" => "HTML::Template", "url" => "http://search.cpan.org/~samtregar/" },
-  { "name" => "Archive::Zip", "url" => "http://search.cpan.org/~adamk/" },
-  { "name" => "Text::Iconv", "url" => "http://search.cpan.org/~mpiotr/" },
-  );
-
-sub module_available {
-  my ($module) = @_;
-
-  if (!defined(eval("require $module;"))) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-
-foreach my $module (@required_modules) {
+foreach my $module (@SL::InstallationCheck::required_modules) {
   print("Looking for $module->{name}...");
-  if (!module_available($module->{"name"})) {
+  if (!SL::InstallationCheck::module_available($module->{"name"})) {
     print(" NOT found\n" .
           "  The module '$module->{name}' is not available on your system.\n" .
           "  Please install it with the CPAN shell, e.g.\n" .
-          "    perl -MCPAN -e install \"install $module->{name}\"\n" .
+          "    perl -MCPAN -e \"install $module->{name}\"\n" .
           "  or download it from this URL and install it manually:\n" .
           "    $module->{url}\n\n");
   } else {
