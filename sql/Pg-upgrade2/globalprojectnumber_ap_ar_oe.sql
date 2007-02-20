1e2a4a3836d317a33166b34f514ca6863e4824c1
Moritz Bunkus <m.bunkus@linet-services.de>
2007-02-20 16:14:56 +0000

    Anscheinend gab es irgendwo im Upgrade-Pfad Unstimmigkeiten, sodass nicht alle Datenbanken bei project einen Primärschlüssel auf id haben, auch wenn dieser in lx-office.sql eingetragen ist. Also project mit einem Primärschlüssel versehen, sofern keiner existiert, bevor mit Fremdschlüsseln darauf verwiesen wird.

--- sql/Pg-upgrade2/globalprojectnumber_ap_ar_oe.sql
+++
@@ -1,9 +0,0 @@
--- @tag: globalprojectnumber_ap_ar_oe
--- @description: Neue Spalte f&uuml;r eine globale Projektnummer in Einkaufs- und Verkaufsbelegen
--- @depends: release_2_4_1
-ALTER TABLE ap ADD COLUMN globalproject_id integer;
-ALTER TABLE ap ADD FOREIGN KEY (globalproject_id) REFERENCES project (id);
-ALTER TABLE ar ADD COLUMN globalproject_id integer;
-ALTER TABLE ar ADD FOREIGN KEY (globalproject_id) REFERENCES project (id);
-ALTER TABLE oe ADD COLUMN globalproject_id integer;
-ALTER TABLE oe ADD FOREIGN KEY (globalproject_id) REFERENCES project (id);
