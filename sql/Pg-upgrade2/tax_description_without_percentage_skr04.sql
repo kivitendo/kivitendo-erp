80d94bafcfb3304ab0adadb8df8bd4d9d37badeb
Moritz Bunkus <m.bunkus@linet-services.de>
2007-03-21 09:13:24 +0000

    Beim Kontenrahmen SKR04 den Steuersatz aus der Steuerschlüsselbeschreibung herausnehmen, weil der vom Programm inzwischen zusätzlich angezeigt wird.

---
+++ sql/Pg-upgrade2/tax_description_without_percentage_skr04.sql
@@ -0,0 +1,122 @@
+-- @tag: tax_description_without_percentage_skr04
+-- @description: SKR04: Die Prozentangaben aus der tax.taxdescription entfernen. (Unter Ber&uuml;cksichtigung der Druckausgabe.)
+-- @depends: ustva_setup_2007
+
+--#############################################################
+--#
+--# Taxdescription setzen
+--#
+--#############################################################
+
+UPDATE tax SET
+  taxdescription = 'USt-frei'
+WHERE taxkey = '1'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Umsatzsteuer'
+WHERE taxkey = '2'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Umsatzsteuer'
+WHERE taxkey = '3'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+
+UPDATE tax SET
+  taxdescription = 'Vorsteuer'
+WHERE taxkey = '8'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Vorsteuer'
+WHERE taxkey = '9'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Im anderen EU-Staat steuerpflichtige Lieferung'
+WHERE taxkey = '10'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Steuerfreie innergem. Lieferung an Abnehmer mit Id.-Nr.'
+WHERE taxkey = '11'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Steuerpflichtige EG-Lieferung zum erm‰ﬂigten Steuersatz'
+WHERE taxkey = '12'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+
+UPDATE tax SET
+  taxdescription = 'Steuerpflichtige EG-Lieferung zum vollen Steuersatz'
+WHERE taxkey = '13'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+
+UPDATE tax SET
+  taxdescription = 'Steuerpflichtiger innergem. Erwerb zum erm‰ﬂigten Steuersatz'
+WHERE taxkey = '18'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
+
+UPDATE tax SET
+  taxdescription = 'Steuerpflichtiger innergem. Erwerb zum vollen Steuersatz'
+WHERE taxkey = '19'
+  AND
+  EXISTS ( -- update only for SKR04
+    SELECT coa FROM defaults
+    WHERE defaults.coa='Germany-DATEV-SKR04EU'
+  )
+;
