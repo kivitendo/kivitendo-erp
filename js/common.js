5def52b0f6322414eb2a8f554b85e1320d230b2c
Thomas Kasulke <t.kasulke@linet-services.de>
2007-04-16 12:19:25 +0000

    Erweiterung der common.js um die function setupDateFormat(dateFormat, wrongDateFormat) und check_right_date_format(input_name), welche ein Datum auf Richtigkeit überprüfen. Einzubingen wie folgt:
    - in jeder pl, in der input-tags überprüft werden sollen, muss $onload durch folgendes erweitert werden:
      $onload .= qq|;setupDateFormat('|. $myconfig{dateformat} .qq|', '|. $locale->text("Falsches Datumsformat!") .qq|')|;
    - die common.js muss eingebunden sein
    - in zu überprüfendem input-tag muß folgendes stehen:
       onBlur=\"check_right_date_format(this)\" 
    
    
    Die Funktion check_right_date_format, welche vorher vorhanden war, wurde in check_right_number_format(input_name) umbenannt!!!

--- js/common.js
+++ js/common.js
@@ -7,7 +7,19 @@ function setupPoints(numberformat, wrongFormat) {
   else {
     thpoint = null;
   }
-  wrongformat = wrongFormat;  
+  wrongNumberformat = wrongFormat + " ( " + numberformat + " ) ";  
+}
+
+function setupDateFormat(setDateFormat, setWrongDateFormat) {
+  dateFormat = setDateFormat;
+  wrongDateFormat = setWrongDateFormat + " ( " + setDateFormat + " ) ";
+  formatArray = new Array();
+  if(dateFormat.match(/^\w\w\W/)) {
+    seperator = dateFormat.substring(2,3);
+  }
+  else {
+    seperator = dateFormat.substring(4,5);
+  }
 }
 
 function centerParms(width,height,extra) {
@@ -35,42 +47,84 @@ function set_longdescription_window(input_name) {
   window.open(url, "_new_generic", parm);
   }
 
-  function check_right_date_format(input_name) {
-    var decnumbers = input_name.value.split(decpoint);
-    if(thpoint) {
-      var thnumbers = input_name.value.split(thpoint);
-      if(thnumbers[thnumbers.length-1].match(/.+decpoint$/g)) {
-        thnumbers[thnumbers.length-1] = thnumbers[thnumbers.length-1].substring(thnumbers[thnumbers.length-1].length-1);
+function check_right_number_format(input_name) {
+  var decnumbers = input_name.value.split(decpoint);
+  if(thpoint) {
+    var thnumbers = input_name.value.split(thpoint);
+    if(thnumbers[thnumbers.length-1].match(/.+decpoint$/g)) {
+      thnumbers[thnumbers.length-1] = thnumbers[thnumbers.length-1].substring(thnumbers[thnumbers.length-1].length-1);
+    }
+    if(thnumbers[thnumbers.length-1].match(/.+decpoint\d$/g)) {
+      thnumbers[thnumbers.length-1] = thnumbers[thnumbers.length-1].substring(thnumbers[thnumbers.length-1].length-2);
+    }  
+    if(thnumbers[thnumbers.length-1].match(/.+decpoint\d\d$/g)) {
+      thnumbers[thnumbers.length-1] = thnumbers[thnumbers.length-1].substring(thnumbers[thnumbers.length-1].length-3);
+    }  
+    for(var i = 1; i < thnumbers.length; i++) {
+      if(!thnumbers[i].match(/\d\d\d/g)) {
+        return show_alert_and_focus(input_name, wrongNumberFormat);
       }
-      if(thnumbers[thnumbers.length-1].match(/.+decpoint\d$/g)) {
-        thnumbers[thnumbers.length-1] = thnumbers[thnumbers.length-1].substring(thnumbers[thnumbers.length-1].length-2);
-      }  
-      if(thnumbers[thnumbers.length-1].match(/.+decpoint\d\d$/g)) {
-        thnumbers[thnumbers.length-1] = thnumbers[thnumbers.length-1].substring(thnumbers[thnumbers.length-1].length-3);
-      }  
-      for(var i = 1; i < thnumbers.length; i++) {
-        if(!thnumbers[i].match(/\d\d\d/g)) {
-          return show_alert_and_focus(input_name);
-        }
-        if(thnumbers[i].match(/.*decpoint.*|.*thpoint.*/g)) {
-          return show_alert_and_focus(input_name);
-        }
-      }
-      if(decnumbers.length > 2 || (decnumbers.length > 1 ? (decnumbers[1].length > 2) : false)) {
-        return show_alert_and_focus(input_name);
+      if(thnumbers[i].match(/.*decpoint.*|.*thpoint.*/g)) {
+        return show_alert_and_focus(input_name, wrongNumberFormat);
       }
     }
-    else {
-      if(decnumbers.length > 1 || decnumbers[0].length > 2) {
-        return show_alert_and_focus(input_name);
-      }
+    if(decnumbers.length > 2 || (decnumbers.length > 1 ? (decnumbers[1].length > 2) : false)) {
+      return show_alert_and_focus(input_name, wrongNumberFormat);
     }
   }
-  
-  function show_alert_and_focus(input_name) {
-    input_name.select();
-    alert(wrongformat + "\n\r\n\r--> " + input_name.value);
-    input_name.focus();
-    return false;
+  else {
+    if(decnumbers.length > 1 || decnumbers[0].length > 2) {
+      return show_alert_and_focus(input_name, wrongNumberFormat);
+    }
   }
+}
+
+function check_right_date_format(input_name) {
+  var matching = new RegExp(dateFormat.replace(/\w/g, '\\d') + "\$","ig");
+  if(!(dateFormat.lastIndexOf("y") == 3) && !matching.test(input_name.value)) {
+    matching = new RegExp(dateFormat.replace(/\w/g, '\\d') + '\\d\\d\$', "ig");
+    if(!matching.test(input_name.value)) {
+      return show_alert_and_focus(input_name, wrongDateFormat);
+    }
+  }
+  else {
+    if (dateFormat.lastIndexOf("y") == 3 && !matching.test(input_name.value)) { 
+      return show_alert_and_focus(input_name, wrongDateFormat);
+    }
+  }
+}
+
+function validate_dates(input_name_1, input_name_2) {
+  var tempArray1 = new Array();
+  var tempArray2 = new Array();
+  tempArray1 = getDateArray(input_name_1);
+  tempArray2 = getDateArray(input_name_2);
+  if(check_right_date_format(input_name_1) && check_right_date_format(input_name_2)) {
+    if(!((new Date(tempArray2[0], tempArray2[1], tempArray2[2])).getTime() >= (new Date(tempArray1[0], tempArray1[1], tempArray1[2])).getTime())) {
+      show_alert_and_focus(input_name_1, wrongDateFormat);
+      return show_alert_and_focus(input_name_2, wrongDateFormat);
+    }
+    if(!((new Date(tempArray2[0], tempArray2[1], tempArray2[2])).getTime() >= (new Date(1900, 1, 1)).getTime())) {
+      show_alert_and_focus(input_name_1, wrongDateFormat);
+      return show_alert_and_focus(input_name_2, wrongDateFormat);
+    }
+  }
+}
+
+function getDateArray(input_name) {
+  formatArray[2] = input_name.value.substring(dateFormat.indexOf("d"), 2);
+  formatArray[1] = input_name.value.substring(dateFormat.indexOf("m"), 2);
+  formatArray[0] = input_name.value.substring(dateFormat.indexOf("y"), (dateFormat.length == 10 ? 4 : 2));
+  if(dateFormat.length == 8) {
+    formatArray[0] += (formatArray[0] < 70 ? 2000 : 1900);
+  }
+  return formatArray;
+}
+
+function show_alert_and_focus(input_name, errorMessage) {
+  input_name.select();
+  alert(errorMessage + "\n\r\n\r--> " + input_name.value);
+  input_name.focus();
+  return false;
+}
   
