c03fba33998b3816e329f5e6c3cb268c0329e451
Moritz Bunkus <m.bunkus@linet-services.de>
2007-05-30 09:58:11 +0000

    Minimale Erweiterung um PlainTextTemplate, um Variablen in Strings leicht ersetzen zu können.

--- SL/Template.pm
+++ SL/Template.pm
@@ -617,6 +617,37 @@ sub convert_to_pdf {
 
 
 ####
+#### PlainTextTemplate
+####
+
+package PlainTextTemplate;
+
+use vars qw(@ISA);
+
+@ISA = qw(LaTeXTemplate);
+
+sub new {
+  my $type = shift;
+
+  return $type->SUPER::new(@_);
+}
+
+sub format_string {
+  my ($self, $variable) = @_;
+
+  return $variable;
+}
+
+sub get_mime_type {
+  return "text/plain";
+}
+
+sub parse {
+}
+
+1;
+
+####
 #### OpenDocumentTemplate
 ####
 
