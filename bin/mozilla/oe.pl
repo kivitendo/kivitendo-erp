cae6316e718234083b892b042e61714ceb13b0ca
Philip Reetz <p.reetz@linet-services.de>
2007-06-13 18:07:25 +0000

    bei oe suche wurde der Mitarbeiter nicht im callback mitgefuehrt

--- bin/mozilla/oe.pl
+++ bin/mozilla/oe.pl
@@ -1647,7 +1647,7 @@ sub orders {
 
   my @hidden_variables = map { "l_${_}" } @columns;
   push @hidden_variables, "l_subtotal", $form->{vc}, qw(l_closed l_notdelivered open closed delivered notdelivered ordnumber quonumber
-                                                        transaction_description transdatefrom transdateto type vc);
+                                                        transaction_description transdatefrom transdateto type vc employee_id);
 
   my $href = build_std_url('action=orders', grep { $form->{$_} } @hidden_variables);
 
