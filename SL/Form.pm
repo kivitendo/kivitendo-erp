b54e817db78d0f178ea1a339a5fdd1e4281db421
Moritz Bunkus <m.bunkus@linet-services.de>
2007-06-21 06:18:56 +0000

    Wenn ein Hash namens %main::debug_options existiert, dann werden all seine Variablen 'key' in HTML-Vorlagen als DEBUG_KEY zur Verfügung gestellt. Wird bisher nur bei Mahnungsvorlagen benutzt. Und ist nur für Entwickler gedacht.

--- SL/Form.pm
+++ SL/Form.pm
@@ -532,6 +532,10 @@ sub _prepare_html_template {
   $additional_params->{"conf_latex_templates"}        = $main::latex;
   $additional_params->{"conf_opendocument_templates"} = $main::opendocument_templates;
 
+  if (%main::debug_options) {
+    map { $additional_params->{'DEBUG_' . uc($_)} = $main::debug_options{$_} } keys %main::debug_options;
+  }
+
   $main::lxdebug->leave_sub();
 
   return $file;
