2ef21b8f2aaae439a9408977458bdeb9b0fd9e25
Sven Schöling <s.schoeling@linet-services.de>
2007-10-22 15:35:14 +0000

    ...und das gemoddete Templating

---
+++ SL/Template/Plugin/LxERP.pm
@@ -0,0 +1,27 @@
+package SL::Template::Plugin::LxERP;
+
+use base qw( Template::Plugin );
+use Template::Plugin;
+
+sub new {
+  my $class   = shift;
+  my $context = shift;
+
+  bless { }, $class;
+}
+
+sub format_amount {
+  my ($self, $var, $places, $skip_zero) = @_;
+
+  return $main::form->format_amount(\%main::myconfig, $var * 1, $places) unless $skip_zero && $var == 0;
+  return '';
+}
+
+sub format_percent {
+  my ($self, $var, $places, $skip_zero) = @_;
+
+  return $self->format_amount($var * 100, $places, $skip_zero);
+}
+
+1;
+
