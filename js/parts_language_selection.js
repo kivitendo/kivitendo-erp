7a4c484b1e36bb461f06b6e1db1eb5e2ebb46cb6
Moritz Bunkus <m.bunkus@linet-services.de>
2007-11-01 10:23:05 +0000

    Teil des Bugfixes, dass Spracheinstellungen nicht erneut im Popup angezeigt wurden.

--- js/parts_language_selection.js
+++ js/parts_language_selection.js
@@ -3,11 +3,11 @@ function parts_language_selection_window(input_name) {
   var name = document.getElementsByName(input_name)[0].value;
   url = "ic.pl?" +
     "action=parts_language_selection&" +
-    "login=" + escape(document.ic.login.value) + "&" +
-    "password=" + escape(document.ic.password.value) + "&" +
-    "id=" + escape(document.ic.id.value) + "&" +
-    "language_values=" + escape(document.ic.language_values.value) + "&" +
-    "name=" + escape(name) + "&" +
-    "input_name=" + escape(input_name) + "&"
+    "login="           + escape_more(document.ic.login.value)           + "&" +
+    "password="        + escape_more(document.ic.password.value)        + "&" +
+    "id="              + escape_more(document.ic.id.value)              + "&" +
+    "language_values=" + escape_more(document.ic.language_values.value) + "&" +
+    "name="            + escape_more(name)                              + "&" +
+    "input_name="      + escape_more(input_name)                        + "&"
   window.open(url, "_new_generic", parm);
 }
