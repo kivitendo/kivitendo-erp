1296b8698b6f6ac80bcb63b0f94dd56e07046e9c
Sven Schöling <s.schoeling@linet-services.de>
2008-02-04 15:46:04 +0000

    Lx-Office Header angefuegt

--- bin/mozilla/admin_groups.pl
+++ bin/mozilla/admin_groups.pl
@@ -1,3 +1,37 @@
+#=====================================================================
+# LX-Office ERP
+# Copyright (C) 2004
+# Based on SQL-Ledger Version 2.1.9
+# Web http://www.lx-office.org
+#
+#=====================================================================
+# SQL-Ledger Accounting
+# Copyright (c) 2002
+#
+#  Author: Moritz Bunkus
+#   Email: mbunkus@linet-services.de
+#     Web: www.linet-services.de
+#
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#======================================================================
+#
+# group administration module
+# add/edit/delete user groups
+#
+#======================================================================
+
 sub edit_groups {
   $lxdebug->enter_sub();
 
