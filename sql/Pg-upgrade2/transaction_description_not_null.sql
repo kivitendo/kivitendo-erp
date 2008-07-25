fbb89b3bd85135f9c95369b3995665607116d2d0
Moritz Bunkus <m.bunkus@linet-services.de>
2008-07-25 16:23:15 +0000

    Das Feld "transaction_description" gibt es auch in der Tabelle "delivery_orders".

--- sql/Pg-upgrade2/transaction_description_not_null.sql
+++ sql/Pg-upgrade2/transaction_description_not_null.sql
@@ -1,6 +1,7 @@
 -- @tag: transaction_description_not_null
 -- @description: Das Feld "transaction_description" sollte nicht NULL-Werte enthalten.
--- @depends: transaction_description
+-- @depends: transaction_description delivery_orders
 UPDATE ap SET transaction_description = '' WHERE transaction_description IS NULL;
 UPDATE ar SET transaction_description = '' WHERE transaction_description IS NULL;
 UPDATE oe SET transaction_description = '' WHERE transaction_description IS NULL;
+UPDATE delivery_orders SET transaction_description = '' WHERE transaction_description IS NULL;
