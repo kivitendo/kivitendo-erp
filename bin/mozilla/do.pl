5b82e66ae01355da0c1d1925dc91867b619b204d
Moritz Bunkus <m.bunkus@linet-services.de>
2008-09-09 08:19:35 +0000

    Doppeltes Quoting von Anführungszeichen vermeiden.

--- bin/mozilla/do.pl
+++ bin/mozilla/do.pl
@@ -222,8 +222,6 @@ sub prepare_order {
     (my $dec_qty) = ($form->{"qty_$i"} =~ /\.(\d+)/);
     $dec_qty = length $dec_qty;
     $form->{"qty_$i"} = $form->format_amount(\%myconfig, $form->{"qty_$i"}, $dec_qty);
-
-    map { $form->{"${_}_$i"} =~ s/\"/&quot;/g } qw(partnumber description unit);
   }
 
   $lxdebug->leave_sub();
