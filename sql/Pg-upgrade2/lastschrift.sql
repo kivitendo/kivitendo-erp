c0591c923d1a5f86eca1db2bb4cb40242a1b8445
Moritz Bunkus <m.bunkus@linet-services.de>
2008-12-05 15:50:31 +0000

    PostgreSQL-Versionen vor 8.0 erlauben nicht, in einem Befehl neue Spalten hinzuzufügen und gleichzeitig den Defaultwert zu setzen.

--- sql/Pg-upgrade2/lastschrift.sql
+++ sql/Pg-upgrade2/lastschrift.sql
@@ -1,5 +1,7 @@
 -- @tag: direct_debit 
 -- @description: Schalter fuer Lastschrift
 -- @depends: release_2_4_3
-ALTER TABLE customer ADD COLUMN direct_debit boolean DEFAULT false;
-ALTER TABLE vendor ADD COLUMN  direct_debit boolean DEFAULT false;
+ALTER TABLE customer ADD COLUMN direct_debit boolean;
+ALTER TABLE customer ALTER direct_debit SET DEFAULT false;
+ALTER TABLE vendor ADD COLUMN  direct_debit boolean;
+ALTER TABLE vendor ALTER direct_debit SET DEFAULT false;
