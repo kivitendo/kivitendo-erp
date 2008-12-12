2654e8ee56337d0e140dcf8bfb314ecf1ae655b0
Moritz Bunkus <m.bunkus@linet-services.de>
2008-12-12 12:32:41 +0000

    Stornierte Rechnung und Stornorechnung falsch herum markiert in der Typ-Spalte.

--- bin/mozilla/ar.pl
+++ bin/mozilla/ar.pl
@@ -1583,8 +1583,8 @@ sub ar_transactions {
 
     map { $ar->{$_} = $form->format_amount(\%myconfig, $ar->{$_}, 2) } qw(netamount tax amount paid due marge_total marge_percent);
 
-    my $is_storno  = $ar->{storno} && !$ar->{storno_id};
-    my $has_storno = $ar->{storno} && $ar->{storno_id};
+    my $is_storno  = $ar->{storno} &&  $ar->{storno_id};
+    my $has_storno = $ar->{storno} && !$ar->{storno_id};
 
     $ar->{type} =
       $has_storno       ? $locale->text("Invoice with Storno (abbreviation)") :
