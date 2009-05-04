ecf8fe1e933fbb3ce5c9fc82259d913a9b941e1d
Moritz Bunkus <m.bunkus@linet-services.de>
2009-05-04 11:25:59 +0000

    Das Modul SL/MIME.pm zum Commit Revision 3987.

---
+++ SL/MIME.pm
@@ -0,0 +1,25 @@
+package SL::MIME;
+
+sub mime_type_from_ext {
+  $main::lxdebug->enter_sub();
+
+  my $self = shift;
+  my $ext  = shift;
+
+  # TODO: Mittels Standardmodulen implementieren.
+  my %mime_types = ('ods'  => 'application/vnd.oasis.opendocument.spreadsheet',
+                    'odt'  => 'application/vnd.oasis.opendocument.text',
+                    'pdf'  => 'application/pdf',
+                    'sql'  => 'text/plain',
+                    'txt'  => 'text/plain',
+                    'html' => 'text/html',
+    );
+
+  $ext =~ s/.*\.//;
+
+  $main::lxdebug->leave_sub();
+
+  return $mime_types{$ext};
+}
+
+1;
