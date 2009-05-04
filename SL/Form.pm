fdae3f7b3e98eb7f80217b1a2b27f9c9b6752fd1
Moritz Bunkus <m.bunkus@linet-services.de>
2009-05-04 11:13:47 +0000

    Kosmetik.

--- SL/Form.pm
+++ SL/Form.pm
@@ -1232,10 +1232,8 @@ sub parse_template {
       } else {
 
         if (!$self->{"do_not_attach"}) {
-          @{ $mail->{attachments} } =
-            ({ "filename" => $self->{"tmpfile"},
-               "name" => $self->{"attachment_filename"} ?
-                 $self->{"attachment_filename"} : $self->{"tmpfile"} });
+          $mail->{attachments} = [{ "filename" => $self->{"tmpfile"},
+                                    "name"     => $self->{"attachment_filename"} ? $self->{"attachment_filename"} : $self->{"tmpfile"} }];
         }
 
         $mail->{message}  =~ s/\r//g;
