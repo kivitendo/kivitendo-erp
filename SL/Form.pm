7dd95f35ee3adf728460ea79d940cf6ae556d3b8
Moritz Bunkus <m.bunkus@linet-services.de>
2009-05-06 13:27:14 +0000

    Die Historiensuchmaske nicht mehr per JavaScript abschicken und dem <form>-Element den Action-Parameter mitgeben. Damit funktioniert nun auch das Abschicken per Enter-Taste, und der Request wird vom Browser nicht mehr zwei mal geschickt.

--- SL/Form.pm
+++ SL/Form.pm
@@ -3168,9 +3168,8 @@ sub get_history {
       qq|SELECT h.employee_id, h.itime::timestamp(0) AS itime, h.addition, h.what_done, emp.name, h.snumbers, h.trans_id AS id | .
       qq|FROM history_erp h | .
       qq|LEFT JOIN employee emp ON (emp.id = h.employee_id) | .
-      qq|WHERE trans_id = | . $trans_id
-      . $restriction . qq| |
-      . $order;
+      qq|WHERE (trans_id = | . $trans_id . qq|) $restriction | .
+      $order;
 
     my $sth = $dbh->prepare($query) || $self->dberror($query);
 
