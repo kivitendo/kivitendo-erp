74076701ab6811c604d95dff9fabfe351b839ebe
Moritz Bunkus <m.bunkus@linet-services.de>
2009-05-06 12:47:51 +0000

    Felder umbenannt.

--- js/show_am_history.js
+++ js/show_am_history.js
@@ -3,8 +3,8 @@ function set_history_uri() {
     "action=show_am_history&" +
     "longdescription=" + "&" +
     "einschraenkungen=" + document.getElementById("einschraenkungen").value + "&" +
-    "transdate=" + document.getElementById("transdate_hidden").value + "&" +
-    "regdate=" + document.getElementById("reqdate_hidden").value + "&" +
+    "fromdate=" + document.getElementById("fromdate_hidden").value + "&" +
+    "todate=" + document.getElementById("todate_hidden").value + "&" +
     "what2search=" + document.getElementById("what2search").value + "&" +
     "searchid=" + document.getElementById("searchid").value + "&" +
     "mitarbeiter=" + document.getElementById("mitarbeiter_hidden").value + "&";
