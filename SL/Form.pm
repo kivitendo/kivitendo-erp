9fb1f03b951e6e79019069f209fe7bbf331b5162
Sven Schöling <s.schoeling@linet-services.de>
2009-05-25 12:51:02 +0000

    Versionspdate auf 2.6.0 RC 1

--- SL/Form.pm
+++ SL/Form.pm
@@ -263,7 +263,7 @@ sub new {
   $self->{action}  =  lc $self->{action};
   $self->{action}  =~ s/( |-|,|\#)/_/g;
 
-  $self->{version} =  "2.6.0 beta 2";
+  $self->{version} =  "2.6.0 RC 1";
 
   $main::lxdebug->leave_sub();
 
