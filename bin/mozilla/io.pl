159857657639b948231ecebf44244274c28d4aa5
Geoffrey Richardson <information@richardson-bueren.de>
2009-05-29 15:27:06 +0000

    Doppelten Eintrag von credit_note bei Druckvorlagen entfernt

--- bin/mozilla/io.pl
+++ bin/mozilla/io.pl
@@ -1075,9 +1075,7 @@ sub print_options {
     ($form->{type} =~ /_delivery_order$/) ? (
       opthash($form->{type},         $form->{PD}{$form->{type}},       $locale->text('Delivery Order')),
       opthash('pick_list',           $form->{PD}{pick_list},           $locale->text('Pick List')),
-    ) : undef,
-    ($form->{type} eq 'credit_note') ?
-      opthash("credit_note",         $form->{PD}{credit_note},         $locale->text('Credit Note')) : undef;
+    ) : undef;
 
   push @SENDMODE,
     opthash("attachment",            $form->{SM}{attachment},          $locale->text('Attachment')),
