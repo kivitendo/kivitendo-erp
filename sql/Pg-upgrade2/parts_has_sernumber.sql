22a63412fb88b026d8ff6e6989416d030cb62652
Holger Lindemann <hli@lx-system.de>
2009-09-29 13:11:03 +0200

    Fehler beseitigt

--- sql/Pg-upgrade2/parts_has_sernumber.sql
+++ sql/Pg-upgrade2/parts_has_sernumber.sql
@@ -1,6 +1,5 @@
 -- @tag: has_sernumber
 -- @description: Artikel hat eine Seriennummer 
--- @depends: parts
-has_sernumber      | boolean                     | default false
+-- @depends: release_2_6_0
 ALTER TABLE parts ADD COLUMN has_sernumber boolean;
 ALTER TABLE parts ALTER COLUMN has_sernumber  SET DEFAULT false;
