ca84e219598d8c17b296112dad10baedbc9c2323
Sven Schöling <s.schoeling@linet-services.de>
2009-10-23 17:30:37 +0200

    am strict

--- am.pl
+++ am.pl
@@ -30,7 +30,7 @@
 #
 #######################################################################
 
-#use strict;
+use strict;
 
 BEGIN {
   unshift @INC, "modules/override"; # Use our own versions of various modules (e.g. YAML).
