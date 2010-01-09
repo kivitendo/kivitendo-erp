07036bf10657855cc7b59f7c978bb6ab0d2d065a
Moritz Bunkus <moritz@bunkus.org>
2010-01-09 11:49:13 +0100

    Cookie-Base-Path richtig berechnen.
    Commit 0c50616fb43bcf934101fadd488278c40317b586 hat den
    Cookie-Gültigkeits-Pfad leider inklusive des Scriptnamens gesetzt,
    wodurch nur noch Zugriff auf login.pl möglich war.

--- SL/Form.pm
+++ SL/Form.pm
@@ -566,12 +566,17 @@ sub create_http_response {
 
   my $session_cookie;
   if (defined $main::auth) {
+    my $uri      = $self->_get_request_uri;
+    my @segments = $uri->path_segments;
+    pop @segments;
+    $uri->path_segments(@segments);
+
     my $session_cookie_value   = $main::auth->get_session_id();
     $session_cookie_value    ||= 'NO_SESSION';
 
     $session_cookie = $cgi->cookie('-name'   => $main::auth->get_session_cookie_name(),
                                    '-value'  => $session_cookie_value,
-                                   '-path'   => $self->_get_request_uri->path,
+                                   '-path'   => $uri->path,
                                    '-secure' => $ENV{HTTPS});
   }
 
