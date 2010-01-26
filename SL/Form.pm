5e6ed189aa20dadbb755b7ce96917a0014aec105
Sven Schöling <s.schoeling@linet-services.de>
2010-01-26 16:19:09 +0100

    javascript im header richtig setzen

--- SL/Form.pm
+++ SL/Form.pm
@@ -636,7 +636,7 @@ sub header {
 
       $jsscript = qq|
         <script type="text/javascript" src="js/jquery.js"></script>
-        <script type='text/javascript' src='js/jquery.autocomplete.js'>
+        <script type='text/javascript' src='js/jquery.autocomplete.js'></script>
         <script type="text/javascript" src="js/common.js"></script>
         <style type="text/css">\@import url(js/jscalendar/calendar-win2k-1.css);</style>
         <script type="text/javascript" src="js/jscalendar/calendar.js"></script>
