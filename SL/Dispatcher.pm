0ea2ddad65a7079dbc914a01f07f49b5cd44ec14
Sven Schöling <s.schoeling@linet-services.de>
2010-01-29 18:41:29 +0100

    Wo use Form steht muss auch der @INC overload drin sein.

--- SL/Dispatcher.pm
+++ SL/Dispatcher.pm
@@ -2,6 +2,12 @@ package SL::Dispatcher;
 
 use strict;
 
+BEGIN {
+  unshift @INC, "modules/override"; # Use our own versions of various modules (e.g. YAML).
+  push    @INC, "modules/fallback"; # Only use our own versions of modules if there's no system version.
+  push    @INC, "SL";               # FCGI won't find modules that are not properly named. Help it by inclduging SL
+}
+
 use CGI qw( -no_xhtml);
 use English qw(-no_match_vars);
 use SL::Auth;
