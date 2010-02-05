8e009de46807bec412ef3c3d449648b18c4ad01a
Sven Schöling <s.schoeling@linet-services.de>
2010-02-05 11:04:01 +0100

    Include Modifikationen sind hier nicht nötig.

--- admin.pl
+++ admin.pl
@@ -2,12 +2,6 @@
 
 use strict;
 
-BEGIN {
-  unshift @INC, "modules/override"; # Use our own versions of various modules (e.g. YAML).
-  push    @INC, "modules/fallback"; # Only use our own versions of modules if there's no system version.
-  push    @INC, "SL";               # FCGI won't find modules that are not properly named. Help it by inclduging SL
-}
-
 use SL::Dispatcher;
 
 SL::Dispatcher::pre_startup();
