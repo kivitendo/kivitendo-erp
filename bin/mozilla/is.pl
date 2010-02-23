bf68e6b00bc557c8f04d914021de9f85f374ef34
Sven Schöling <s.schoeling@linet-services.de>
2010-02-23 13:00:55 +0100

    once warning fixen.

--- bin/mozilla/is.pl
+++ bin/mozilla/is.pl
@@ -333,7 +333,7 @@ sub form_header {
   my %labels = map { $_ => $_ } @{ $form->{ALL_CURRENCIES} };
   $form->{currency}            = $form->{defaultcurrency} unless $form->{currency};
   $TMPL_VAR{show_exchangerate} = $form->{currency} ne $form->{defaultcurrency};
-  $TMPL_VAR{currencies}        = NTI($cgi->popup_menu('-name' => 'currency', '-default' => $form->{"currency"},
+  $TMPL_VAR{currencies}        = NTI($::cgi->popup_menu('-name' => 'currency', '-default' => $form->{"currency"},
                                                       '-values' => \@values, '-labels' => \%labels)) if scalar @values;
   push @custom_hiddens, "forex";
   push @custom_hiddens, "exchangerate" if $form->{forex};
