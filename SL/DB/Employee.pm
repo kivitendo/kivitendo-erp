c7176e6155cd227ab5ce99d7a02d08982edf1499
Moritz Bunkus <m.bunkus@linet-services.de>
2010-10-06 17:40:30 +0200

    Convenience: Employee->has_right() als Wrapper um $::auth->check_right()

--- SL/DB/Employee.pm
+++ SL/DB/Employee.pm
@@ -5,5 +5,11 @@ use strict;
 use SL::DB::MetaSetup::Employee;
 use SL::DB::Manager::Employee;
 
+sub has_right {
+  my $self  = shift;
+  my $right = shift;
+
+  return $::auth->check_right($self->login, $right);
+}
 
 1;
