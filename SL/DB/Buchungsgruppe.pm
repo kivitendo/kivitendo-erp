7ed8903a4ff6e75d34069eb3bdeba2cf4f8f11db
Sven Schöling <s.schoeling@linet-services.de>
2010-10-21 11:34:45 +0200

    accessoren für income/expense_accno_id, die die postfix Indizes abstrahieren.

--- SL/DB/Buchungsgruppe.pm
+++ SL/DB/Buchungsgruppe.pm
@@ -10,4 +10,20 @@ use SL::DB::MetaSetup::Buchungsgruppe;
 # Creates get_all, get_all_count, get_all_iterator, delete_all and update_all.
 __PACKAGE__->meta->make_manager_class;
 
+sub income_accno_id {
+  my ($self, $taxzone) = @_;
+  my $taxzone_id = ref $taxzone && $taxzone->isa('SL::DB::TaxZone') ? $taxzone->id : $taxzone;
+  my $method = 'income_accno_id_' . $taxzone_id;
+
+  return $self->$method;
+}
+
+sub expense_accno_id {
+  my ($self, $taxzone) = @_;
+  my $taxzone_id = ref $taxzone && $taxzone->isa('SL::DB::TaxZone') ? $taxzone->id : $taxzone;
+  my $method = 'expense_accno_id_' . $taxzone_id;
+
+  return $self->$method;
+}
+
 1;
