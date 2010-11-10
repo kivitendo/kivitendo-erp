5547891f42ef3b13d477eba65bb0537ece1d703c
Moritz Bunkus <m.bunkus@linet-services.de>
2010-11-10 12:54:53 +0100

    Kalkulator in Einkaufsrechnungen noch nicht einbinden
    Ist schlicht dafür noch nicht angepasst und deshalb gefährlich.

--- SL/DB/PurchaseInvoice.pm
+++ SL/DB/PurchaseInvoice.pm
@@ -5,7 +5,8 @@ use strict;
 use SL::DB::MetaSetup::PurchaseInvoice;
 use SL::DB::Manager::PurchaseInvoice;
 use SL::DB::Helper::LinkedRecords;
-use SL::DB::Helper::PriceTaxCalculator;
+# The calculator hasn't been adjusted for purchase invoices yet.
+# use SL::DB::Helper::PriceTaxCalculator;
 
 __PACKAGE__->meta->add_relationship(invoiceitems => { type         => 'one to many',
                                                       class        => 'SL::DB::InvoiceItem',
