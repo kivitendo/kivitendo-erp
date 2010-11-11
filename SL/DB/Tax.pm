c19b30d22f462a5913610d1f9005a34ed95e1119
Moritz Bunkus <moritz@bunkus.org>
2010-11-11 17:38:40 +0100

    Verknüpfung Tax -> Chart

--- SL/DB/Tax.pm
+++ SL/DB/Tax.pm
@@ -1,11 +1,16 @@
-# This file has been auto-generated only because it didn't exist.
-# Feel free to modify it at will; it will not be overwritten automatically.
-
 package SL::DB::Tax;
 
 use strict;
 
 use SL::DB::MetaSetup::Tax;
+
+__PACKAGE__->meta->add_relationships(chart => { type         => 'one to one',
+                                                class        => 'SL::DB::Chart',
+                                                column_map   => { chart_id => 'id' },
+                                              },
+                                    );
+
+__PACKAGE__->meta->initialize;
 
 # Creates get_all, get_all_count, get_all_iterator, delete_all and update_all.
 __PACKAGE__->meta->make_manager_class;
