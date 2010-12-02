ed17965728399d530135538e83c436f844ec94ff
Moritz Bunkus <m.bunkus@linet-services.de>
2010-12-02 09:11:03 +0100

    Datenbankupgradescript für Unterscheidung Bankeinzug/Überweisung bei SEPA

---
+++ sql/Pg-upgrade2/sepa_in.sql
@@ -0,0 +1,12 @@
+-- @tag: sepa_in
+-- @description: Erweiterung SEPA für Kontoeinzüge
+-- @depends: release_2_6_1
+-- @charset: utf-8
+ALTER TABLE sepa_export ADD COLUMN vc varchar(10);
+UPDATE sepa_export SET vc = 'vendor';
+
+ALTER TABLE sepa_export_items ALTER COLUMN ap_id DROP NOT NULL;
+ALTER TABLE sepa_export_items ADD COLUMN ar_id integer;
+ALTER TABLE sepa_export_items ADD FOREIGN KEY (ar_id) REFERENCES ar (id);
+ALTER TABLE sepa_export_items RENAME vendor_iban TO vc_iban;
+ALTER TABLE sepa_export_items RENAME vendor_bic TO vc_bic;
