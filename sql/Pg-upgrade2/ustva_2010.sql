596cb5d6b26c139b7e6669546d76f4c3c0f6ee58
Sven Schöling <s.schoeling@linet-services.de>
2010-12-29 11:20:12 +0100

    UStVA 2010 Rahmen.
    Neue Felder hinzugefügt und html Template aktualisiert. PDF Vorlage ist dabei.
    Jetzt fehlt nur noch das Tex Template und ein Gang über die Kontenzuordnungen.

---
+++ sql/Pg-upgrade2/ustva_2010.sql
@@ -0,0 +1,15 @@
+-- @tag: ustva_2010
+-- @description: Zusätzliche UStVA Kennziffern.
+-- @charset: utf-8
+-- @depends: release_2_6_1
+
+INSERT INTO tax.report_variables (id, position, heading_id, description, taxbase, dec_places, valid_from)
+  VALUES (47, '21', 66, 'Nicht steuerbare sonstige Leistungen gem. § 18b Satz 1 Nr. 2 UStG', 0, 0, '01.01.2010');
+INSERT INTO tax.report_variables (id, position, heading_id, description, taxbase, dec_places, valid_from)
+  VALUES (48, '46', 6, 'Im Inland steuerpflichtige sonstige Leistungen von im übrigen Gemeinschaftsgebiet ansässigen Unternehmen (§13b Abs. 1 UStG)', 0, 0, '01.01.2010');
+INSERT INTO tax.report_variables (id, position, heading_id, description, taxbase, dec_places, valid_from)
+  VALUES (49, '47', 6, '', 49, 2, '01.01.2010');
+INSERT INTO tax.report_variables (id, position, heading_id, description, taxbase, dec_places, valid_from)
+  VALUES (50, '83', 8, 'Verbleibender Überschuss - bitte dem Betrag ein Minuszeichen voranstellen -', 0, 0, '01.01.2010');
+
+
