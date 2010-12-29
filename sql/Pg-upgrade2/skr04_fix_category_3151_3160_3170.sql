f6a51edf9a1aed8a362c97b6a0afca7f107d5837
Moritz Bunkus <m.bunkus@linet-services.de>
2010-12-29 15:55:14 +0100

    Falsche Kontenklasse für Konten 3151, 3160, 3170 im SKR04
    Fix für Bug 1449.

---
+++ sql/Pg-upgrade2/skr04_fix_category_3151_3160_3170.sql
@@ -0,0 +1,8 @@
+-- @tag: skr04_fix_category_3151_3160_3170
+-- @description: Falscher Kontentyp von 3151, 3160, 3170 im SKR04
+-- @depends: release_2_6_1
+-- @charset: utf-8
+UPDATE chart
+  SET category = 'L'
+  WHERE (accno IN ('3151', '3160', '3170'))
+    AND ((SELECT coa FROM defaults LIMIT 1) = 'Germany-DATEV-SKR04EU');
