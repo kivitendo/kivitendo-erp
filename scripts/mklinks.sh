6a651b73fe74db48e9581bcf80e0e21eef63e4ff
Holger Lindemann <hli@lenny.hoch.ul>
2011-01-07 10:11:18 +0100

    Ergänzen der Links und ändern der Reihenfolge wohin verlinkt wird. Zuerst auf admin.pl, dann auf am.pl, dann der Rest

--- scripts/mklinks.sh
+++ scripts/mklinks.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-for i in acctranscorrections amcvar amtemplates ap ar bankaccounts bp ca common cp ct datev dn do fu gl ic ir is licenses menujs menunew menu menuv3 menuv4 menuXML oe pe projects rc rp sepa todo ustva wh; do 
+for i in am dispatcher kopf login; do
+	rm $i.pl 2> /dev/null
+	ln -s admin.pl $i.pl
+done
+for i in acctranscorrections amcvar amtemplates ap ar bankaccounts bp ca common cp ct datev dn do fu gl ic ir is licenses menujs menunew menu menuv3 menuv4 menuXML oe pe projects rc rp sepa todo ustva wh vk; do 
 	rm $i.pl 2> /dev/null
 	ln -s am.pl $i.pl
 done
-rm admin.pl 2> /dev/null
-ln -s login.pl admin.pl
 rm generictranslations.pl 2> /dev/null
 ln -s common.pl generictranslations.pl
+rm dispatcher.fcgi 2> /dev/null
+ln -s dispatcher.fpl dispatcher.fcgi
