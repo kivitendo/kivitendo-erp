327e6e57d242cd915f656fc95b2f485a494700e6
Moritz Bunkus <m.bunkus@linet-services.de>
2011-02-25 17:53:44 +0100

    Hashen der Passwörter mittels Digest::SHA1 (Fallback auf Digest::MD5)
    Teil eines Fixes für Bug 1606. Hintergrund ist, dass bei Nutzernamen
    mit Umlaut als zweitem Char crypt() ungültiges UTF-8 erstellt: Crypt
    nimmt die ersten zwei Bytes (nicht Zeichen) einer Zeichenfolge und
    stellt diese als Salt dem Ergebnis vorweg. Das führt zu einem
    ungültigen UTF-8-Char, falls das erste Zeichen ASCII 7 bit und das
    zweite Zeichen UTF-8 ist. Beispiel: "möp"

--- SL/Auth/DB.pm
+++ SL/Auth/DB.pm
@@ -1,9 +1,11 @@
 package SL::Auth::DB;
 
+use strict;
+
+use Carp;
+
 use SL::Auth::Constants qw(:all);
 use SL::DBUtils;
-
-use strict;
 
 sub new {
   $main::lxdebug->enter_sub();
@@ -26,7 +28,6 @@ sub authenticate {
   my $self       = shift;
   my $login      = shift;
   my $password   = shift;
-  my $is_crypted = shift;
 
   my $dbh        = $self->{auth}->dbconnect();
 
@@ -38,8 +39,13 @@ sub authenticate {
   my $query             = qq|SELECT password FROM auth."user" WHERE login = ?|;
   my ($stored_password) = $dbh->selectrow_array($query, undef, $login);
 
-  $password        = crypt $password, substr($login, 0, 2)        if (!$password || !$is_crypted);
-  $stored_password = crypt $stored_password, substr($login, 0, 2) if (!$stored_password);
+  my ($algorithm, $algorithm2);
+
+  # Empty password hashes in the database mean just that -- empty
+  # passwords. Hash it for easier comparison.
+  $stored_password               = $self->hash_password(password => $stored_password) unless $stored_password;
+  ($algorithm, $stored_password) = $self->parse_password_entry($stored_password);
+  ($algorithm2, $password)       = $self->parse_password_entry($self->hash_password(password => $password, algorithm => $algorithm, login => $login));
 
   $main::lxdebug->leave_sub();
 
@@ -65,7 +71,7 @@ sub change_password {
     return ERR_BACKEND;
   }
 
-  $password = crypt $password, substr($login, 0, 2) if (!$is_crypted);
+  $password = $self->hash_password(password => $password) unless $is_crypted;
 
   do_query($main::form, $dbh, qq|UPDATE auth."user" SET password = ? WHERE login = ?|, $password, $login);
 
@@ -78,6 +84,44 @@ sub change_password {
 
 sub verify_config {
   return 1;
+}
+
+sub hash_password {
+  my ($self, %params) = @_;
+
+  if (!$params{algorithm}) {
+    $params{algorithm}          = 'SHA1';
+    $params{fallback_algorithm} = 'MD5';
+  }
+
+  if ($params{algorithm} eq 'SHA1') {
+    if (eval { require Digest::SHA1; 1 }) {
+      return '{SHA1}' . Digest::SHA1::sha1_hex($params{password});
+
+    } elsif ($params{fallback_algorithm}) {
+      return $self->hash_password(%params, algorithm => $params{fallback_algorithm});
+
+    } else {
+      die 'Digest::SHA1 not available';
+    }
+
+  } elsif ($params{algorithm} eq 'MD5') {
+    require Digest::MD5;
+    return '{MD5}' . Digest::MD5::md5_hex($params{password});
+
+  } elsif ($params{algorithm} eq 'CRYPT') {
+    return '{CRYPT}' . crypt($params{password}, substr($params{login}, 0, 2));
+
+  } else {
+    croak 'Unsupported hash algorithm ' . $params{algorithm};
+  }
+}
+
+sub parse_password_entry {
+  my ($self, $password) = @_;
+
+  return ($1, $2) if $password =~ m/^\{ ([^\}]+) \} (.+)/x;
+  return ('CRYPT', $password);
 }
 
 1;
