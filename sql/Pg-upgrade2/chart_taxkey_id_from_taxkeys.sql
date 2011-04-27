90221504d9934369ef85bc88b7270e9da56f9c89
Moritz Bunkus <moritz@bunkus.org>
2011-04-27 10:47:44 +0200

    Beim Speichern eines Kontos chart.taxkey_id aus taxkeys aktualisieren/setzen
    Fix für Bug 1627.

---
+++ sql/Pg-upgrade2/chart_taxkey_id_from_taxkeys.sql
@@ -0,0 +1,12 @@
+-- @tag: chart_taxkey_id_from_taxkeys
+-- @description: chart.taxkey_id aus taxkeys aktualisieren
+-- @depends: release_2_6_2
+-- @charset: utf-8
+UPDATE chart
+SET taxkey_id = (
+  SELECT taxkey_id
+  FROM taxkeys
+  WHERE taxkeys.chart_id = chart.id
+  ORDER BY startdate DESC
+  LIMIT 1
+);
