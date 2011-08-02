6339766b422889992687847e131f6d8b2aa9f57e
G. Richardson <information@lx-office-hosting.de>
2011-08-02 12:48:42 +0200

    Changelog zu Erweiterung Standardsprache
    und kleine Verbesserungen

--- sql/Pg-upgrade2/defaults_add_language_id.sql
+++ sql/Pg-upgrade2/defaults_add_language_id.sql
@@ -1,5 +1,5 @@
 -- @tag: defaults_add_language_id
 -- @description: Id der Standardsprache in defaults speichern
--- @depends: release_2_6_2
+-- @depends: release_2_6_3
 -- @charset: utf-8
 ALTER TABLE defaults ADD COLUMN language_id integer;
