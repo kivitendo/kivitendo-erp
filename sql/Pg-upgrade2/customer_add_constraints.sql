e05b8d340be3f8b7516ae37f4532977d3860c10d
Jan Büren <jan@lx-office-hosting.de>
2011-10-06 18:47:06 +0200

    Verwaiste Zahlungsbedingungen in vc vorher auf NULL setzen und danach den neuen Fremdschlüssel

--- sql/Pg-upgrade2/customer_add_constraints.sql
+++ sql/Pg-upgrade2/customer_add_constraints.sql
@@ -4,6 +4,9 @@
 -- @charset: utf-8
 -- @ignore: 0
 
+-- verwaiste Zahlungsbedingungen vorher entfernen:
+update customer set payment_id = NULL where payment_id not in (select id from payment_terms);
+
 ALTER TABLE customer ADD FOREIGN KEY (payment_id) REFERENCES payment_terms (id);
 ALTER TABLE customer ADD FOREIGN KEY (language_id) REFERENCES language (id);
 ALTER TABLE customer ADD FOREIGN KEY (business_id) REFERENCES business (id);
