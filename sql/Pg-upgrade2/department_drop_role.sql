eaad1d7c4806965240c38151dc0258c42313cf56
Jan Büren <jan@lx-office-hosting.de>
2011-10-12 16:42:59 +0200

    Rechtschreibfehler in SQL-Datei korrigiert

--- sql/Pg-upgrade2/department_drop_role.sql
+++ sql/Pg-upgrade2/department_drop_role.sql
@@ -1,5 +1,5 @@
 -- @tag: department_drop_role
--- @description: Kosten- und Erfolgstellen zu unterscheiden, macht(e) mittlerweile keinen Sinn mehr, da man ja entsprechend Kosten als Erfolg auf eine Kostenstelle buchen möchte. Ferner wird auch die Auswahlliste schon länger nicht mehr unterschieden
+-- @description: Kosten- und Erfolgstellen zu unterscheiden macht(e) mittlerweile keinen Sinn mehr, da man ja entsprechend Kosten als Erfolg auf eine Kostenstelle buchen möchte. Ferner wird auch die Auswahlliste schon länger nicht mehr unterschieden
 -- @depends: release_2_6_3
 -- @charset: utf-8
 -- @ignore: 0
