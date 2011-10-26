1d64d7e59fe964edde711f52beeb291e092459ab
Sven Schöling <s.schoeling@linet-services.de>
2011-10-26 14:58:15 +0200

    Loginmechanismus für Testscripte

---
+++ t/Support/TestSetup.pm
@@ -0,0 +1,52 @@
+package Support::TestSetup;
+
+use strict;
+
+use Data::Dumper;
+use CGI qw( -no_xhtml);
+use SL::Auth;
+use SL::Form;
+use SL::Locale;
+use SL::LXDebug;
+use Data::Dumper;
+use SL::LxOfficeConf;
+use SL::InstanceConfiguration;
+SL::LxOfficeConf->read;
+
+sub _login {
+  my $login = shift;
+
+  die 'need login' unless $login;
+
+  package main;
+
+  $::lxdebug       = LXDebug->new(file => \*STDERR);
+  $::locale        = Locale->new($::lx_office_conf{system}->{language});
+  $::form          = Form->new;
+  $::auth          = SL::Auth->new;
+  $::instance_conf = SL::InstanceConfiguration->new;
+  $::request       = { cgi => CGI->new({}) };
+
+  die 'cannot reach auth db'               unless $::auth->session_tables_present;
+
+  $::auth->restore_session;
+
+  require "bin/mozilla/common.pl";
+
+  die "cannot find user $login"            unless %::myconfig = $::auth->read_user($login);
+
+  $::form->{login} = $login; # normaly implicit at login
+
+  die "cannot find locale for user $login" unless $::locale   = Locale->new($::myconfig{countrycode});
+
+  $::instance_conf->init;
+
+  return 1;
+}
+
+sub login {
+  my $login        = shift || $::lx_office_conf{testing}{login}        || 'demo';
+  _login($login);
+}
+
+1;
