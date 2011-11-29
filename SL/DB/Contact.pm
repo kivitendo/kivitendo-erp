9209583a11c108b33ce24ebfc8d26fc1ef1f8268
Sven Schöling <s.schoeling@linet-services.de>
2011-11-29 16:44:30 +0100

    full_name und full_name_dep methoden für Contact

--- SL/DB/Contact.pm
+++ SL/DB/Contact.pm
@@ -14,4 +14,17 @@ use SL::DB::Helper::CustomVariables (
 # Creates get_all, get_all_count, get_all_iterator, delete_all and update_all.
 __PACKAGE__->meta->make_manager_class;
 
+sub full_name {
+  my ($self) = @_;
+  die 'not an accessor' if @_ > 1;
+  join ', ', grep $_, $self->cp_name, $self->cp_givenname;
+}
+
+sub full_name_dep {
+  my ($self) = @_;
+  die 'not an accessor' if @_ > 1;
+  $self->full_name
+    . join '', map { " ($_)" } grep $_, $self->cp_abteilung;
+}
+
 1;
