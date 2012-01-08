4c4939d0e5e8d9c872a298571aaff27082a2b95d
Bernd Blessmann <bibi@online.de>
2012-01-08 16:22:02 +0100

    Fix für Bug 1773: SQL Fehler bei Anzeige von Angeboten
    DB-Upgrade Sktript für commit 94802c79cda890c12128a83bc931cb342aed470c

---
+++ sql/Pg-upgrade2/customer_vendor_add_currency.sql
@@ -0,0 +1,6 @@
+-- @tag: customer_vendor_add_currency
+-- @description: Spalten für Währung bei Kunde/Lieferant
+-- @depends: release_2_6_3
+-- @charset: utf-8
+ALTER TABLE customer ADD COLUMN curr character(3);
+ALTER TABLE vendor   ADD COLUMN curr character(3);
