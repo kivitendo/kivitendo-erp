2d193a2e8467c752922bb124c1f9495ee7413b9d
Jan Büren <jan@lx-office-hosting.de>
2012-01-18 20:06:15 +0100

    Entfernen der Spalte datevexport in Tabelle vendor und customer.
    Dieser Status wird beim Datevexport überhaupt nicht berücksichtigt.
    Entsprechend auch die Funktion setdatevexport und die beiden Trigger
    customer_datevexport und vendor_datevexport rausgenommen.

---
+++ sql/Pg-upgrade2/drop_datevexport.sql
@@ -0,0 +1,9 @@
+-- @tag: drop_datevexport
+-- @description: Entfernen der Spalte datevexport in Tabelle chart, vendor und customer. Dieser Status wird beim Datevexport überhaupt nicht berücksichtigt.
+-- @charset: utf-8
+-- @depends: release_2_6_3
+ALTER TABLE vendor DROP COLUMN datevexport;
+ALTER TABLE customer DROP COLUMN datevexport;
+DROP TRIGGER vendor_datevexport on vendor;
+DROP TRIGGER customer_datevexport on customer;
+DROP FUNCTION set_datevexport();
