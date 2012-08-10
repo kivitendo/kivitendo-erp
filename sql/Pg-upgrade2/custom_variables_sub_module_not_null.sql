5ee95a8f01b5854ca9d5acf5d80c23dbe564c19f
Sven Schöling <s.schoeling@linet-services.de>
2012-08-10 10:05:04 +0200

    Natürlich nur die sub_modules auf '' setzen, die auch NULL sind.

--- sql/Pg-upgrade2/custom_variables_sub_module_not_null.sql
+++ sql/Pg-upgrade2/custom_variables_sub_module_not_null.sql
@@ -2,7 +2,7 @@
 -- @description: sub_module in custom_variables auf NOT NULL ändern.
 -- @encoding: utf-8
 -- @depends: release_2_7_0
-UPDATE custom_variables SET sub_module = '';
+UPDATE custom_variables SET sub_module = '' WHERE sub_module IS NULL;
 ALTER TABLE custom_variables ALTER COLUMN sub_module SET DEFAULT '';
 ALTER TABLE custom_variables ALTER COLUMN sub_module SET NOT NULL;
 
