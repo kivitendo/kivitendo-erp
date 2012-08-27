9ac69514fec5d49a989f7489beaed480a42d217d
Moritz Bunkus <m.bunkus@linet-services.de>
2012-08-27 11:32:23 +0200

    Konfigurationsdateien config/lx_office.conf{.default} in kivitendo.conf{.default} umbenennen

--- SL/LxOfficeConf.pm
+++ SL/LxOfficeConf.pm
@@ -30,10 +30,13 @@ sub read {
 
   return unless $class->safe_require($may_fail);
 
-  read_config('config/lx_office.conf.default' => \%::lx_office_conf);
+  # Backwards compatibility: read lx_office.conf.default if
+  # kivitendo.conf.default does't exist.
+  my $default_config = -f "config/kivitendo.conf.default" ? 'kivitendo' : 'lx_office';
+  read_config("config/${default_config}.conf.default" => \%::lx_office_conf);
   _decode_recursively(\%::lx_office_conf);
 
-  $file_name ||= 'config/lx_office.conf';
+  $file_name ||= -f 'config/kivitendo.conf' ? 'config/kivitendo.conf' : 'config/lx_office.conf';
 
   if (-f $file_name) {
     read_config($file_name => \ my %local_conf);
