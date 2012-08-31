42575e7218eebbeef11c641ac4879adb2775fdd8
Sven Schöling <s.schoeling@linet-services.de>
2012-08-31 12:12:11 +0200

    switchmenuframe so umgeschrieben dass es keine fehler wirft

--- js/switchmenuframe.js
+++ js/switchmenuframe.js
@@ -1,17 +1,15 @@
 var vSwitch_Menu = 1;
-var FrameSize = (parent.document.getElementById('menuframe').cols);
+var Frame;
+var FrameSize;
 
-function Switch_Menu()
-{
-    if (vSwitch_Menu)
-    {
-        vSwitch_Menu=false;
-                parent.document.getElementById('menuframe').setAttribute('cols','30,*');
-    }
-    else
-    {
-        vSwitch_Menu=true;
-                parent.document.getElementById('menuframe').setAttribute('cols',FrameSize);
-    }
-    return;
+function Switch_Menu() {
+  if (Frame) {
+    Frame.attr('cols',vSwitch_Menu ? '30,*' : FrameSize);
+    vSwitch_Menu=!vSwitch_Menu;
+  }
 }
+
+$(function(){
+  Frame = $(parent.document.getElementById('menuframe'));
+  FrameSize = Frame.attr('cols');
+})
