1cd0721d9aa749dda37f3dccaa3da2ba0ebcd2cd
Sven Schöling <s.schoeling@linet-services.de>
2012-11-29 16:37:06 +0100

    customer_autocomplete
    Javascript war nicht in der Lage mehr als ein Feld zu handhaben. Es ist
    jetzt ausgelagert in ein Javascript und wird nur bei Bedarf geladen.

---
+++ js/autocomplete_customer.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('input.customer_autocomplete').each(function(i,real){
+    var dummy = $('#' + real.id + '_name');
+    $(dummy).autocomplete({
+      source: function(req, rsp) {
+        $.ajax({
+          url: 'controller.pl?action=Customer/ajax_autocomplete',
+          dataType: "json",
+          data: {
+            term: req.term,
+            current: function() { real.val },
+            obsolete: 0,
+          },
+          success: function (data){ rsp(data) }
+        });
+      },
+      limit: 20,
+      delay: 50,
+      select: function(event, ui) {
+        $(real).val(ui.item.id);
+        $(dummy).val(ui.item.name);
+      },
+    });
+  })
+})
