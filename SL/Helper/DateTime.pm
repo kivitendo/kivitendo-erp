14817fd938d8bdc6627e37aca09abb7aba43c290
Moritz Bunkus <m.bunkus@linet-services.de>
2013-01-23 10:28:56 +0100

    DateTime-Helfer: Funktionsnamen in (to|from)_kivitendo umbenannt
    Mit Legacy-Aliasen.

--- SL/Helper/DateTime.pm
+++ SL/Helper/DateTime.pm
@@ -10,14 +10,24 @@ sub today_local {
   return shift->now(time_zone => $::locale->get_local_time_zone)->truncate(to => 'day');
 }
 
-sub to_lxoffice {
+sub to_kivitendo {
   my $self   = shift;
   my %params = (scalar(@_) == 1) && (ref($_[0]) eq 'HASH') ? %{ $_[0] } : @_;
   return $::locale->format_date_object($self, %params);
 }
 
-sub from_lxoffice {
+sub to_lxoffice {
+  # Legacy name.
+  goto &to_kivitendo;
+}
+
+sub from_kivitendo {
   return $::locale->parse_date_to_object(\%::myconfig, $_[1]);
+}
+
+sub from_lxoffice {
+  # Legacy name.
+  gofrom &from_kivitendo;
 }
 
 1;
@@ -42,18 +52,22 @@ Returns the current time with the time zone set to the local time zone.
 
 Returns the current date with the time zone set to the local time zone.
 
-=item C<to_lxoffice %param>
+=item C<to_kivitendo %param>
 
 Formats the date and time according to the current kivitendo user's
 date format with L<Locale::format_datetime_object>.
 
-=item C<from_lxoffice $string>
+The legacy name C<to_lxoffice> is still supported.
+
+=item C<from_kivitendo $string>
 
 Parses a date string formatted in the current kivitendo user's date
 format and returns an instance of L<DateTime>.
 
 Note that only dates can be parsed at the moment, not the time
-component (as opposed to L<to_lxoffice>).
+component (as opposed to L<to_kivitendo>).
+
+The legacy name C<from_lxoffice> is still supported.
 
 =back
 
