8bf1fc96aa2da3aa3fa957b78e943d9969484cd2
Moritz Bunkus <m.bunkus@linet-services.de>
2013-01-31 15:13:14 +0100

    Kundenstammdaten: Stundensatz bearbeiten können

--- SL/Controller/CustomerVendor.pm
+++ SL/Controller/CustomerVendor.pm
@@ -641,6 +641,7 @@ sub _instantiate_args {
     $self->{cv}->taxincluded_checked(undef);
   }
 
+  $self->{cv}->hourly_rate($::lx_office_conf{'features/customer'}->{default_hourly_rate}) if $self->is_customer && !$self->{cv}->hourly_rate;
 
   foreach my $cvar (@{$self->{cv}->cvars_by_config()}) {
     my $value = $::form->{cv_cvars}->{$cvar->config->name};
