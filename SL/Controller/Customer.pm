d157b20fa9377ec48a8b00b7f5ccf8299897f0d2
Moritz Bunkus <m.bunkus@linet-services.de>
2013-02-01 16:04:36 +0100

    Customer-Controller: 'get_hourly_rate'-AJAX-Funktion

---
+++ SL/Controller/Customer.pm
@@ -0,0 +1,29 @@
+package SL::Controller::Customer;
+
+use strict;
+use parent qw(SL::Controller::Base);
+
+use SL::DB::Customer;
+use SL::JSON;
+
+# safety
+__PACKAGE__->run_before(sub { $::auth->assert('customer_vendor_edit') });
+
+sub action_get_hourly_rate {
+  my ($self, %params) = @_;
+
+  my $customer;
+  $customer = SL::DB::Customer->new(id => $::form->{id})->load if $::form->{id};
+  my $data  = {};
+
+  if ($customer) {
+    $data = {
+      hourly_rate           => $customer->hourly_rate * 1,
+      hourly_rate_formatted => $::form->format_amount(\%::myconfig, $customer->hourly_rate, 2),
+    };
+  }
+
+  $self->render(\to_json($data), { type => 'json' });
+}
+
+1;
