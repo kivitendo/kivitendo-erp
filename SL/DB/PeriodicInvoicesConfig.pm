75603a224c1bc962d18e9398972fa3bf2ff77f87
Moritz Bunkus <m.bunkus@linet-services.de>
2013-02-15 14:50:53 +0100

    Rose-Model Wiederkehrende Rechnungen: Foreign Key "order" nennen, nicht "oe"

--- SL/DB/PeriodicInvoicesConfig.pm
+++ SL/DB/PeriodicInvoicesConfig.pm
@@ -4,16 +4,6 @@ use strict;
 
 use SL::DB::MetaSetup::PeriodicInvoicesConfig;
 
-__PACKAGE__->meta->add_relationships(
-  order        => {
-    type       => 'many to one',
-    class      => 'SL::DB::Order',
-    column_map => { oe_id => 'id' },
-  },
-);
-
-__PACKAGE__->meta->initialize;
-
 # Creates get_all, get_all_count, get_all_iterator, delete_all and update_all.
 __PACKAGE__->meta->make_manager_class;
 
