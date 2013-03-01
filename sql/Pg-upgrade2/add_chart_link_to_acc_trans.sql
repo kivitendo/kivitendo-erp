ca9d536ff53e36883d715fd02ddabd829374dd90
Niclas Zimmermann <niclas@kivitendo-premium.de>
2013-03-01 13:08:20 +0100

    Verbesserung sql-Upgrate
    Das Update definiert nun die Spalte chart.link als Pflichtfeld bevor
    in der acc_trans die Spalte chart_link hinzugefügt wird. Alle Einträge
    in chart.link, die bisher NULL waren werden durch '' ersetzt.

--- sql/Pg-upgrade2/add_chart_link_to_acc_trans.sql
+++ sql/Pg-upgrade2/add_chart_link_to_acc_trans.sql
@@ -2,11 +2,17 @@
 -- @description: Neue Spalte chart_link in der acc_trans
 -- @depends: release_3_0_0 
 
---neue Spalte hinzufügen:
+--Spalte link in der chart updaten:
+UPDATE chart SET link = '' WHERE link IS NULL;
+
+--chart.link als Pflichtfeld definieren:
+ALTER TABLE chart ALTER link SET NOT NULL;
+
+--neue Spalte chart_link zur acc_trans hinzufügen:
 ALTER TABLE acc_trans ADD COLUMN chart_link text;
 
 --Spalte mit Werten füllen:
 UPDATE acc_trans SET chart_link = (SELECT link FROM chart WHERE id=chart_id);
 
---Spalte als Pflichtfeld definieren:
+--Spalte acc_trans.chart_link als Pflichtfeld definieren:
 ALTER TABLE acc_trans ALTER chart_link SET NOT NULL;
