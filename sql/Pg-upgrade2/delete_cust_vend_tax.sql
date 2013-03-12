001ff85869942b7b69b27438afc4a4d49b9e464a
Niclas Zimmermann <niclas@kivitendo-premium.de>
2013-03-12 11:41:05 +0100

    Alte Trigger anpassen
    Die Tabellen vendortax und customertax wurden in Commit 9f07753b
    entfernt. Allerdings mussten die Trigger del_vendor() und
    del_customer() noch angepasst werden.
    
    Behebt Bug #2201.

---
+++ sql/Pg-upgrade2/delete_cust_vend_tax.sql
@@ -0,0 +1,17 @@
+-- @tag: delete_cust_vend_tax
+-- @description: Alte Tabellen löschen
+-- @depends: delete_customertax_vendortax_partstax
+-- @charset: utf-8
+
+CREATE OR REPLACE FUNCTION del_customer() RETURNS "trigger" AS
+  'BEGIN
+     DELETE FROM shipto WHERE trans_id = old.id;
+     RETURN NULL;
+   END;' LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION del_vendor() RETURNS "trigger" AS
+  'BEGIN
+    DELETE FROM shipto WHERE trans_id = old.id;
+    RETURN NULL;
+  END;' LANGUAGE plpgsql;
+
