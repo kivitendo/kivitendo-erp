ffb54c7e12b01c82598a724fe67579a45572327c
Bernd Bleßmann <bernd@kivitendo-premium.de>
2013-03-13 10:07:22 +0100

    Neue Spalte numheaders in csv_import_reports für Reports bei Multiplex-CSV-Daten
      Dabei auch nicht-leere Tabellen berücksichtigen.

---
+++ sql/Pg-upgrade2/csv_import_reports_add_numheaders.sql
@@ -0,0 +1,8 @@
+-- @tag: csv_import_reports_add_numheaders
+-- @description: Anzahl der Header-Zeilen in Csv Import Report speichern
+-- @depends: csv_import_report_cache
+-- @encoding: utf-8
+
+ALTER TABLE csv_import_reports ADD COLUMN numheaders INTEGER;
+UPDATE csv_import_reports SET numheaders = 1;
+ALTER TABLE csv_import_reports ALTER COLUMN numheaders SET NOT NULL;
