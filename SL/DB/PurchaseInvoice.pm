ce004da66dbdc852ab24aa598edabfe1d326147c
Moritz Bunkus <m.bunkus@linet-services.de>
2013-04-30 14:38:49 +0200

    Kosmetik

--- SL/DB/PurchaseInvoice.pm
+++ SL/DB/PurchaseInvoice.pm
@@ -10,12 +10,14 @@ use SL::DB::Helper::LinkedRecords;
 # The calculator hasn't been adjusted for purchase invoices yet.
 # use SL::DB::Helper::PriceTaxCalculator;
 
-__PACKAGE__->meta->add_relationship(invoiceitems => { type         => 'one to many',
-                                                      class        => 'SL::DB::InvoiceItem',
-                                                      column_map   => { id => 'trans_id' },
-                                                      manager_args => { with_objects => [ 'part' ] }
-                                                    },
-                                   );
+__PACKAGE__->meta->add_relationship(
+  invoiceitems   => {
+    type         => 'one to many',
+    class        => 'SL::DB::InvoiceItem',
+    column_map   => { id => 'trans_id' },
+    manager_args => { with_objects => [ 'part' ] }
+  },
+);
 
 __PACKAGE__->meta->initialize;
 
