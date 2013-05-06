dc5f7965f497eae8ea6531185acca03a61cd0820
Sven Schöling <s.schoeling@linet-services.de>
2013-05-06 16:02:52 +0200

    Manager für Projekt Status

--- SL/DB/ProjectStatus.pm
+++ SL/DB/ProjectStatus.pm
@@ -1,13 +1,29 @@
-# This file has been auto-generated only because it didn't exist.
-# Feel free to modify it at will; it will not be overwritten automatically.
-
 package SL::DB::ProjectStatus;
 
 use strict;
 
 use SL::DB::MetaSetup::ProjectStatus;
+use SL::DB::Manager::ProjectStatus;
 
-# Creates get_all, get_all_count, get_all_iterator, delete_all and update_all.
-__PACKAGE__->meta->make_manager_class;
+use SL::DB::Helper::ActsAsList;
+
+__PACKAGE__->meta->add_relationship(
+  projects => {
+    type         => 'many to one',
+    class        => 'SL::DB::Project',
+    column_map   => { id => 'project_status_id' },
+  },
+);
+
+__PACKAGE__->meta->initialize;
+
+sub validate {
+  my ($self) = @_;
+
+  my @errors;
+  push @errors, $::locale->text('The description is missing.') if !$self->description;
+
+  return @errors;
+}
 
 1;
