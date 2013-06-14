91b948e473f5b731a6886e4b2cd4c526a42ace4e
Moritz Bunkus <m.bunkus@linet-services.de>
2013-06-14 10:09:44 +0200

    Manager für Warehouse- und Currency-Models

--- SL/DB/Warehouse.pm
+++ SL/DB/Warehouse.pm
@@ -1,14 +1,10 @@
-# This file has been auto-generated only because it didn't exist.
-# Feel free to modify it at will; it will not be overwritten automatically.
-
 package SL::DB::Warehouse;
 
 use strict;
 
 use SL::DB::MetaSetup::Warehouse;
+use SL::DB::Manager::Warehouse;
 use SL::DB::Helper::ActsAsList;
-
-__PACKAGE__->meta->make_manager_class;
 
 __PACKAGE__->meta->add_relationship(
   bins => {
@@ -18,13 +14,14 @@ __PACKAGE__->meta->add_relationship(
   }
 );
 
-# Creates get_all, get_all_count, get_all_iterator, delete_all and update_all.
-#__PACKAGE__->meta->make_manager_class;
-
 __PACKAGE__->meta->initialize;
 
+sub bins_sorted {
+  return [ sort { $a->id <=> $b->id } @{ shift()->bins || [] } ];
+}
+
 sub first_bin {
-  return shift()->bins->[0];
+  return shift()->bins_sorted->[0];
 }
 
 1;
