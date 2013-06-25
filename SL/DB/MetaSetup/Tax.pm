e237d75a359d816142959fb01689785f35b1435f
Sven Schöling <s.schoeling@linet-services.de>
2013-06-25 11:53:00 +0200

    Metasetup für tax aus tax_constraints.pl, steuerfilterung.pl

--- SL/DB/MetaSetup/Tax.pm
+++ SL/DB/MetaSetup/Tax.pm
@@ -10,19 +10,27 @@ __PACKAGE__->meta->setup(
   table   => 'tax',
 
   columns => [
-    chart_id       => { type => 'integer' },
-    rate           => { type => 'numeric', precision => 5, scale => 15 },
-    taxnumber      => { type => 'text' },
-    taxkey         => { type => 'integer' },
-    taxdescription => { type => 'text' },
-    itime          => { type => 'timestamp', default => 'now()' },
-    mtime          => { type => 'timestamp' },
-    id             => { type => 'integer', not_null => 1, sequence => 'id' },
+    chart_id         => { type => 'integer' },
+    rate             => { type => 'numeric', default => '0', not_null => 1, precision => 5, scale => 15 },
+    taxnumber        => { type => 'text' },
+    taxkey           => { type => 'integer', not_null => 1 },
+    taxdescription   => { type => 'text', not_null => 1 },
+    itime            => { type => 'timestamp', default => 'now()' },
+    mtime            => { type => 'timestamp' },
+    id               => { type => 'integer', not_null => 1, sequence => 'id' },
+    chart_categories => { type => 'text', not_null => 1 },
   ],
 
   primary_key_columns => [ 'id' ],
 
   allow_inline_column_values => 1,
+
+  foreign_keys => [
+    chart => {
+      class       => 'SL::DB::Chart',
+      key_columns => { chart_id => 'id' },
+    },
+  ],
 );
 
 1;
