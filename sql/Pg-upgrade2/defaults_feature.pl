a0da7f1d521283359ccb3bf79ab52157c0cdc043
Moritz Bunkus <m.bunkus@linet-services.de>
2013-06-27 15:53:36 +0200

    "konfigurierbare Feature nach defaults migriert (trac 2300) und Dokumentenbelege (2301) in Webdav speichern"
    SQL-Upgrade und Edit-Maske. Fehlen noch einige Teile.

---
+++ sql/Pg-upgrade2/defaults_feature.pl
@@ -0,0 +1,36 @@
+# @tag: defaults_feature
+# @description: Einstellen der Feature vom Config-File in die DB verlagern.
+# @depends: release_3_0_0
+package SL::DBUpgrade2::defaults_feature;
+
+use utf8;
+
+use parent qw(SL::DBUpgrade2::Base);
+use strict;
+
+sub run {
+  my ($self) = @_;
+
+  # this query will fail if column already exist (new database)
+  $self->db_query(qq|ALTER TABLE defaults ADD COLUMN webdav              boolean DEFAULT false|);
+  $self->db_query(qq|ALTER TABLE defaults ADD COLUMN webdav_documents    boolean DEFAULT false|);
+  $self->db_query(qq|ALTER TABLE defaults ADD COLUMN vertreter           boolean DEFAULT false|);
+  $self->db_query(qq|ALTER TABLE defaults ADD COLUMN parts_show_image    boolean DEFAULT true|);
+  $self->db_query(qq|ALTER TABLE defaults ADD COLUMN parts_listing_image boolean DEFAULT true|);
+  $self->db_query(qq|ALTER TABLE defaults ADD COLUMN parts_image_css     text    DEFAULT 'border:0;float:left;max-width:250px;margin-top:20px:margin-right:10px;margin-left:10px;'|);
+
+  # check current configuration and set default variables accordingly, so that
+  # kivitendo's behaviour isn't changed by this update
+  # if checks are not set in config set it to true
+  foreach my $check (qw(webdav vertreter parts_show_image parts_listing_image)) {
+    my $check_set = $::lx_office_conf{features}->{$check} ? 1 : 0;
+    $self->db_query("UPDATE defaults SET $check = ?", bind => [ $check_set ]);
+  }
+
+  my $update_column = "UPDATE defaults SET parts_image_css = ?";
+  $self->db_query($update_column, bind => [ $::lx_office_conf{features}->{parts_image_css} ]);
+
+  return 1;
+}
+
+1;
