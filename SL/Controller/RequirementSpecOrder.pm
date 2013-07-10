c1f5a8f3f28952c290edb35ee3c0a37f5621ef69
Moritz Bunkus <m.bunkus@linet-services.de>
2013-07-10 15:47:06 +0200

    Pflichtenhefte: Maske fürs Erstellen von Angeboten/Aufträgen

--- SL/Controller/RequirementSpecOrder.pm
+++ SL/Controller/RequirementSpecOrder.pm
@@ -6,6 +6,7 @@ use utf8;
 use parent qw(SL::Controller::Base);
 
 use SL::ClientJS;
+use SL::DB::Customer;
 use SL::DB::Part;
 use SL::DB::RequirementSpec;
 use SL::DB::RequirementSpecOrder;
@@ -14,7 +15,7 @@ use SL::Locale::String;
 
 use Rose::Object::MakeMethods::Generic
 (
-  'scalar --get_set_init' => [ qw(requirement_spec js all_parts) ],
+  'scalar --get_set_init' => [ qw(requirement_spec js all_customers all_parts) ],
 );
 
 __PACKAGE__->run_before('setup');
@@ -28,6 +29,14 @@ sub action_list {
   my ($self) = @_;
 
   $self->render('requirement_spec_order/list', { layout => 0 });
+}
+
+sub action_new {
+  my ($self) = @_;
+
+  my $html   = $self->render('requirement_spec_order/new', { output => 0 }, make_part_title => sub { $_[0]->partnumber . ' ' . $_[0]->description });
+  $self->js->html('#ui-tabs-4', $html)
+           ->render($self);
 }
 
 sub action_edit_assignment {
@@ -84,6 +93,7 @@ sub init_js {
 # helpers
 #
 
-sub init_all_parts { SL::DB::Manager::Part->get_all_sorted }
+sub init_all_customers { SL::DB::Manager::Customer->get_all_sorted }
+sub init_all_parts     { SL::DB::Manager::Part->get_all_sorted     }
 
 1;
