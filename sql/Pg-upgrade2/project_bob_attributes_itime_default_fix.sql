5ab85d98e067816760dd68b26a2656b1f1790bc7
Moritz Bunkus <m.bunkus@linet-services.de>
2013-07-15 10:07:20 +0200

    SQL-Upgrade-Abhängigkeiten gefixt

--- sql/Pg-upgrade2/project_bob_attributes_itime_default_fix.sql
+++ sql/Pg-upgrade2/project_bob_attributes_itime_default_fix.sql
@@ -1,6 +1,6 @@
 -- @tag: project_bob_attributes_itime_default_fix
 -- @description: Standardwert für 'itime'-Spalten in Bob-Tabellen fixen
--- @depends: release_3_0_0
+-- @depends: project_bob_attributes
 
 ALTER TABLE project                    ALTER COLUMN itime SET DEFAULT now();
 ALTER TABLE project_status             ALTER COLUMN itime SET DEFAULT now();
