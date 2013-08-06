1bdfacee11393f18563ccd8143a9408f07449ede
Moritz Bunkus <m.bunkus@linet-services.de>
2013-08-06 17:35:29 +0200

    Template: as_list-Methode zum Forçieren vom Listenkontext

--- SL/Template/Plugin/HTMLFixes.pm
+++ SL/Template/Plugin/HTMLFixes.pm
@@ -1,6 +1,7 @@
 package SL::Template::Plugin::HTMLFixes;
 
 use Template::Plugin::HTML;
+use Template::Stash;
 
 1;
 
@@ -25,5 +26,21 @@ sub url {
     $text =~ s/([^a-zA-Z0-9_.-])/uc sprintf("%%%02x",ord($1))/eg;
     return $text;
 }
+
+1;
+
+package Template::Stash;
+
+# A method for forcing list context. If a method uses 'wantarray' then
+# calling that method from Template will do strange stuff like chosing
+# scalar context. The most obvious offender are RDBO relationships.
+
+# Example of how NOT to test whether or not a customer has contacts:
+#   [% IF customer.contacts.size %] ...
+# Instead force list context and then test the size:
+#   [% IF customer.contacts.as_list.size %] ...
+$Template::Stash::LIST_OPS->{ as_list } = sub {
+  return ref( $_[0] ) eq 'ARRAY' ? shift : [shift];
+};
 
 1;
