701674940c1daf886257a14c5e933fcda2a1883d
Moritz Bunkus <m.bunkus@linet-services.de>
2013-12-10 15:49:45 +0100

    RDBO-Model von delivery_terms neu erzeugen, als Mapping hinzufügen

--- SL/DB/MetaSetup/DeliveryTerm.pm
+++ SL/DB/MetaSetup/DeliveryTerm.pm
@@ -6,22 +6,20 @@ use strict;
 
 use base qw(SL::DB::Object);
 
-__PACKAGE__->meta->setup(
-  table   => 'delivery_terms',
+__PACKAGE__->meta->table('delivery_terms');
 
-  columns => [
-    id               => { type => 'integer', not_null => 1, sequence => 'id' },
-    description      => { type => 'text' },
-    description_long => { type => 'text' },
-    sortkey          => { type => 'integer', not_null => 1 },
-    itime            => { type => 'timestamp', default => 'now()' },
-    mtime            => { type => 'timestamp' },
-  ],
-
-  primary_key_columns => [ 'id' ],
-
-  allow_inline_column_values => 1,
+__PACKAGE__->meta->columns(
+  description      => { type => 'text' },
+  description_long => { type => 'text' },
+  id               => { type => 'integer', not_null => 1, sequence => 'id' },
+  itime            => { type => 'timestamp', default => 'now()' },
+  mtime            => { type => 'timestamp' },
+  sortkey          => { type => 'integer', not_null => 1 },
 );
+
+__PACKAGE__->meta->primary_key_columns([ 'id' ]);
+
+__PACKAGE__->meta->allow_inline_column_values(1);
 
 1;
 ;
