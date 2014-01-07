1bc764da7a82ada6469850daeabbbc50b16965f8
Moritz Bunkus <m.bunkus@linet-services.de>
2014-01-07 09:54:03 +0100

    Ordentliche Beschreibung zu DB-Upgrade

--- sql/Pg-upgrade2/delete_close_follow_ups_when_order_is_deleted_closed_fkey_deletion.pl
+++ sql/Pg-upgrade2/delete_close_follow_ups_when_order_is_deleted_closed_fkey_deletion.pl
@@ -1,5 +1,5 @@
 # @tag: delete_close_follow_ups_when_order_is_deleted_closed_fkey_deletion
-# @description: delete_close_follow_ups_when_order_is_deleted_closed_fkey_deletion
+# @description: Wiedervorlagen löschen/schließen, wenn dazugehörige Belege gelöscht/geschlossen werden Teil 1: Fremdschlüssel löschen
 # @depends: release_3_0_0
 package SL::DBUpgrade2::delete_close_follow_ups_when_order_is_deleted_closed_fkey_deletion;
 
