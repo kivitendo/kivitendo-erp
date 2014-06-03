f7c4665fd435b94e599ea1ac2ba23d8e21a8420f
Moritz Bunkus <m.bunkus@linet-services.de>
2014-06-03 17:10:25 +0200

    Test-BackgroundJob: Möglichkeiten, das Ergebnis zu steuern

--- SL/BackgroundJob/Test.pm
+++ SL/BackgroundJob/Test.pm
@@ -5,10 +5,14 @@ use strict;
 use parent qw(SL::BackgroundJob::Base);
 
 sub run {
-  my $self   = shift;
-  my $db_obj = shift;
+  my ($self, $db_obj) = @_;
+  my $data            = $db_obj->data_as_hash;
 
   $::lxdebug->message(0, "Test job is being executed.");
+
+  die "Oh cruel world: " . $data->{exception} if $data->{exception};
+
+  return exists $data->{result} ? $data->{result} : 1;
 }
 
 1;
