dcb372751d2a810aabcb850532a72f3c3133c5f5
Sven Schöling <s.schoeling@linet-services.de>
2014-06-23 13:27:49 +0200

    Angebote/Aufträge drucken: auch das Order-Objekt ausliefern

--- SL/OE.pm
+++ SL/OE.pm
@@ -1387,6 +1387,8 @@ sub order_details {
   $form->{delivery_term} = SL::DB::Manager::DeliveryTerm->find_by(id => $form->{delivery_term_id} || undef);
   $form->{delivery_term}->description_long($form->{delivery_term}->translated_attribute('description_long', $form->{language_id})) if $form->{delivery_term} && $form->{language_id};
 
+  $::form->{order} = SL::DB::Manager::Order->find_by(id => $::form->{id});
+
   $main::lxdebug->leave_sub();
 }
 
