2bdd0bc580210b18c1b76a0d0bd4fc3bd3fceca2
Sven Schöling <s.schoeling@linet-services.de>
2014-07-28 15:40:12 +0200

    PriceSource: pricegroup migration

--- sql/Pg-upgrade2/recorditem_active_price_source.sql
+++ sql/Pg-upgrade2/recorditem_active_price_source.sql
@@ -3,6 +3,10 @@
 -- @depends: release_2_6_2
 -- @encoding: utf-8
 
-ALTER TABLE orderitems ADD COLUMN active_price_source TEXT NOT NULL DEFAULT '';
+ALTER TABLE orderitems           ADD COLUMN active_price_source TEXT NOT NULL DEFAULT '';
 ALTER TABLE delivery_order_items ADD COLUMN active_price_source TEXT NOT NULL DEFAULT '';
-ALTER TABLE invoice ADD COLUMN active_price_source TEXT NOT NULL DEFAULT '';
+ALTER TABLE invoice              ADD COLUMN active_price_source TEXT NOT NULL DEFAULT '';
+
+UPDATE orderitems           SET active_price_source = 'pricegroup/' || pricegroup_id WHERE pricegroup_id > 0;
+UPDATE delivery_order_items SET active_price_source = 'pricegroup/' || pricegroup_id WHERE pricegroup_id > 0;
+UPDATE invoice              SET active_price_source = 'pricegroup/' || pricegroup_id WHERE pricegroup_id > 0;
