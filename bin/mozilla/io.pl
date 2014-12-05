f712349fa9e232714df6621b025863c93c160be9
Sven Schöling <s.schoeling@linet-services.de>
2014-12-05 12:57:20 +0100

    PriceRule: Klassenauflösung bei Einkaufsaufträgen gefixt

--- bin/mozilla/io.pl
+++ bin/mozilla/io.pl
@@ -1973,7 +1973,7 @@ sub _make_record_item {
 sub _make_record {
   my $class = {
     sales_order             => 'Order',
-    purchase_oder           => 'Order',
+    purchase_order          => 'Order',
     sales_quotation         => 'Order',
     request_quotation       => 'Order',
     purchase_delivery_order => 'DeliveryOrder',
