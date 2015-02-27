cb99781448df318ed40db82d6eb82776b88cc985
Jan Büren <jan@kivitendo-premium.de>
2015-02-27 16:45:10 +0100

    Bessere Erklärung für Upgrade-Skript bei DB-Anomalien
    Ausgelagerte Lieferscheine wurden wahrscheinlich, hart gelöscht

--- sql/Pg-upgrade2/warehouse_add_delivery_order_items_stock_id.sql
+++ sql/Pg-upgrade2/warehouse_add_delivery_order_items_stock_id.sql
@@ -1,5 +1,5 @@
 -- @tag: warehouse_add_delivery_order_items_stock_id
--- @description: Constraints für inventory auf delivery_order (dois und do). Ferner sinnvolle Umbenennung zumindestens von einer Spalte (orderitems -> dois)
+-- @description: Constraints für inventory auf delivery_order (dois und do). Ferner sinnvolle Umbenennung zumindestens von einer Spalte (orderitems -> dois). <br><b>Falls die Constraint nicht gesetzt werden kann, kontaktieren Sie einen Dienstleister und / oder löschen sie die Verknüpfung der Warenbewegung mit Lieferschein auf eigene Verantwortung mit: "UPDATE inventory SET oe_id = NULL WHERE oe_id NOT IN (select id from delivery_orders);"<br>Hintergrund: Eingelagerte Lieferscheine können / sollen nicht gelöscht werden, allerdings weist dieser Datenbestand genau diesen Fall auf.</b>
 -- @depends: release_3_1_0
 ALTER TABLE inventory RENAME orderitems_id TO delivery_order_items_stock_id;
 ALTER TABLE inventory ADD CONSTRAINT delivery_order_items_stock_id_fkey FOREIGN KEY (delivery_order_items_stock_id) REFERENCES delivery_order_items_stock (id);
