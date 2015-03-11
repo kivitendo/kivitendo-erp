2470781cb1392906c2dae82036a8b5e764802d27
Jan Büren <jan@kivitendo-premium.de>
2015-03-11 14:02:13 +0100

    Release 3.1.0 gibt es nicht, entsprechend in 3.2.0 gebündelt

--- sql/Pg-upgrade2-auth/release_3_2_0.sql
+++ sql/Pg-upgrade2-auth/release_3_2_0.sql
@@ -1,14 +1,5 @@
 -- @tag: release_3_2_0
--- @description: Abhängigkeitsscript für Release 3.2.0
--- @depends: release_3_1_0
--- delivery_process_value.pl
+-- @description: Abhängigkeitsscript für Release 3.2.0, bzw. vergessene 3.1.0
+-- @depends: release_3_0_0 delivery_process_value clients_webdav foreign_key_constraints_on_delete requirement_spec_rights delivery_plan_rights rights_for_showing_ar_and_ap_transactions productivity_rights clients details_and_report_of_parts
 -- @charset: utf-8
-
--- skripte die bisher drin sind:
---add_api_token.sql          clients.pl   delivery_process_value.pl       password_hashing.sql    release_3_1_0.sql   requirement_spec_rights.pl
---add_batch_printing_to_full_access.sql  clients_webdav.pl  details_and_report_of_parts.pl        productivity_rights.pl  remove_menustyle_v4.sql rights_for_showing_ar_and_ap_transactions.pl
---auth_schema_normalization_1.pl         delivery_plan_rights.pl  foreign_key_constraints_on_delete.pl  release_3_0_0.sql       remove_menustyle_xml.sql  session_content_auto_restore.sql
--- remove_menustyle_v4 remove_menustyle_xml add_batch_printing_to_full_access auth_schema_normalization_1 session_content_auto_restore add_api_token password_hashing
-
-
 
