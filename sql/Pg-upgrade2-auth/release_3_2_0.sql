30c9fd203f9285f08501a1f9e985dcd8c3a79715
Jan Büren <jan@kivitendo-premium.de>
2015-03-11 12:40:53 +0100

    Vergessenes release 3.2.0 skript für auth

---
+++ sql/Pg-upgrade2-auth/release_3_2_0.sql
@@ -0,0 +1,14 @@
+-- @tag: release_3_2_0
+-- @description: Abhängigkeitsscript für Release 3.2.0
+-- @depends: release_3_1_0
+-- delivery_process_value.pl
+-- @charset: utf-8
+
+-- skripte die bisher drin sind:
+--add_api_token.sql          clients.pl   delivery_process_value.pl       password_hashing.sql    release_3_1_0.sql   requirement_spec_rights.pl
+--add_batch_printing_to_full_access.sql  clients_webdav.pl  details_and_report_of_parts.pl        productivity_rights.pl  remove_menustyle_v4.sql rights_for_showing_ar_and_ap_transactions.pl
+--auth_schema_normalization_1.pl         delivery_plan_rights.pl  foreign_key_constraints_on_delete.pl  release_3_0_0.sql       remove_menustyle_xml.sql  session_content_auto_restore.sql
+-- remove_menustyle_v4 remove_menustyle_xml add_batch_printing_to_full_access auth_schema_normalization_1 session_content_auto_restore add_api_token password_hashing
+
+
+
