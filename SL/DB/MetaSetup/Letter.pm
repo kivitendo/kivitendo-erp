b683eb9df58054cc5b3ecb03344dd3c7ff395500
Sven Schöling <s.schoeling@linet-services.de>
2015-05-29 15:32:01 +0200

    RDBO Update: Letter

--- SL/DB/MetaSetup/Letter.pm
+++ SL/DB/MetaSetup/Letter.pm
@@ -9,32 +9,34 @@ use base qw(SL::DB::Object);
 __PACKAGE__->meta->table('letter');
 
 __PACKAGE__->meta->columns(
-  id                => { type => 'integer', not_null => 1, sequence => 'id' },
-  vc_id             => { type => 'integer', not_null => 1 },
-  rcv_name          => { type => 'text' },
-  rcv_contact       => { type => 'text' },
-  rcv_address       => { type => 'text' },
-  rcv_countrycode   => { type => 'text' },
-  rcv_zipcode       => { type => 'text' },
-  rcv_city          => { type => 'text' },
-  letternumber      => { type => 'text' },
-  jobnumber         => { type => 'text' },
-  text_created_for  => { type => 'text' },
-  date              => { type => 'text' },
-  subject           => { type => 'text' },
-  greeting          => { type => 'text' },
   body              => { type => 'text' },
   close             => { type => 'text' },
   company_name      => { type => 'text' },
+  cp_id             => { type => 'integer' },
+  date              => { type => 'date' },
   employee_id       => { type => 'integer' },
   employee_position => { type => 'text' },
+  greeting          => { type => 'text' },
+  id                => { type => 'integer', not_null => 1, sequence => 'id' },
+  intnotes          => { type => 'text' },
+  itime             => { type => 'timestamp', default => 'now()' },
+  jobnumber         => { type => 'text' },
+  letternumber      => { type => 'text' },
+  mtime             => { type => 'timestamp' },
+  page_created_for  => { type => 'text' },
+  rcv_address       => { type => 'text' },
+  rcv_city          => { type => 'text' },
+  rcv_contact       => { type => 'text' },
+  rcv_country       => { type => 'text' },
+  rcv_countrycode   => { type => 'text' },
+  rcv_name          => { type => 'text' },
+  rcv_zipcode       => { type => 'text' },
+  reference         => { type => 'text' },
   salesman_id       => { type => 'integer' },
   salesman_position => { type => 'text' },
-  itime             => { type => 'timestamp', default => 'now()' },
-  mtime             => { type => 'timestamp' },
-  rcv_country       => { type => 'text' },
-  page_created_for  => { type => 'text' },
-  cp_id             => { type => 'integer' },
+  subject           => { type => 'text' },
+  text_created_for  => { type => 'text' },
+  vc_id             => { type => 'integer', not_null => 1 },
 );
 
 __PACKAGE__->meta->primary_key_columns([ 'id' ]);
@@ -42,6 +44,11 @@ __PACKAGE__->meta->primary_key_columns([ 'id' ]);
 __PACKAGE__->meta->allow_inline_column_values(1);
 
 __PACKAGE__->meta->foreign_keys(
+  contact => {
+    class       => 'SL::DB::Contact',
+    key_columns => { cp_id => 'cp_id' },
+  },
+
   employee => {
     class       => 'SL::DB::Employee',
     key_columns => { employee_id => 'id' },
@@ -52,8 +59,6 @@ __PACKAGE__->meta->foreign_keys(
     key_columns => { salesman_id => 'id' },
   },
 );
-
-# __PACKAGE__->meta->initialize;
 
 1;
 ;
