5b26a7ef809bfb77b97f626869086b27312ede87
G. Richardson <information@kivitendo-premium.de>
2015-06-12 14:49:42 +0200

    Rechte für Bankbewegungen in Bankerweiterung setzen
    Als sinnvollen Default erhalten beim Upgrade Gruppen, die schon das
    Recht für "Zahlungseingang, Zahlungsausgang, Kontenabgleich" (cash)
    besitzen, auch alle Rechte für die Bankerweiterung, also die Arbeit mit
    den importierten Bankbewegungen.

---
+++ sql/Pg-upgrade2-auth/bank_transaction_rights.pl
@@ -0,0 +1,26 @@
+# @tag: bank_transaction_rights
+# @description: Setzt das neue Recht die Bankerweiterung zu nutzen (für Gruppen die auch Recht Kontenabgleich haben)
+# @depends: release_3_2_0
+package SL::DBUpgrade2::bank_transaction_rights;
+
+use strict;
+use utf8;
+
+use parent qw(SL::DBUpgrade2::Base);
+
+use SL::DBUtils;
+
+sub run {
+  my ($self) = @_;
+
+  my $groups = $main::auth->read_groups();
+
+  foreach my $group (values %{$groups}) {
+    $group->{rights}->{bank_transaction} = $group->{rights}->{cash};
+    $main::auth->save_group($group);
+  }
+
+  return 1;
+} # end run
+
+1;
