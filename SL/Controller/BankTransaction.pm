0966a8326e5c0d6b46cb4ea836386ef5fca318ec
G. Richardson <information@kivitendo-premium.de>
2015-07-01 18:14:56 +0200

    Skonto - korrekte Variablenprüfung payment_term
    für den Fall, daß kein payment_term bei den ausgewählten Rechnungen gesetzt ist.

--- SL/Controller/BankTransaction.pm
+++ SL/Controller/BankTransaction.pm
@@ -373,7 +373,7 @@ sub action_save_invoices {
 
     foreach my $invoice (@invoices) {
       my $payment_type;
-      if (@{ $skonto_hash->{"$bt_id"} }) {
+      if ( defined $skonto_hash->{"$bt_id"} ) {
         $payment_type = shift(@{ $skonto_hash->{"$bt_id"} });
       } else {
         $payment_type = 'without_skonto';
