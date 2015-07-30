e8c424de675ba308c1eb478403402d5474276c45
G. Richardson <information@kivitendo-premium.de>
2015-07-30 12:42:31 +0200

    ChartPicker: Neue Iconnamen

--- js/autocomplete_chart.js
+++ js/autocomplete_chart.js
@@ -234,7 +234,7 @@ namespace('kivi', function(k){
     var picker = $('<div>');
     $dummy.after(pcont);
     pcont.append(picker);
-    picker.addClass('icon16 crm--search').click(open_dialog);
+    picker.addClass('icon16 search').click(open_dialog);
 
     var cp = {
       real:           function() { return $real },
