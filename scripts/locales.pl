b694861228781e86a952d4e67110201f2ee2cc2a
G. Richardson <information@kivitendo-premium.de>
2015-08-20 18:30:54 +0200

    locales menu Datei glob von <> auf glob() umgestellt
    ab mindestens perl 5.15.7 funktioniert das mit der
    Variableninterpolation, bei 5.14.2 und kleiner noch nicht.

--- scripts/locales.pl
+++ scripts/locales.pl
@@ -44,7 +44,7 @@ my $basedir      = "../..";
 my $locales_dir  = ".";
 my $bindir       = "$basedir/bin/mozilla";
 my @progdirs     = ( "$basedir/SL" );
-my @menufiles    = <"${basedir}/menus/*/*">;
+my @menufiles    = glob("${basedir}/menus/*/*");
 my @javascript_dirs = ($basedir .'/js', $basedir .'/templates/webpages');
 my $javascript_output_dir = $basedir .'/js';
 my $submitsearch = qr/type\s*=\s*[\"\']?submit/i;
