b103ea259f681f2ca5036e7b0915423c2a8ccc7a
Bernd Bleßmann <bernd@kivitendo-premium.de>
2015-09-30 09:23:06 +0200

    FlattenToForm: sellprice und qty beim Formatieren nicht runden …
    Hintergrund: In den Belegmasken (z.B.) Auftrag kann man Menge oder Verkaufspreis
    mit mehr als zwei Nachkommastellen angeben und es wird auch damit gerechnet.
    Funktionen, die FlattenToForm verwenden (autom. Druck wiederkehrender
    Rechnungen / Massendruck) zeigten dann aber in bestimmten Fällen andere Summen
    als die Maske bzw. als in der Datenbank steht.
    
    Beispiele:
    Maske:   qty=10; sellprice=5,256                => linetotal=52,56
    Flatten: qty=10; sellprice=5,256 (runden 5,26)  => linetotal=52,60
    
    Maske:   qty=5,256;               sellprice=10  => linetotal=52,56
    Flatten: qty=5,256 (runden 5,26); sellprice=10  => linetotal=52,60
    
    Dieser commit sorgt dafür, dass qty und sellprice bei flatten_to_form nicht
    gerundet werden.

--- SL/DB/Helper/FlattenToForm.pm
+++ SL/DB/Helper/FlattenToForm.pm
@@ -53,6 +53,7 @@ sub flatten_to_form {
   my $format_amounts = $params{format_amounts} ? 1 : 0;
   my $format_notnull = $params{format_amounts} ? 2 : 0;
   my $format_percent = $params{format_amounts} ? 3 : 0;
+  my $format_noround = $params{format_amounts} ? 4 : 0;
   foreach my $item (@{ $self->items_sorted }) {
     next if _has($item, 'assemblyitem');
 
@@ -63,7 +64,8 @@ sub flatten_to_form {
     _copy($item->part,    $form, '',        "_${idx}", 0,               qw(listprice));
     _copy($item,          $form, '',        "_${idx}", 0,               qw(description project_id ship serialnumber pricegroup_id ordnumber donumber cusordnumber unit
                                                                            subtotal longdescription price_factor_id marge_price_factor approved_sellprice reqdate transdate));
-    _copy($item,          $form, '',        "_${idx}", $format_amounts, qw(qty sellprice marge_total marge_percent lastcost));
+    _copy($item,          $form, '',        "_${idx}", $format_noround, qw(qty sellprice));
+    _copy($item,          $form, '',        "_${idx}", $format_amounts, qw(marge_total marge_percent lastcost));
     _copy($item,          $form, '',        "_${idx}", $format_percent, qw(discount));
     _copy($item->project, $form, 'project', "_${idx}", 0,               qw(number description)) if _has($item, 'project_id');
 
@@ -95,6 +97,7 @@ sub _copy {
   map { $form->{"${prefix}${_}${postfix}"} =                $::form->format_amount(\%::myconfig, $src->$_ * 1, 2)       } @columns if  $format_amounts == 1;
   map { $form->{"${prefix}${_}${postfix}"} = $src->$_ * 1 ? $::form->format_amount(\%::myconfig, $src->$_ * 1, 2) : 0   } @columns if  $format_amounts == 2;
   map { $form->{"${prefix}${_}${postfix}"} = $src->$_ * 1 ? $::form->format_amount(\%::myconfig, $src->$_ * 100, 2) : 0 } @columns if  $format_amounts == 3;
+  map { $form->{"${prefix}${_}${postfix}"} = $src->$_ * 1 ? $::form->format_amount(\%::myconfig, $src->$_ * 1)    : 0   } @columns if  $format_amounts == 4;
 
   return $src;
 }
