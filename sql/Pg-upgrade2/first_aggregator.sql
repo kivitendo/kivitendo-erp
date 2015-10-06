27cc5749aa0a60296c6383f3a60d969b17c278a2
Sven Schöling <s.schoeling@linet-services.de>
2015-10-06 16:04:53 +0200

    neue Aggregatfunktion "FIRST", für Auswertungen

---
+++ sql/Pg-upgrade2/first_aggregator.sql
@@ -0,0 +1,15 @@
+-- @tag: first_aggregator
+-- @description: SQL Aggregat Funktion FIRST
+-- @depends: release_3_0_0
+-- @encoding: utf-8
+
+CREATE OR REPLACE FUNCTION public.first_agg ( anyelement, anyelement )
+RETURNS anyelement LANGUAGE SQL IMMUTABLE STRICT AS $$
+  SELECT $1;
+$$;
+
+CREATE AGGREGATE public.FIRST (
+  sfunc    = public.first_agg,
+  basetype = anyelement,
+  stype    = anyelement
+);
