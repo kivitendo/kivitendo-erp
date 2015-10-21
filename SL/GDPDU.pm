192f024586c5b716a863380e364bf817b968b71a
Sven Schöling <s.schoeling@linet-services.de>
2015-10-21 16:45:00 +0200

    GDPDU: Typo

--- SL/GDPDU.pm
+++ SL/GDPDU.pm
@@ -177,7 +177,7 @@ sub table {
       })
       ->tag('Format', $date_format)
     })
-    ->tag('UTF8'),
+    ->tag('UTF8')
     ->tag('DecimalSymbol', '.')
     ->tag('DigitGroupingSymbol', '|')     # see CAVEATS in documentation
     ->tag('VariableLength', sub { $self
