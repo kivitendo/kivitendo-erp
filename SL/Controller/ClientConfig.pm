b6195db7d8aad54816866b5152961b4709491734
Jan Büren <jan@kivitendo-premium.de>
2015-11-24 08:48:31 +0100

    mininmal Kosmetik

--- SL/Controller/ClientConfig.pm
+++ SL/Controller/ClientConfig.pm
@@ -146,7 +146,7 @@ sub init_all_languages   { SL::DB::Manager::Language->get_all_sorted            
 sub init_all_currencies  { SL::DB::Manager::Currency->get_all_sorted                                                     }
 sub init_all_weightunits { my $unit = SL::DB::Manager::Unit->find_by(name => 'kg'); $unit ? $unit->convertible_units : [] }
 sub init_all_templates   { +{ SL::Template->available_templates }                                                        }
-sub init_h_unit_name     { first { SL::DB::Manager::Unit->find_by(name => $_) } qw(Std h Stunde)                         };
+sub init_h_unit_name     { first { SL::DB::Manager::Unit->find_by(name => $_) } qw(Std h Stunde)                         }
 sub init_all_project_types    { SL::DB::Manager::ProjectType->get_all_sorted                                             }
 sub init_all_project_statuses { SL::DB::Manager::ProjectStatus->get_all_sorted                                           }
 
