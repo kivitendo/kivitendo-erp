6010f8bfd99c2231fa9a0d278b796a02005a8c8d
Jan Büren <jan@kivitendo-premium.de>
2015-11-30 10:42:35 +0100

    Kosmetik

--- SL/OE.pm
+++ SL/OE.pm
@@ -538,7 +538,7 @@ sub save {
       # force new project, if not set yet
       if ($::instance_conf->get_order_always_project && !$form->{"globalproject_id"} && ($form->{type} eq 'sales_order')) {
         require SL::DB::Customer;
-        my $customer = SL::DB::Manager::Customer->find_by( id => $form->{customer_id} );
+        my $customer = SL::DB::Manager::Customer->find_by(id => $form->{customer_id});
         die "Can't find customer" unless $customer;
         my $new_project = SL::DB::Project->new(
           projectnumber     => $form->{ordnumber},
