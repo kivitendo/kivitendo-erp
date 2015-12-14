5b77c5df6b012e57517d21d7ecb89d1511da2fbe
Moritz Bunkus <m.bunkus@linet-services.de>
2015-12-14 12:43:36 +0100

    Rose-Schema für fbf7532 aktualisiert
    Mea culpa :)

--- SL/DB/MetaSetup/Buchungsgruppe.pm
+++ SL/DB/MetaSetup/Buchungsgruppe.pm
@@ -11,11 +11,18 @@ __PACKAGE__->meta->table('buchungsgruppen');
 __PACKAGE__->meta->columns(
   description        => { type => 'text' },
   id                 => { type => 'integer', not_null => 1, sequence => 'id' },
-  inventory_accno_id => { type => 'integer' },
+  inventory_accno_id => { type => 'integer', not_null => 1 },
   sortkey            => { type => 'integer', not_null => 1 },
 );
 
 __PACKAGE__->meta->primary_key_columns([ 'id' ]);
+
+__PACKAGE__->meta->foreign_keys(
+  inventory_accno => {
+    class       => 'SL::DB::Chart',
+    key_columns => { inventory_accno_id => 'id' },
+  },
+);
 
 1;
 ;
