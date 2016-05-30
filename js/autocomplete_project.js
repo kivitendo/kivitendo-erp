48e32abf9b0af914d6bad46ce7a45f37a65d9946
Bernd Bleßmann <bernd@kivitendo-premium.de>
2016-05-30 16:57:14 +0200

    ProjectPicker: Auch auf Paste-Events reagieren.
    Siehe auch commit 2cf7a2cd78d812d917fb4c92f8ee57f6a32f3685
    (PartPicker: Auch auf Paste-Events reagieren.)

--- js/autocomplete_project.js
+++ js/autocomplete_project.js
@@ -116,6 +116,27 @@ namespace('kivi', function(k){
       timer = window.setTimeout(update_results, 100);
     }
 
+    function handle_changed_text(callbacks) {
+      $.ajax({
+        url: 'controller.pl?action=Project/ajax_autocomplete',
+        dataType: "json",
+        data: $.extend( ajax_data($dummy.val()), { prefer_exact: 1 } ),
+        success: function (data) {
+          if (data.length == 1) {
+            set_item(data[0]);
+            if (callbacks && callbacks.match_one) callbacks.match_one(data[0]);
+          } else if (data.length > 1) {
+            state = STATES.UNDEFINED;
+            if (callbacks && callbacks.match_many) callbacks.match_many(data);
+          } else {
+            state = STATES.UNDEFINED;
+            if (callbacks &&callbacks.match_none) callbacks.match_none();
+          }
+          annotate_state();
+        }
+      });
+    };
+
     $dummy.autocomplete({
       source: function(req, rsp) {
         $.ajax($.extend(o, {
@@ -150,26 +171,25 @@ namespace('kivi', function(k){
         } else if (state == STATES.PICKED) {
           return true;
         }
-        if (event.which == KEY.TAB) event.preventDefault();
-        $.ajax({
-          url: 'controller.pl?action=Project/ajax_autocomplete',
-          dataType: "json",
-          data: $.extend( ajax_data($dummy.val()), { prefer_exact: 1 } ),
-          success: function (data) {
-            if (data.length == 1) {
-              set_item(data[0]);
-              if (event.which == KEY.ENTER)
-                $('#update_button').click();
-            } else {
-            }
-            annotate_state();
-          }
-        });
-        if (event.which == KEY.ENTER)
+        if (event.which == KEY.TAB) {
+          event.preventDefault();
+          handle_changed_text();
+        }
+        if (event.which == KEY.ENTER) {
+          handle_changed_text({
+            match_one:  function(){$('#update_button').click();},
+          });
           return false;
+        }
       } else {
         state = STATES.UNDEFINED;
       }
+    });
+
+    $dummy.on('paste', function(){
+      setTimeout(function() {
+        handle_changed_text();
+      }, 1);
     });
 
     $dummy.blur(function(){
