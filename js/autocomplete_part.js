3ca17b1b1db28ea6b1ddbb90b68d6a580f5fee87
Moritz Bunkus <m.bunkus@linet-services.de>
2016-07-04 13:39:09 +0200

    Revert "Partpicker styling"
    This reverts commit b9889576a1229f41edc2c1daf9b649d97fb7fd2e.
    
    Grund: im Chrome (sowohl unter Linux als auch Windows) sieht das
    Ergebnis ziemlich mies und hochgradig verwirrend aus. Im Internet
    Explorer ist die Lupe zusätzlich zu weit unten positioniert (eher ein
    Schönheitsfehler).

--- js/autocomplete_part.js
+++ js/autocomplete_part.js
@@ -237,9 +237,11 @@ namespace('kivi', function(k){
     });
 
     // now add a picker div after the original input
-    var popup_button = $('<span>').addClass('ppp_popup_button');
-    $dummy.after(popup_button);
-    popup_button.click(open_dialog);
+    var pcont  = $('<span>').addClass('position-absolute');
+    var picker = $('<div>');
+    $dummy.after(pcont);
+    pcont.append(picker);
+    picker.addClass('icon16 search').click(open_dialog);
 
     var pp = {
       real:           function() { return $real },
