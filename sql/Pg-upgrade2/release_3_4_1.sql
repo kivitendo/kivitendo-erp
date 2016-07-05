032cc42fe652b8e12f4bce38a54b9a71930a289a
G. Richardson <information@kivitendo-premium.de>
2016-07-05 14:37:38 +0200

    SQL release Upgrade Skripte für 3.4.1

---
+++ sql/Pg-upgrade2/release_3_4_1.sql
@@ -0,0 +1,4 @@
+-- @tag: release_3_4_1
+-- @description: Leeres Script, das alle Upgradescripte bis zum Release 3.4.1 voraussetzt, um ein fest definiertes Schema zu definieren.
+-- @depends: release_3_4_0 add_parts_price_history2 defaults_add_features periodic_invoices_send_email payment_terms_for_invoices delete_from_generic_translations_on_language_deletion inventory_fix_shippingdate_assemblies letter_cleanup defaults_add_quick_search_modules
+
