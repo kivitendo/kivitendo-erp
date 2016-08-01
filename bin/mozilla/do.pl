263971ba6ce308b3f058705d51e265c3577338d6
Jan Büren <jan@kivitendo-premium.de>
2016-08-01 14:54:30 +0200

    Lieferschein-Positions-Rabatt mit Nachkommastellen
    behebt #200. Nachkommastellen werden beim Workflow Lieferschein -> Rechnung
    verschluckt, da ein parse_amount fehlt.

--- bin/mozilla/do.pl
+++ bin/mozilla/do.pl
@@ -837,7 +837,7 @@ sub invoice {
         }
       }
     }
-    map { $form->{"${_}_${i}"} = $form->parse_amount(\%myconfig, $form->{"${_}_${i}"}) if $form->{"${_}_${i}"} } qw(ship qty sellprice lastcost basefactor);
+    map { $form->{"${_}_${i}"} = $form->parse_amount(\%myconfig, $form->{"${_}_${i}"}) if $form->{"${_}_${i}"} } qw(ship qty sellprice lastcost basefactor discount);
     $form->{"donumber_$i"} = $form->{donumber};
     $form->{"converted_from_delivery_order_items_id_$i"} = delete $form->{"delivery_order_items_id_$i"};
   }
