3b249feb1cf3b3dd91dfa3038ef8f694db251988
G. Richardson <information@kivitendo-premium.de>
2016-08-07 22:37:22 +0200

    Assembly - linetotal Helperfunktionen

--- SL/DB/Assembly.pm
+++ SL/DB/Assembly.pm
@@ -12,4 +12,18 @@ __PACKAGE__->meta->make_manager_class;
 
 __PACKAGE__->meta->initialize;
 
+sub linetotal {
+  my ($self) = @_;
+
+  return 0 unless $self->qty > 0 and $self->part->sellprice > 0;
+  return $self->qty * $self->part->sellprice / ( $self->part->price_factor_id ? $self->part->price_factor->factor : 1 );
+}
+
+sub linetotal_lastcost {
+  my ($self) = @_;
+
+  return 0 unless $self->qty > 0 and $self->part->lastcost > 0;
+  return $self->qty * $self->part->lastcost / ( $self->part->price_factor_id ? $self->part->price_factor->factor : 1 );
+}
+
 1;
