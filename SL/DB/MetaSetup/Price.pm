f05d7f16bbb968e602c50b938f4934d5a82f28dc
G. Richardson <information@kivitendo-premium.de>
2016-08-07 22:48:09 +0200

    Tabelle prices - not null und unique auf parts_id und partsgroup_id

--- SL/DB/MetaSetup/Price.pm
+++ SL/DB/MetaSetup/Price.pm
@@ -10,12 +10,14 @@ __PACKAGE__->meta->table('prices');
 
 __PACKAGE__->meta->columns(
   id            => { type => 'serial', not_null => 1 },
-  parts_id      => { type => 'integer' },
+  parts_id      => { type => 'integer', not_null => 1 },
   price         => { type => 'numeric', precision => 15, scale => 5 },
-  pricegroup_id => { type => 'integer' },
+  pricegroup_id => { type => 'integer', not_null => 1 },
 );
 
 __PACKAGE__->meta->primary_key_columns([ 'id' ]);
+
+__PACKAGE__->meta->unique_keys([ 'parts_id', 'pricegroup_id' ]);
 
 __PACKAGE__->meta->foreign_keys(
   parts => {
