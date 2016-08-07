24fae966a6ee03b58dc049129d23b3b8fa288f67
G. Richardson <information@kivitendo-premium.de>
2016-08-07 22:52:27 +0200

    Manager für PartsGroup mit sort_spec

---
+++ SL/DB/Manager/PartsGroup.pm
@@ -0,0 +1,19 @@
+package SL::DB::Manager::PartsGroup;
+
+use strict;
+
+use SL::DB::Helper::Manager;
+use base qw(SL::DB::Helper::Manager);
+
+use SL::DB::Helper::Sorted;
+
+sub object_class { 'SL::DB::PartsGroup' }
+
+__PACKAGE__->make_manager_methods;
+
+sub _sort_spec {
+  return ( default => [ 'partsgroup', 1 ],
+           columns => { SIMPLE => 'ALL' });
+}
+
+1;
