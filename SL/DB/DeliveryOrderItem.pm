201c9038c6c64d91b9be59991b6b6449c33a0d4f
G. Richardson <information@kivitendo-premium.de>
2016-10-17 17:18:30 +0200

    Relationship delivery_order_stock_entries für DeliveryOrderItem

--- SL/DB/DeliveryOrderItem.pm
+++ SL/DB/DeliveryOrderItem.pm
@@ -19,6 +19,17 @@ use SL::DB::Helper::CustomVariables (
 
 __PACKAGE__->meta->make_manager_class;
 
+__PACKAGE__->meta->add_relationship(
+  delivery_order_stock_entries => {
+    type         => 'one to many',
+    class        => 'SL::DB::DeliveryOrderItemsStock',
+    column_map   => { id => 'delivery_order_item_id' },
+    manager_args => {
+      with_objects => [ 'inventory' ]
+    },
+  },
+);
+
 __PACKAGE__->meta->initialize;
 
 __PACKAGE__->configure_acts_as_list(group_by => [qw(delivery_order_id)]);
