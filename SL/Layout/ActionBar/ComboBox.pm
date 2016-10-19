121f8ff14cb83da8be3654a471d8180f657337f1
Sven Schöling <s.schoeling@linet-services.de>
2016-10-19 14:08:45 +0200

    ComboBox: stateful dropdown

--- SL/Layout/ActionBar/ComboBox.pm
+++ SL/Layout/ActionBar/ComboBox.pm
@@ -21,7 +21,7 @@ sub add_actions {
 sub render {
   my ($first, @rest) = @{ $_[0]->parsed_actions };
   $_[0]->p->html_tag('div',
-    $_[0]->p->html_tag('div', $first->render, class => 'layout-actionbar-combobox-head') .
+    $_[0]->p->html_tag('div', $first->render . $_[0]->p->html_tag('span'), class => 'layout-actionbar-combobox-head') .
     $_[0]->p->html_tag('div', join('', map { $_->render } @rest), class => 'layout-actionbar-combobox-list'),
     id    => $_[0]->id,
     class => 'layout-actionbar-combobox',
--- js/kivi.ActionBar.js
+++ js/kivi.ActionBar.js
@@ -2,52 +2,82 @@ namespace('kivi', function(k){
   'use strict';
 
   var CLASSES = {
-    disabled: 'layout-actionbar-action-disabled'
+    active:   'active',
+    actionbar: 'layout-actionbar',
+    disabled: 'layout-actionbar-action-disabled',
+    action:   'layout-actionbar-action',
+    combobox: 'layout-actionbar-combobox',
   }
 
-   k.ActionBarAction = function(e) {
-     var data = $(e).data('action');
-     if (undefined === data) return;
+  k.ActionBarCombobox = function(e) {
+    this.combobox = e;
+    this.head     = e.childNodes[0];
+    this.toggle   = this.head.childNodes[1];
+    this.list     = e.childNodes[0];
+    this.init();
+  }
+  k.ActionBarCombobox.prototype = {
+    init: function() {
+      var obj = this;
+      $(obj.toggle).on('click', function(event){
+        $(obj.combobox).toggleClass(CLASSES.active);
+        event.stopPropagation();
+      });
+    }
+  }
 
-     if (data.disabled) {
-       $(e).addClass(CLASSES.disabled);
-     }
+  k.ActionBarAction = function(e) {
+    var data = $(e).data('action');
+    if (undefined === data) return;
 
-     if (data.call || data.submit) {
-       $(e).click(function(event) {
-         var $hidden, key, func, check;
-         if ($(e).hasClass(CLASSES.disabled)) return;
-         if (data.checks) {
-           for (var i=0; i < data.checks.length; i++) {
-             check = data.checks[i];
-             func = kivi.get_function_by_name(check);
-             if (!func) console.log('Cannot find check function: ' + check);
-             if (!func()) return;
-           }
-         }
-         if (data.confirm && !confirm(data.confirm)) return;
-         if (data.call) {
-           func = kivi.get_function_by_name(data.call[0]);
-           func.apply(document, data.call.slice(1))
-         }
-         if (data.submit) {
-           var form   = data.submit[0];
-           var params = data.submit[1];
-           for (key in params) {
-             $hidden = $('<input type=hidden>')
-             $hidden.attr('name', key)
-             $hidden.attr('value', params[key])
-             $(form).append($hidden)
-           }
-           $(form).submit();
-         }
-       });
-     }
-   }
+    if (data.disabled) {
+      $(e).addClass(CLASSES.disabled);
+    }
+
+    if (data.call || data.submit) {
+      $(e).click(function(event) {
+        var $hidden, key, func, check;
+        if ($(e).hasClass(CLASSES.disabled)) {
+          event.stopPropagation();
+          return;
+        }
+        if (data.checks) {
+          for (var i=0; i < data.checks.length; i++) {
+            check = data.checks[i];
+            func = kivi.get_function_by_name(check);
+            if (!func) console.log('Cannot find check function: ' + check);
+            if (!func()) return;
+          }
+        }
+        if (data.confirm && !confirm(data.confirm)) return;
+        if (data.call) {
+          func = kivi.get_function_by_name(data.call[0]);
+          func.apply(document, data.call.slice(1))
+        }
+        if (data.submit) {
+          var form   = data.submit[0];
+          var params = data.submit[1];
+          for (key in params) {
+            $hidden = $('<input type=hidden>')
+            $hidden.attr('name', key)
+            $hidden.attr('value', params[key])
+            $(form).append($hidden)
+          }
+          $(form).submit();
+        }
+      });
+    }
+  }
 });
 
 $(function(){
   $('div.layout-actionbar .layout-actionbar-action').each(function(_, e) {
-    kivi.ActionBarAction(e);
+    kivi.ActionBarAction(e)
+  });
+  $('div.layout-actionbar-combobox').each(function(_, e) {
+    $(e).data('combobox', new kivi.ActionBarCombobox(e));
+  });
+   $(document).click(function() {
+    $('div.layout-actionbar-combobox').removeClass('active');
   });
 });
