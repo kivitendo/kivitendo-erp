3bed12835acd27f26ff501eb46cd741135cf24f8
Sven Schöling <s.schoeling@linet-services.de>
2016-10-20 11:40:54 +0200

    ActionBar: V3 Layout

--- SL/Layout/V3.pm
+++ SL/Layout/V3.pm
@@ -6,13 +6,17 @@ use parent qw(SL::Layout::Base);
 use SL::Layout::None;
 use SL::Layout::Top;
 use SL::Layout::CssMenu;
+use SL::Layout::ActionBar;
 use SL::Layout::Content;
 
 sub init_sub_layouts {
+  $_[0]->sub_layouts_by_name->{actionbar} = SL::Layout::ActionBar->new;
+
   [
     SL::Layout::None->new,
     SL::Layout::Top->new,
     SL::Layout::CssMenu->new,
+    $_[0]->sub_layouts_by_name->{actionbar},
     SL::Layout::Content->new,
   ]
 }
