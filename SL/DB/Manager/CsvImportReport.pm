9a7376fb5f5a10e0ec12842b19bf76b7b708221d
Bernd Bleßmann <bernd@kivitendo-premium.de>
2017-02-21 11:53:37 +0100

    CsvImportReport: Manager-Methode destroy löscht nicht aus aktiver Sitzung
    Vorher wurden alle Reports bis auf den letzten aus der aktiven Sitzung gelöscht.
    Da aber mit den Reports auch das Profile gelöscht wird und im Profil der
    zufällige Dateiname der temporären Csv-Datei enthalten ist und dieser nach einem
    Test-Import für weitere Test-Importe oder den eigentlichen Import benötigt wird,
    darf dieser Report nicht gelöscht werden.

--- SL/DB/Manager/CsvImportReport.pm
+++ SL/DB/Manager/CsvImportReport.pm
@@ -19,18 +19,6 @@ sub cleanup {
   ]);
 
   $_->destroy for @$objects;
-
-  # get reports for the active session that aren't the latest
-  $objects = $self->get_all(
-    query => [ session_id => $::auth->get_session_id, ],
-    sort_by => [ 'id' ],
-  );
-
-  # skip the last one
-  for (0 .. $#$objects - 1) {
-    $objects->[$_]->destroy;
-  }
 }
 
 1;
-
