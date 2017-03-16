6aeb25c506e856dcabb3d943876a6c8b540c4428
Sven Schöling <s.schoeling@linet-services.de>
2017-03-16 12:53:40 +0100

    SL::X: und ohne typo

--- SL/X.pm
+++ SL/X.pm
@@ -5,7 +5,7 @@ use strict;
 use Exception::Lite qw(declareExceptionClass);
 
 declareExceptionClass('SL::X::FormError');
-declareExceptionClass('SL::X::DBError'                         [ '%s', qw(error) ]);
+declareExceptionClass('SL::X::DBError',                        [ '%s', qw(error) ]);
 declareExceptionClass('SL::X::DBHookError',  'SL::X::DBError', [ '%s hook \'%s\' for object type \'%s\' failed', qw(when hook object_type object) ]);
 declareExceptionClass('SL::X::DBRoseError',  'SL::X::DBError', [ '\'%s\' in object of type \'%s\' occured', qw(error class) ]);
 declareExceptionClass('SL::X::DBUtilsError', 'SL::X::DBError', [ '%s: %s', qw(msg error) ]);
