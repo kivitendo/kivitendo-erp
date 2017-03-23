731ba6d6ca63d90b09268f9f9e698410cf4fc37d
Moritz Bunkus <m.bunkus@linet-services.de>
2017-03-23 13:39:54 +0100

    überflüssiges SQL-Upgrade-Script »parts_add_chart_foreign_keys.sql« entfernt
    Im dem Script werden drei Fremdschlüssel für Spalten angelegt, die im
    nachfolgenden Script »parts_remove_unneeded_fields.sql« gleich gedroppt
    werden. Damit ist das Script überflüssig und sogar schädlich, falls
    jemand noch Datenbankinhalt hat, bei dem die Spalten IDs enthalten, zu
    denen es in den Zieltabellen keine Zeilen mehr gibt.

--- sql/Pg-upgrade2/parts_add_chart_foreign_keys.sql
+++
@@ -1,8 +0,0 @@
--- @tag: parts_add_chart_foreign_keys
--- @description: Warenkonten mit chart-Tabelle per Fremdschlüssel verknüpft.
--- @depends: release_3_4_1
--- @ignore: 0
-
-ALTER TABLE parts ADD FOREIGN KEY (income_accno_id)    REFERENCES chart(id);
-ALTER TABLE parts ADD FOREIGN KEY (inventory_accno_id) REFERENCES chart(id);
-ALTER TABLE parts ADD FOREIGN KEY (expense_accno_id)   REFERENCES chart(id);
