826d79929635c726f7459296fafa7e630c00e72d
Werner Hahn <wh@futureworldsearch.net>
2017-09-25 21:13:15 +0200

    WebshopApi: falsche sql update Abhängigkeit

--- sql/Pg-upgrade2/shopimages_2.sql
+++ sql/Pg-upgrade2/shopimages_2.sql
@@ -1,7 +1,7 @@
 -- @tag:shopimages_2
 -- @description: Umbennung der Spalten für Weite und Breite in die Weite und Breite des orginal Bildes
 -- @charset: UTF-8
--- @depends: release-3.5.0 files shop_parts shopimages
+-- @depends: release-3_5_0 files shop_parts shopimages
 -- @ignore: 0
 
 ALTER TABLE shop_images RENAME thumbnail_width TO org_file_width;
