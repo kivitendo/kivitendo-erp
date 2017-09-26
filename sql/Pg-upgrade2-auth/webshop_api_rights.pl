9f68c5f37f21f68997587d79f2e01369454cec46
Werner Hahn <wh@futureworldsearch.net>
2017-09-26 18:02:27 +0200

    WebshopApi: geloweredcased rückgängig

--- sql/Pg-upgrade2-auth/webshop_api_rights.pl
+++ sql/Pg-upgrade2-auth/webshop_api_rights.pl
@@ -1,25 +1,24 @@
 # @tag: webshop_api_rights
 # @description: Setzt die Rechte Shopconfig, Shopbestellungen, Shopartikel, per Default erlaubt
 # @depends: release_3_5_0
-# @locales: create and edit shopparts
-# @locales: get shoporders
-# @locales: create and edit webshops
-
-package sl::dbupgrade2::auth::webshop_api_rights;
+# @locales: Create and edit shopparts
+# @locales: Get shoporders
+# @locales: Create and edit webshops
+package SL::DBUpgrade2::Auth::webshop_api_rights;
 
 use strict;
 use utf8;
 
-use parent qw(sl::dbupgrade2::base);
+use parent qw(SL::DBUpgrade2::Base);
 
-use sl::dbutils;
+use SL::DBUtils;
 
 sub run {
   my ($self) = @_;
 
-  $self->db_query("insert into auth.master_rights (position, name, description) values ( 550,  'shop_part_edit',   'create and edit shopparts')");
-  $self->db_query("insert into auth.master_rights (position, name, description) values ( 950,  'shop_order',       'get shoporders')");
-  $self->db_query("insert into auth.master_rights (position, name, description) values ( 4300, 'edit_shop_config', 'create and edit webshops')");
+  $self->db_query("INSERT INTO auth.master_rights (position, name, description) VALUES ( 550,  'shop_part_edit',   'Create and edit shopparts')");
+  $self->db_query("INSERT INTO auth.master_rights (position, name, description) VALUES ( 950,  'shop_order',       'Get shoporders')");
+  $self->db_query("INSERT INTO auth.master_rights (position, name, description) VALUES ( 4300, 'edit_shop_config', 'Create and edit webshops')");
 
   my $groups = $main::auth->read_groups();
 
