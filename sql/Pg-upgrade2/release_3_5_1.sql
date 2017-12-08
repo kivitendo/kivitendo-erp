78e743a375eeb5686229dba240ba393c5b768506
Jan Büren <jan@kivitendo-premium.de>
2017-12-08 08:31:34 +0100

    DB-Sammelskript für 3.5.1 Standard

---
+++ sql/Pg-upgrade2/release_3_5_1.sql
@@ -0,0 +1,4 @@
+-- @tag: release_3_5_1
+-- @description: Leeres Script, das alle Upgradescripte bis zum Release 3.5.0 voraussetzt, um ein fest definiertes Schema zu definieren.
+-- @depends: release_3_5_0 shop_3 shop_2 trigram_indices shop_1 trigram_indices_webshop get_shipped_qty_config shop_orders_update_3 shopimages_2 customer_orderlock alter_record_template_tables shopimages_3
+
