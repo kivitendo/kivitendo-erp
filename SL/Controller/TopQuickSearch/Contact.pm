932f289597f4466ab198317b7edaa474ed9ed45d
Sven Schöling <s.schoeling@linet-services.de>
2018-02-14 14:02:48 +0100

    TopQuickSearch::Contacts: direkt auf den Ansprechpartner tab springen

--- SL/Controller/TopQuickSearch/Contact.pm
+++ SL/Controller/TopQuickSearch/Contact.pm
@@ -58,7 +58,7 @@ sub select_autocomplete {
 
   my $contact = SL::DB::Manager::Contact->find_by(cp_id => $::form->{id});
 
-  SL::Controller::CustomerVendor->new->url_for(action => 'edit', id => $contact->cp_cv_id, db => db_for_contact($contact));
+  SL::Controller::CustomerVendor->new->url_for(action => 'edit', id => $contact->cp_cv_id, contact_id => $contact->cp_id, db => db_for_contact($contact), hash => 'contacts');
 }
 
 sub do_search {
