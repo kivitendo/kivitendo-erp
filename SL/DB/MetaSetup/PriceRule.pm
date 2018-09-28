20d7a6ae4fde195c7f449051532ab45483115309
Sven Schöling <s.schoeling@linet-services.de>
2018-09-28 16:00:35 +0200

    PriceRuleMacro: Backend

--- SL/DB/MetaSetup/PriceRule.pm
+++ SL/DB/MetaSetup/PriceRule.pm
@@ -9,19 +9,27 @@ use parent qw(SL::DB::Object);
 __PACKAGE__->meta->table('price_rules');
 
 __PACKAGE__->meta->columns(
-  discount  => { type => 'numeric', precision => 15, scale => 5 },
-  id        => { type => 'serial', not_null => 1 },
-  itime     => { type => 'timestamp' },
-  mtime     => { type => 'timestamp' },
-  name      => { type => 'text' },
-  obsolete  => { type => 'boolean', default => 'false', not_null => 1 },
-  price     => { type => 'numeric', precision => 15, scale => 5 },
-  priority  => { type => 'integer', default => 3, not_null => 1 },
-  reduction => { type => 'numeric', precision => 15, scale => 5 },
-  type      => { type => 'text' },
+  discount            => { type => 'numeric', precision => 15, scale => 5 },
+  id                  => { type => 'serial', not_null => 1 },
+  itime               => { type => 'timestamp' },
+  mtime               => { type => 'timestamp' },
+  name                => { type => 'text' },
+  obsolete            => { type => 'boolean', default => 'false', not_null => 1 },
+  price               => { type => 'numeric', precision => 15, scale => 5 },
+  price_rule_macro_id => { type => 'integer' },
+  priority            => { type => 'integer', default => 3, not_null => 1 },
+  reduction           => { type => 'numeric', precision => 15, scale => 5 },
+  type                => { type => 'text' },
 );
 
 __PACKAGE__->meta->primary_key_columns([ 'id' ]);
+
+__PACKAGE__->meta->foreign_keys(
+  price_rule_macro => {
+    class       => 'SL::DB::PriceRuleMacro',
+    key_columns => { price_rule_macro_id => 'id' },
+  },
+);
 
 1;
 ;
---
+++ sql/Pg-upgrade2/price_rules_macros.sql
@@ -0,0 +1,16 @@
+-- @tag: price_rules_macros
+-- @description:  Struktur für Preisregel Makros
+-- @depends: release_18_09 price_rules
+
+CREATE TABLE price_rule_macros (
+  id               SERIAL PRIMARY KEY,
+  name             TEXT NOT NULL,
+  type             TEXT NOT NULL,
+  priority         INTEGER NOT NULL DEFAULT 3,
+  obsolete         BOOLEAN NOT NULL DEFAULT FALSE,
+  json_definition  JSON NOT NULL,
+  itime            TIMESTAMP,
+  mtime            TIMESTAMP
+);
+
+ALTER TABLE price_rules ADD COLUMN price_rule_macro_id INTEGER REFERENCES price_rule_macros(id);
