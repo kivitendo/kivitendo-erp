a4fd607229f0625738ccf80a25af6879080bbe6b
Sven Schöling <sven.schoeling@opendynamic.de>
2019-05-28 15:56:43 +0200

    PriceRuleMacro: typo in Upgradescript

--- sql/Pg-upgrade2/price_rule_macro_foreign_key.sql
+++ sql/Pg-upgrade2/price_rule_macro_foreign_key.sql
@@ -2,5 +2,5 @@
 -- @description: Preisregeln löschen - Datenbankkonsistenz
 -- @depends: release_19_04 price_rules_macros
 
-ALTER TABLE price_rules DROP CONSTRAINT "price_rules_price_rule_macros_id_fkey";
+ALTER TABLE price_rules DROP CONSTRAINT "price_rules_price_rule_macro_id_fkey";
 ALTER TABLE price_rules ADD FOREIGN KEY (price_rule_macro_id) REFERENCES price_rule_macros(id) ON DELETE CASCADE;
