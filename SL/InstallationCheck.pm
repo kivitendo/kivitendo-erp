00a6bdd7580be1c0cd0bc72eafc9af911ae63ff9
Sven Schöling <sven.schoeling@opendynamic.de>
2019-07-05 19:12:51 +0200

    scripts/console: utf8 Kompatibilität und Doku zu Term::ReadLine::Gnu

--- SL/InstallationCheck.pm
+++ SL/InstallationCheck.pm
@@ -82,6 +82,7 @@ BEGIN {
 @developer_modules = (
   { name => "DBIx::Log4perl",                      url => "http://search.cpan.org/~mjevans/", },
   { name => "Devel::REPL",                         url => "http://search.cpan.org/~doy/",       debian => 'libdevel-repl-perl' },
+  { name => "Term::ReadLine::Gnu",                 url => "http://search.cpan.org/~hayashi/",   debian => 'libterm-readline-gnu-perl' },
   { name => "Log::Log4perl",                       url => "http://search.cpan.org/~mschilli/",  debian => 'liblog-log4perl-perl' },
   { name => "LWP::Simple",                         url => "http://search.cpan.org/~gaas/",      debian => 'libwww-perl', dist_name => 'libwww-perl' },
   { name => "Moose::Role",                         url => "http://search.cpan.org/~doy/",       debian => 'libmoose-perl' },
