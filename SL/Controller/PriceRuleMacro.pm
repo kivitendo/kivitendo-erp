562b0cb09ada74a1d303be39b2d5f54ca3fd3f71
Sven Schöling <sven.schoeling@opendynamic.de>
2019-09-25 13:32:38 +0200

    PriceRuleMaco: Report in subcontroller ausgelagert

--- SL/Controller/PriceRuleMacro.pm
+++ SL/Controller/PriceRuleMacro.pm
@@ -10,20 +10,26 @@ use SL::DB::Pricegroup;
 use SL::Locale::String qw(t8);
 use SL::Helper::Flash qw(flash flash_later);
 use SL::Presenter;
-use SL::ReportGenerator;
-use SL::Controller::Helper::GetModels;
-use SL::Controller::Helper::ReportGenerator;
+use SL::Controller::PriceRuleMacro::Report;
+use SL::Controller::Helper::ReportGenerator ();
 
 use Rose::Object::MakeMethods::Generic (
   scalar                  => [ qw() ],
   'scalar --get_set_init' => [ qw(
     price_rule_macro meta
-    all_price_types models
+    all_price_types
+    report
   ) ],
 );
 
 __PACKAGE__->run_before('check_auth');
 __PACKAGE__->run_before('add_javascript');
+
+use SL::Helper::Object (
+  delegate => [
+    report => [ qw(action_list), @SL::Controller::Helper::ReportGenerator::EXPORT ],
+  ],
+);
 
 sub action_new {
   my ($self) = @_;
@@ -244,92 +250,7 @@ sub action_add_element {
     ->render;
 }
 
-sub action_list {
-  my ($self) = @_;
-
-  $self->make_filter_summary;
-
-  $self->prepare_report;
-
-  $self->report_generator_list_objects(
-    report  => $self->{report},
-    objects => $self->models->get,
-    layout  => !$::form->{inline},
-    header  => !$::form->{inline},
-    options => {
-      action_bar_setup_hook => sub { $self->setup_search_action_bar(report_generator_actions => [ @_ ]) },
-    },
-  );
-}
-
 ### internal
-
-sub prepare_report {
-  my ($self)      = @_;
-
-  my $callback    = $self->models->get_callback;
-
-  my $report      = SL::ReportGenerator->new(\%::myconfig, $::form);
-  $self->{report} = $report;
-
-  my @columns     = qw(name type priority name items);
-  my @sortable    = qw(name type priority name notes      );
-
-  my %column_defs = (
-    name          => { obj_link => sub { $self->url_for(action => 'load', 'price_rule_macro.id' => $_[0]->id, callback => $callback) } },
-    priority      => { sub  => sub { $_[0]->priority_as_text } },
-    obsolete      => { sub  => sub { $_[0]->obsolete_as_bool_yn } },
-    notes         => { sub  => sub { $_[0]->notes } },
-#     items         => { sub  => sub { $_[0]->item_summary } },
-  );
-
-  map { $column_defs{$_}->{text} ||= $::locale->text( $self->models->get_sort_spec->{$_}->{title} ) } keys %column_defs;
-
-  $report->set_options(
-    std_column_visibility => 1,
-    controller_class      => 'PriceRuleMacro',
-    output_format         => 'HTML',
-    title                 => t8('Price Rules'),
-    allow_pdf_export      => !$::form->{inline},
-    allow_csv_export      => !$::form->{inline},
-  );
-  $report->set_columns(%column_defs);
-  $report->set_column_order(@columns);
-  $report->set_export_options(qw(list filter));
-  $report->set_options_from_form;
-  $self->models->disable_plugin('paginated') if $report->{options}{output_format} =~ /^(pdf|csv)$/i;
-  $self->models->get_models_url_params(sub{ map { $_ => $::form->{$_} } qw(inline) });
-  $self->models->set_report_generator_sort_options(report => $report, sortable_columns => \@sortable);
-  $report->set_options(
-    raw_bottom_info_text  => $self->render('price_rule_macro/report_bottom', { output => 0 }),
-    raw_top_info_text     => $self->render('price_rule_macro/report_top', { output => 0 }),
-  );
-}
-
-sub make_filter_summary {
-  my ($self) = @_;
-
-  my $filter = $::form->{filter} || {};
-  my @filter_strings;
-
-  my @filters = (
-    [ $filter->{"name:substr::ilike"}, t8('Name')  ],
-    [ $filter->{"price:number"},       t8('Price') ],
-    [ $filter->{"discount:number"},    t8('Discount') ],
-  );
-
-  for (@filters) {
-    push @filter_strings, "$_->[1]: $_->[0]" if $_->[0];
-  }
-
-#   if ($filter->{has_item_type}) {
-#     push @filter_strings, sprintf "%s: %s", t8('Has item type'), join ', ', map {
-#       SL::DB::Manager::PriceRuleItem->get_type($_)->{description}
-#     } @{ $filter->{has_item_type} || [] };
-#   }
-
-  $self->{filter_summary} = join ', ', @filter_strings;
-}
 
 # todo: make this clean and in model
 sub allowed_elements_for {
@@ -365,27 +286,16 @@ sub init_price_rule_macro {
   my $price_rule_macro = SL::DB::Manager::PriceRuleMacro->find_by_or_create(id => $::form->{price_rule_macro}{id} || 0);
 }
 
+sub init_report {
+  SL::Controller::PriceRuleMacro::Report->new(controller => $_[0]);
+}
+
 sub init_meta {
   SL::DB::PriceRuleMacro->create_definition_meta
 }
 
 sub init_all_price_types {
   [ SL::DB::Manager::PriceRule->all_price_types ]
-}
-
-sub init_models {
-  my ($self) = @_;
-
-  SL::Controller::Helper::GetModels->new(
-    controller => $self,
-    sorted => {
-      name     => t8('Name'),
-      type     => t8('Type'),
-      priority => t8('Priority'),
-      obsolete => t8('Obsolete'),
-      digest    => t8('Rule Details'),
-    },
-  );
 }
 
 sub from_form {
@@ -437,41 +347,6 @@ sub setup_form_action_bar {
         t8('Abort'),
         link => $self->url_for(action => 'list', 'filter.type' => $self->price_rule_macro->type),
       ],
-    );
-  }
-}
-
-sub setup_search_action_bar {
-  my ($self, %params) = @_;
-
-  return if $::form->{inline};
-
-  for my $bar ($::request->layout->get('actionbar')) {
-    $bar->add(
-      action => [
-        t8('Update'),
-        submit    => [ '#search_form', { action => 'PriceRuleMacro/list' } ],
-        accesskey => 'enter',
-        checks    => [ [ 'kivi.validate_form', '#search_form' ] ],
-      ],
-
-      'separator',
-
-      @{ $params{report_generator_actions} },
-
-      combobox => [
-        action => [
-          t8('Add'),
-        ],
-        link => [
-          t8('New Sales Price Rule'),
-          link => $self->url_for(action => 'new', 'price_rule_macro.type' => 'customer', callback => $self->models->get_callback),
-        ],
-        link => [
-          t8('New Purchase Price Rule'),
-          link => $self->url_for(action => 'new', 'price_rule_macro.type' => 'vendor', callback => $self->models->get_callback),
-        ],
-      ], # end of combobox "Add"
     );
   }
 }
