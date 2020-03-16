e47236274c273b267417094489eb8ab5ec492c7a
Bernd Bleßmann <bernd@kivitendo-premium.de>
2020-03-16 18:37:42 +0100

    Belegmasken: weitere Werte f. PrintOptions aus form füllen
    Das ist für Proforma-Rg aus Aufträgen wichtig, denn diese werden nach einem
    erneuten Laden über js gedruckt. Und dazu müssen die vorher eingestellten
    Werte wieder richtig vorhanden sein.
    
    Problem war: Auftrag als Proforma drucken ging erst beim zweiten Drucken.

--- bin/mozilla/io.pl
+++ bin/mozilla/io.pl
@@ -2001,7 +2001,7 @@ sub setup_sales_purchase_print_options {
   $print_form->{printers}  = SL::DB::Manager::Printer->get_all_sorted;
   $print_form->{languages} = SL::DB::Manager::Language->get_all_sorted;
 
-  $print_form->{$_} = $::form->{$_} for qw(type media language_id printer_id storno);
+  $print_form->{$_} = $::form->{$_} for qw(type media language_id printer_id storno formname groupitems);
 
   return SL::Helper::PrintOptions->get_print_options(
     form    => $print_form,
