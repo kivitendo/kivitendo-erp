699b0d4781a9364686355489373843614bca5976
Moritz Bunkus <m.bunkus@linet-services.de>
2020-06-08 15:20:46 +0200

    ZUGFeRD: Modifikationen an Tabelle customer in einem Rutsch

--- sql/Pg-upgrade2/customer_create_zugferd_invoices.sql
+++ sql/Pg-upgrade2/customer_create_zugferd_invoices.sql
@@ -2,11 +2,5 @@
 -- @description: Kundenstammdaten: Einstellungen für ZUGFeRD-Rechnungen
 -- @depends: release_3_5_5
 ALTER TABLE customer
-ADD COLUMN create_zugferd_invoices INTEGER;
-
-UPDATE customer
-SET create_zugferd_invoices = -1;
-
-ALTER TABLE customer
-ALTER COLUMN create_zugferd_invoices SET DEFAULT -1,
-ALTER COLUMN create_zugferd_invoices SET NOT NULL;
+ADD COLUMN create_zugferd_invoices INTEGER
+DEFAULT -1 NOT NULL;
