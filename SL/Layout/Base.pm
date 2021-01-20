5b8c6acbd61239eb07476bcf9e767ee2cfec2921
Sven Schöling <s.schoeling@googlemail.com>
2021-01-20 11:21:22 +0100

    Layout::Base: git_commit_reload_resources config parameter
    Ermöglicht das Ausschalten von git commit Resourcesversionierung das
    bisher immer an war.

--- SL/Layout/Base.pm
+++ SL/Layout/Base.pm
@@ -44,21 +44,27 @@ sub get {
 }
 
 sub init_auto_reload_resources_param {
-  return sprintf('?rand=%d-%d-%d', Time::HiRes::gettimeofday(), int(rand 1000000000000)) if $::lx_office_conf{debug}->{auto_reload_resources};
+  if ($::lx_office_conf{debug}->{auto_reload_resources}) {
+    return sprintf('?rand=%d-%d-%d', Time::HiRes::gettimeofday(), int(rand 1000000000000));
+  }
 
-  my $git_dir = SL::System::Process::exe_dir() . '/.git';
+  if ($::lx_office_conf{debug}{git_commit_reload_resources}) {
+    my $git_dir = SL::System::Process::exe_dir() . '/.git';
 
-  return '' unless -d $git_dir;
+    return '' unless -d $git_dir;
 
-  my $content = eval { scalar(read_file($git_dir . '/HEAD')) };
+    my $content = eval { scalar(read_file($git_dir . '/HEAD')) };
 
-  return '' unless ($content // '') =~ m{\Aref: ([^\r\n]+)};
+    return '' unless ($content // '') =~ m{\Aref: ([^\r\n]+)};
 
-  $content = eval { scalar(read_file($git_dir . '/' . $1)) };
+    $content = eval { scalar(read_file($git_dir . '/' . $1)) };
 
-  return '' unless ($content // '') =~ m{\A([0-9a-fA-F]+)};
+    return '' unless ($content // '') =~ m{\A([0-9a-fA-F]+)};
 
-  return '?rand=' . $1;
+    return '?rand=' . $1;
+  }
+
+  return '';
 }
 
 ##########################################
