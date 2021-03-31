2a2c09d6b2fe0ab52f4ecc3d8f7c9e74ace86130
Moritz Bunkus <m.bunkus@linet.de>
2021-03-31 17:53:25 +0200

    MT940: bei unstrukturiertem Feld 86 das erste Zeichen nicht verschlucken

--- SL/MT940.pm
+++ SL/MT940.pm
@@ -124,7 +124,7 @@ sub parse {
 
       } else {
         # unstructured
-        $transaction{purpose} = substr($line->[0], 5);
+        $transaction{purpose} = substr($line->[0], 4);
       }
 
       $store_transaction->();
