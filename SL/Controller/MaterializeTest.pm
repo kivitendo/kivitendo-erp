c2994c1ac7846f00356146eb8bcf085ea607e231
Sven Schöling <s.schoeling@googlemail.com>
2021-04-23 15:13:43 +0200

    MaterialComponents: modal test page

--- SL/Controller/MaterializeTest.pm
+++ SL/Controller/MaterializeTest.pm
@@ -3,8 +3,12 @@ package SL::Controller::MaterializeTest;
 use strict;
 use parent qw(SL::Controller::Base);
 
-sub action_test {
+sub action_components {
   $_[0]->render("test/components");
+}
+
+sub action_modal {
+  $_[0]->render("test/modal");
 }
 
 1;
