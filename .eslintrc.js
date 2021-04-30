0bf838be2439afa8349d4caf0acf9d5a774f2d37
Sven Schöling <s.schoeling@googlemail.com>
2021-04-30 10:54:33 +0200

    .eslint.rc

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": [
+            "error",
+            {
+                "allow": [
+                    "warn",
+                    "error"
+                ],
+            }
+        ]
+    },
+    "globals": {
+      "namespace": true,
+      "kivi": true
+    },
+};
+
