d9c917a85646df768fc5c921a08fcd1ead15fde5
Jan Büren <jan@kivitendo.de>
2021-06-23 12:50:29 +0200

    Revert "Weiche Mandantenkonfiguration: Untererzeugnisse direkt mitfertigen"
    This reverts commit 033138109231a9737da9b2f0ee7cb5d20413ddbf.

--- sql/Pg-upgrade2/defaults_assembly_level.sql
+++
@@ -1,5 +0,0 @@
--- @tag: defaults_assembly_level
--- @description: Mandantenkonfiguration: Erzeugnis fertigen und alle Untererzeugnisse mitfertigen (standardmäßig deaktiviert)
--- @depends: release_3_5_6_1
-
-ALTER TABLE defaults ADD COLUMN produce_assembly_multiple_levels BOOLEAN DEFAULT FALSE;
