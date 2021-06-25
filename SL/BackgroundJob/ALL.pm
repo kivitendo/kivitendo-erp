01091aa9c5cebaf9b803afc26b8f95c4fa2d35a4
Bernd Bleßmann <bernd@kivitendo-premium.de>
2021-06-25 10:41:41 +0200

    SL::BackgroundJob::ALL.pm für BackgroundJobs aktualisiert

--- SL/BackgroundJob/ALL.pm
+++ SL/BackgroundJob/ALL.pm
@@ -4,10 +4,21 @@ use strict;
 
 use SL::BackgroundJob::Base;
 use SL::BackgroundJob::BackgroundJobCleanup;
+use SL::BackgroundJob::CleanAuthSessions;
 use SL::BackgroundJob::CleanBackgroundJobHistory;
 use SL::BackgroundJob::CloseProjectsBelongingToClosedSalesOrders;
 use SL::BackgroundJob::ConvertTimeRecordings;
 use SL::BackgroundJob::CreatePeriodicInvoices;
+use SL::BackgroundJob::CsvImport;
 use SL::BackgroundJob::FailedBackgroundJobsReport;
+use SL::BackgroundJob::MassDeliveryOrderPrinting;
+use SL::BackgroundJob::MassRecordCreationAndPrinting;
+use SL::BackgroundJob::SelfTest;
+use SL::BackgroundJob::SelfTest::Base;
+use SL::BackgroundJob::SelfTest::Transactions;
+use SL::BackgroundJob::SetNumberRange;
+use SL::BackgroundJob::ShopOrderMassTransfer;
+use SL::BackgroundJob::ShopPartMassUpload;
+use SL::BackgroundJob::Test;
 
 1;
