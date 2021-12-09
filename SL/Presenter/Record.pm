4a05bb49ee75a5adfbf67f71c17b89940c9d1fcf
Tamino Steinert <tamino.steinert@tamino.st>
2021-12-09 15:05:45 +0100

    Reclamation: added to RecordLinks

--- SL/Presenter/Record.pm
+++ SL/Presenter/Record.pm
@@ -49,6 +49,7 @@ sub grouped_record_list {
   $output .= _sales_order_list(            $groups{sales_orders},             %params) if $groups{sales_orders};
   $output .= _sales_delivery_order_list(   $groups{sales_delivery_orders},    %params) if $groups{sales_delivery_orders};
   $output .= _rma_delivery_order_list(     $groups{rma_delivery_orders},      %params) if $groups{rma_delivery_orders};
+  $output .= _sales_reclamation_list(      $groups{sales_reclamation},        %params) if $groups{sales_reclamation};
   $output .= _sales_invoice_list(          $groups{sales_invoices},           %params) if $groups{sales_invoices};
   $output .= _ar_transaction_list(         $groups{ar_transactions},          %params) if $groups{ar_transactions};
 
@@ -56,6 +57,7 @@ sub grouped_record_list {
   $output .= _purchase_order_list(         $groups{purchase_orders},          %params) if $groups{purchase_orders};
   $output .= _purchase_delivery_order_list($groups{purchase_delivery_orders}, %params) if $groups{purchase_delivery_orders};
   $output .= _supplier_delivery_order_list($groups{supplier_delivery_orders}, %params) if $groups{supplier_delivery_orders};
+  $output .= _purchase_reclamation_list(   $groups{purchase_reclamation},     %params) if $groups{purchase_reclamation};
   $output .= _purchase_invoice_list(       $groups{purchase_invoices},        %params) if $groups{purchase_invoices};
   $output .= _ap_transaction_list(         $groups{ap_transactions},          %params) if $groups{ap_transactions};
 
@@ -189,12 +191,14 @@ sub _group_records {
     sales_orders             => sub { (ref($_[0]) eq 'SL::DB::Order')           &&  $_[0]->is_type('sales_order')       },
     sales_delivery_orders    => sub { (ref($_[0]) eq 'SL::DB::DeliveryOrder')   &&  $_[0]->is_type('sales_delivery_order') },
     rma_delivery_orders      => sub { (ref($_[0]) eq 'SL::DB::DeliveryOrder')   &&  $_[0]->is_type('rma_delivery_order')   },
+    sales_reclamation        => sub { (ref($_[0]) eq 'SL::DB::Reclamation')     &&  $_[0]->is_type('sales_reclamation') },
     sales_invoices           => sub { (ref($_[0]) eq 'SL::DB::Invoice')         &&  $_[0]->invoice                      },
     ar_transactions          => sub { (ref($_[0]) eq 'SL::DB::Invoice')         && !$_[0]->invoice                      },
     purchase_quotations      => sub { (ref($_[0]) eq 'SL::DB::Order')           &&  $_[0]->is_type('request_quotation') },
     purchase_orders          => sub { (ref($_[0]) eq 'SL::DB::Order')           &&  $_[0]->is_type('purchase_order')    },
     purchase_delivery_orders => sub { (ref($_[0]) eq 'SL::DB::DeliveryOrder')   &&  $_[0]->is_type('purchase_delivery_order') },
     supplier_delivery_orders => sub { (ref($_[0]) eq 'SL::DB::DeliveryOrder')   &&  $_[0]->is_type('supplier_delivery_order') },
+    purchase_reclamation     => sub { (ref($_[0]) eq 'SL::DB::Reclamation')     &&  $_[0]->is_type('purchase_reclamation')},
     purchase_invoices        => sub { (ref($_[0]) eq 'SL::DB::PurchaseInvoice') &&  $_[0]->invoice                      },
     ap_transactions          => sub { (ref($_[0]) eq 'SL::DB::PurchaseInvoice') && !$_[0]->invoice                      },
     sepa_collections         => sub { (ref($_[0]) eq 'SL::DB::SepaExportItem')  &&  $_[0]->ar_id                        },
@@ -428,6 +432,26 @@ sub _supplier_delivery_order_list {
       [ $::locale->text('Project'),                 'globalproject', ],
       [ $::locale->text('Delivered'),               'delivered'                                                                ],
       [ $::locale->text('Closed'),                  'closed'                                                                   ],
+    ],
+    %params,
+  );
+}
+
+sub _sales_reclamation_list {
+  my ($list, %params) = @_;
+
+  return record_list(
+    $list,
+    title   => $::locale->text('Sales Reclamation'),
+    type    => 'sales_reclamation',
+    columns => [
+      [ $::locale->text('Reclamation Date'),        'transdate'                                                          ],
+      [ $::locale->text('Reclamation Number'),      sub { $_[0]->presenter->sales_reclamation(display => 'table-cell') } ],
+      [ $::locale->text('Customer'),                'customer'                                                           ],
+      [ $::locale->text('Transaction description'), 'transaction_description'                                            ],
+      [ $::locale->text('Project'),                 'globalproject',                                                     ],
+      [ $::locale->text('Delivered'),               'delivered'                                                          ],
+      [ $::locale->text('Closed'),                  'closed'                                                             ],
     ],
     %params,
   );
@@ -712,6 +736,8 @@ The order in which the records are grouped is:
 
 =item * rma delivery orders
 
+=item * sales reclamation
+
 =item * sales invoices
 
 =item * AR transactions
@@ -723,6 +749,8 @@ The order in which the records are grouped is:
 =item * purchase delivery orders
 
 =item * supplier delivery orders
+
+=item * purchase reclamation
 
 =item * purchase invoices
 
