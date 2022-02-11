8b3f1910b40a6e282302e32c635ca8b2e880e4c8
Jan Büren <jan@kivitendo.de>
2022-02-11 13:11:08 +0100

    advance_payment_transfer_charts: Ergänzung Abhängigkeit und chart.link setzen

--- sql/Pg-upgrade2/defaults_advance_payment_transfer_charts.sql
+++ sql/Pg-upgrade2/defaults_advance_payment_transfer_charts.sql
@@ -1,10 +1,12 @@
 -- @tag: defaults_advance_payment_transfer_charts
 -- @description: Standardkonten für erhaltene versteuerte Anzahlungen 7% und 19% setzen
--- @depends:new_chart_3260_1711 new_chart_3272_1718
+-- @depends:new_chart_3260_1711 new_chart_3272_1718 defaults_advance_payment_clearing_chart_id
 
 
 ALTER TABLE defaults ADD COLUMN advance_payment_taxable_19_id INTEGER;
 ALTER TABLE defaults ADD COLUMN advance_payment_taxable_7_id  INTEGER;
+UPDATE charts set link ='AR' where id = (select advance_payment_clearing_chart_id from defaults);
+
 
 DO $$
 BEGIN
