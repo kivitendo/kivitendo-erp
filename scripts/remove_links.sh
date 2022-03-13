a4083eb7ebd7a010c156bebd06a9fc472f44080b
Jan Helbling <jh@jan-helbling.ch>
2022-03-13 18:37:27 +0100

    Scripts um symlinks zu erstellen und entfernen erstellt.

---
+++ scripts/remove_links.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+rm ar.pl
+rm am.pl
+rm bp.pl
+rm login.pl
+rm todo.pl
+rm gl.pl
+rm ir.pl
+rm ct.pl
+rm dn.pl
+rm do.pl
+rm oe.pl
+rm wh.pl
+rm ap.pl
+rm rp.pl
+rm ca.pl
+rm ustva.pl
+rm is.pl
+rm amtemplates.pl
+rm ic.pl
