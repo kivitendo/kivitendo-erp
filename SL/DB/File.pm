7718459cd0be728a57d7ca75dd8077316df7d730
Bernd Bleßmann <bernd@kivitendo-premium.de>
2022-04-06 11:22:01 +0200

    Volltext-Suche: Rose-Beziehung von File zu full_text

--- SL/DB/File.pm
+++ SL/DB/File.pm
@@ -8,6 +8,14 @@ use strict;
 use SL::DB::MetaSetup::File;
 use SL::DB::Manager::File;
 
+__PACKAGE__->meta->add_relationship(
+  full_text            => {
+    type               => 'one to one',
+    class              => 'SL::DB::FileFullText',
+    column_map         => { id => 'file_id' },
+  },
+);
+
 __PACKAGE__->meta->initialize;
 
 
