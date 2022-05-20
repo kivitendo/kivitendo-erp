55962438cef1e1b0030483c001344644a5c53a56
Jan Büren <jan@kivitendo.de>
2022-05-20 14:58:28 +0200

    ShopConnector: Vorgangsbezeichnung geht auch mit Shopware6

--- SL/DB/Shop.pm
+++ SL/DB/Shop.pm
@@ -29,7 +29,6 @@ sub validate {
 
   # not yet implemented checks
   if ($self->{connector} eq 'shopware6') {
-    push @errors, $::locale->text('Transaction Description is not yet implemented')  if $self->{transaction_description};
     push @errors, $::locale->text('Shipping cost article is not implemented')        if $self->{shipping_costs_parts_id};
     push @errors, $::locale->text('Fetch from last order number is not implemented') if $self->{last_order_number};
   } else {
