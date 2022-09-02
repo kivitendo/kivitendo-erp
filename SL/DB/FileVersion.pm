6e0c953b84ca7b63c65ff1432ce35c83c8a24cf0
Jan Büren <jan@kivitendo.de>
2022-09-02 09:44:39 +0200

    FileVersion: Funktion für Test und Speicherort im Dateisystem

--- SL/DB/FileVersion.pm
+++ SL/DB/FileVersion.pm
@@ -10,4 +10,52 @@ use SL::DB::Manager::FileVersion;
 
 __PACKAGE__->meta->initialize;
 
+
+sub get_system_location {
+  my ($self) = @_;
+
+  my $filesystem_file = $self->doc_path . $self->file_location;
+
+  die "Invalid state, file has vanished at: $filesystem_file" unless -f $filesystem_file;
+
+  return $filesystem_file;
+
+}
+
+sub file_name {
+  my ($self) = @_;
+
+  return $self->file->file_name;
+
+}
+
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+SL::DB::FileVersion
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item C<get_system_location>
+
+Returns the filesystem's file location for this exact version.
+Dies if no plain file exists at the expected location.
+
+=item C<file_name>
+
+Shortcut for $self->file->file_name.
+
+
+=back
+
+=head1 AUTHOR
+
+Jan E<lt>jan@kivitendo.deE<gt>
+=cut
