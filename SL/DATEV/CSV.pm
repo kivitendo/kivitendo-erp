dd69eacf5323dc7be1efcb40a46e93977b07595d
Jan Büren <jan@echinacea.es>
2022-09-05 12:20:01 +0200

    DATEV: Belegexport -> Eine Buchung darf mehrere Belege haben und ...
    Filter für nicht Buchungsdokumente (trans_id ist nur in ar, ap und gl
    eindeutig).

--- SL/DATEV/CSV.pm
+++ SL/DATEV/CSV.pm
@@ -160,8 +160,20 @@ my @kivitendo_to_datev = (
                                                       # "8DB85C02-4CC3-FF3E-06D7-7F87EEECCF3A".
                               type            => 'Text',
                               default         => '',
-                              input_check     => sub { my ($check) = @_; return 1 unless $check; return ($check =~ m/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/) },
-                              formatter       => sub { my ($input) = @_; return '' unless $input; return 'BEDI "' . $input . '"' },
+                              input_check     => sub { my ($check) = @_; return 1 unless $check;
+                                                       my @guids = split(/,/,$check);
+                                                       foreach my $guid (@guids) {
+                                                         return unless ($guid =~ m/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/);
+                                                       }
+                                                       return 1; },
+                              formatter       => sub { my ($input) = @_; return '' unless $input;
+                                                       my @guids = split (/,/,$input);
+                                                       my $first = shift @guids;
+                                                       my $bedi = 'BEDI "' . $first . '"';
+                                                       foreach my $guid (@guids) {
+                                                         $bedi .= ',"' . $guid . '"';
+                                                       }
+                                                       return $bedi; },
 
                             }, # pos 20
                             {
