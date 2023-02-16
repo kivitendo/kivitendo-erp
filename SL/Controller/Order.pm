5a48d9e5f474c08d12bfdd262721148bd066d9b4
Bernd Bleßmann <bernd@kivitendo-premium.de>
2023-02-16 17:37:47 +0100

    Order-Controller: Kosmetik: Ausrichtung

--- SL/Controller/Order.pm
+++ SL/Controller/Order.pm
@@ -2141,10 +2141,10 @@ sub save {
                        : undef;
 
   SL::Model::Record->save($self->order,
-                          with_validity_token        => { scope => SL::DB::ValidityToken::SCOPE_ORDER_SAVE(), token => $::form->{form_validity_token} },
-                          delete_custom_shipto       => $self->is_custom_shipto_to_delete || $self->order->custom_shipto->is_empty,
-                          items_to_delete            => $items_to_delete,
-                          objects_to_close           => $objects_to_close,
+                          with_validity_token  => { scope => SL::DB::ValidityToken::SCOPE_ORDER_SAVE(), token => $::form->{form_validity_token} },
+                          delete_custom_shipto => $self->is_custom_shipto_to_delete || $self->order->custom_shipto->is_empty,
+                          items_to_delete      => $items_to_delete,
+                          objects_to_close     => $objects_to_close,
                           link_requirement_specs_linking_to_created_from_objects => \@converted_from_oe_ids,
                           set_project_in_linked_requirement_specs                => 1,
   );
