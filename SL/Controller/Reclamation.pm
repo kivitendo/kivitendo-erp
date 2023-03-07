449fa8e37a0e137f6cf499e6d7603fceb4a7e4fb
Tamino Steinert <tamino.steinert@tamino.st>
2023-03-07 10:22:10 +0100

    Model::Record: Entferne TypedataProxy
    Auf Typedata kann jetzt immer über den Belege zugegriffen werden.

--- SL/Controller/Reclamation.pm
+++ SL/Controller/Reclamation.pm
@@ -101,7 +101,7 @@ __PACKAGE__->run_before('get_record_links_data_from_form',
 sub action_add {
   my ($self) = @_;
 
-  $self->reclamation(SL::Model::Record->update_after_new($self->reclamation, $self->type));
+  $self->reclamation(SL::Model::Record->update_after_new($self->reclamation));
 
   $self->pre_render();
 
