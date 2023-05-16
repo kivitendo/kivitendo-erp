15b0ea3dff35030e8445fce2f487adf329599713
Jan Büren <jan@kivitendo.de>
2023-05-16 07:59:25 +0200

    SQL-Abhängigkeit auf aktuellen Master gesetzt

--- sql/Pg-upgrade2/defaults_transit_items_account.sql
+++ sql/Pg-upgrade2/defaults_transit_items_account.sql
@@ -1,6 +1,6 @@
 -- @tag: defaults_transit_items_account
 -- @description: Voreingestelltes Konto für Durchlaufende Posten
--- @depends: release_3_7_0
+-- @depends: release_3_8_0
 
 ALTER TABLE defaults ADD COLUMN transit_items_chart_id INTEGER;
 UPDATE defaults set transit_items_chart_id = (select id from chart where description='Durchlaufende Posten' LIMIT 1);
