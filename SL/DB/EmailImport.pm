786bfd7a68e02370d75055852c49791884f17de8
Tamino Steinert <tamino.steinert@tamino.st>
2023-07-05 14:52:46 +0200

    EmailSync: EmailJournal erweitert und EmailImport hinzugefügt

---
+++ SL/DB/EmailImport.pm
@@ -0,0 +1,18 @@
+package SL::DB::EmailImport;
+
+use strict;
+
+use SL::DB::MetaSetup::EmailImport;
+use SL::DB::Manager::EmailImport;
+
+__PACKAGE__->meta->add_relationship(
+  email_journal => {
+    type      => 'one to many',
+    class     => 'SL::DB::EmailJournal',
+    column_map => { id => 'email_import_id' },
+  },
+);
+
+__PACKAGE__->meta->initialize;
+
+1;
