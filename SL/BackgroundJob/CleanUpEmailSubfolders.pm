309034fe2d7a1d2d2cf77a81693fdb2fd7b564f2
Tamino Steinert <tamino.steinert@tamino.st>
2023-08-03 10:28:56 +0200

    BJ zum Belege synchronisieren angepasst, auch EmailJournal nutzen
    Alle Emails in Unterordnern werden synchronisiert und Emaildateien zu
    den Belegen gespeichert. Alle unter Ordner, die nicht zu offenen Belegen
    gehören, werden gelöscht.

---
+++ SL/BackgroundJob/CleanUpEmailSubfolders.pm
@@ -0,0 +1,61 @@
+package SL::BackgroundJob::CleanUpEmailSubfolders;
+
+use strict;
+use warnings;
+
+use parent qw(SL::BackgroundJob::Base);
+
+use SL::IMAPClient;
+
+sub clean_up_subfolders {
+  my ($self) = @_;
+  my $imap_client = SL::IMAPClient->new();
+
+  my $open_sales_orders = SL::DB::Manager::Order->get_all(
+    query => [
+      vendor_id => undef,
+      closed => 0,
+    ],
+  );
+
+  my $email_import = $imap_client->clean_up_subfolders($open_sales_orders);
+  return unless $email_import;
+
+  $self->{job_obj}->set_data(last_email_import_id => $email_import->id)->save;
+}
+
+sub run {
+  my ($self, $job_obj) = @_;
+  $self->{job_obj} = $job_obj;
+
+  $self->clean_up_subfolders();
+
+  return;
+}
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+SL::BackgroundJob::CleanUpEmailSubfolders - Background job for removing all email
+subfolders except open records.
+
+=head1 SYNOPSIS
+
+This background job syncs all emails in subfolders and adds emails files to the
+corresponding record. After that is removes all subfolders except for open
+records.
+
+=head1 BUGS
+
+Nothing here yet.
+
+=head1 AUTHOR
+
+Tamino Steinert E<lt>tamino.steinert@tamino.stE<gt>
+
+=cut
