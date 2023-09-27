95f4c39629ba52eaf992d896de96f7eeef25eb5f
Sven Schöling <s.schoeling@googlemail.com>
2023-09-27 14:34:32 +0200

    ckeditor5: alten ckeditor entfernt

--- js/ckeditor/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
-
-CKEDITOR.editorConfig = function( config ) {
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
-};
