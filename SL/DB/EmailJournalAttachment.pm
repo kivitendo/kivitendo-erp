0196da3aed48abfc04711c48eb2cd9d6fa609a51
Tamino Steinert <tamino.steinert@tamino.st>
2023-10-12 14:45:36 +0200

    EmailJournalAttachment: FIX: entferne nicht genutzten Import

--- SL/DB/EmailJournalAttachment.pm
+++ SL/DB/EmailJournalAttachment.pm
@@ -2,7 +2,6 @@ package SL::DB::EmailJournalAttachment;
 
 use strict;
 
-use SL::DB::PurchaseInvoice;
 use SL::DB::MetaSetup::EmailJournalAttachment;
 use SL::DB::Manager::EmailJournalAttachment;
 use SL::DB::Helper::ActsAsList (group_by => [ qw(email_journal_id) ]);
