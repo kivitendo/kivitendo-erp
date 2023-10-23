9d44ec0211cadf8ebc25c72289686b67c6d6bc19
Bernd Bleßmann <bernd@kivitendo-premium.de>
2023-10-23 15:37:57 +0200

    EK-Rechnung: Änderung EK-Preis Preisfaktor berücksichtigen auch bei keinem inventory_accno
    Und Erzeugnisse/Sortimente auch updaten.
    Und nur aktualisieren, wenn geändert.
    Genau wie in dem if-Zweig, wo es ein inventroy_accno gibt.
    
    commit 6b8e3b8f9763a6a92c713092e232d8157ea4e772
    Author: Bernd Bleßmann <bernd@kivitendo-premium.de>
    Date:   Fri Oct 13 15:06:27 2023 +0200
    
        EK-Rechnung:

--- SL/IR.pm
+++ SL/IR.pm
@@ -422,9 +422,14 @@ sub _post_invoice {
 
       next if $payments_only;
 
-      # update lastcost
-      $query = qq|UPDATE parts SET lastcost = ? WHERE id = ?|;
-      do_query($form, $dbh, $query, $form->{"sellprice_$i"} / $basefactor, conv_i($form->{"id_$i"}));
+      # change lastcost for part and all assemblies and assortments recursively
+      my $a = SL::DB::Part->load_cached(conv_i($form->{"id_$i"}));
+      my $part_price_factor = $a->price_factor_id ? $a->price_factor->factor : 1;
+      my $new_lastcost      = abs($fxsellprice * $form->{exchangerate} / $basefactor / $price_factor * $part_price_factor);
+      if ( abs($a->lastcost - $new_lastcost) >= 0.009 ) {
+        $a->update_attributes(lastcost => $new_lastcost);
+        $a->set_lastcost_assemblies_and_assortiments;
+      }
     }
 
     next if $payments_only;
