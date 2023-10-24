7289518a4d918a1855f552a57e582f8c5b94f03e
Bernd Bleßmann <bernd@kivitendo-premium.de>
2023-10-24 13:21:25 +0200

    Lieferantenauftragsbestätigung: DB-Type order_types

---
+++ sql/Pg-upgrade2/oe_purchase_order_confirmation_order_types.sql
@@ -0,0 +1,5 @@
+-- @tag: oe_purchase_order_confirmation_order_types
+-- @description: order_types-Eintrag für Lieferantenauftragsbesätigung (purchase_order_confirmation)
+-- @depends: order_type
+
+ALTER TYPE order_types ADD VALUE IF NOT EXISTS 'purchase_order_confirmation';
