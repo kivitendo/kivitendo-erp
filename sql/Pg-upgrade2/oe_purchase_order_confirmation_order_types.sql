7b9155095173ec97b60c475157ff537f5047d7f8
Bernd Bleßmann <bernd@kivitendo-premium.de>
2023-11-24 16:04:45 +0100

    Lieferantenauftragsbestätigung: DB-Upgrade-Skript bekommt ignore-Steuerzeile.
    Damit kann dann einfacher mit einem sed-Kommando das Skript auf
    "ignorieren" gesetzt werden (siehe UPGRADE).

--- sql/Pg-upgrade2/oe_purchase_order_confirmation_order_types.sql
+++ sql/Pg-upgrade2/oe_purchase_order_confirmation_order_types.sql
@@ -1,5 +1,6 @@
 -- @tag: oe_purchase_order_confirmation_order_types
 -- @description: order_types-Eintrag für Lieferantenauftragsbesätigung (purchase_order_confirmation)
 -- @depends: order_type
+-- @ignore: 0
 
 ALTER TYPE order_types ADD VALUE IF NOT EXISTS 'purchase_order_confirmation';
