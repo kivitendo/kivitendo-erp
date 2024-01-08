daa95513162cbde748d7d29fbe5d0f8ef1cc7e8f
Tamino Steinert <tamino.steinert@tamino.st>
2024-01-08 10:45:52 +0100

    Varianten: einfachen Artikel in Variante umwandeln

--- SL/Presenter/Part.pm
+++ SL/Presenter/Part.pm
@@ -301,6 +301,10 @@ If C<%params> contains C<part_type> only parts of this type will be used
 for autocompletion. You may comma separate multiple types as in
 C<part,assembly>.
 
+If C<%params> contains C<variant_type> only parts of this variant type will be
+used for autocompletion. You may comma separate multiple variant types as in
+C<single,variant>.
+
 If C<%params> contains C<status> only parts of this status will be used
 for autocompletion. C<status> can be one of the following strings:
 C<active>, C<obsolete> or C<all>. C<active> is the default if C<status> is
