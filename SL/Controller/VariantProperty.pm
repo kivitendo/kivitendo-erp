a442061885d528d333a2b6d0a76346c2dbcae120
Tamino Steinert <tamino.steinert@tamino.st>
2024-01-24 17:18:34 +0100

    Varianten: Bearbeiten von Varianten Eigenschaften und Ausprägungen

--- SL/Controller/VariantProperty.pm
+++ SL/Controller/VariantProperty.pm
@@ -10,17 +10,17 @@ use SL::DB::Default;
 use SL::DB::Manager::VariantProperty;
 
 use Rose::Object::MakeMethods::Generic (
-  'scalar --get_set_init' => [ qw(variant_property variant_property_value) ]
+  'scalar --get_set_init' => [ qw(variant_property) ]
 );
 
 #__PACKAGE__->run_before('check_auth');
-__PACKAGE__->run_before('add_javascripts', only => [ qw(edit_property) ]);
+__PACKAGE__->run_before('add_javascripts', only => [ qw(edit_variant_property) ]);
 
 #
 # actions
 #
 
-sub action_list_properties {
+sub action_list_variant_properties {
   my ($self) = @_;
 
   $self->_setup_list_action_bar;
@@ -30,92 +30,114 @@ sub action_list_properties {
                );
 }
 
-sub action_edit_property {
+sub action_edit_variant_property {
   my ($self) = @_;
 
   my $is_new = !$self->variant_property->id;
   $self->_setup_form_action_bar;
-  $self->render('variant_property/variant_property_form', title => ($is_new ? t8('Add Variant Property') : t8('Edit Variant Property')));
+  $self->render(
+    'variant_property/variant_property_form',
+    title => ($is_new ? t8('Add Variant Property') : t8('Edit Variant Property')),
+  );
 }
 
-sub action_save_property {
+sub action_save_variant_property {
   my ($self) = @_;
 
-  $self->create_or_update_property;
+  $self->create_or_update_variant_property;
 }
 
-sub action_delete_property {
+sub action_delete_variant_property {
   my ($self) = @_;
 
-  if ( eval { $self->shop->delete; 1; } ) {
-    flash_later('info',  $::locale->text('The shop has been deleted.'));
+  if ( eval {
+      SL::DB->client->with_transaction(sub {
+        $_->delete for $self->variant_property->property_values;
+        $self->variant_property->delete;
+      });
+      1;
+    } ) {
+    flash_later('info',  t8('The Variant Property has been deleted.'));
   } else {
-    flash_later('error', $::locale->text('The shop is in use and cannot be deleted.'));
+    flash_later('error', t8('The Variant Property is in use and cannot be deleted.'));
   };
-  $self->redirect_to(action => 'list_properties');
+  $self->redirect_to(action => 'list_variant_properties');
 }
 
-sub action_reorder_properties {
+sub action_reorder_variant_properties {
   my ($self) = @_;
 
   SL::DB::VariantProperty->reorder_list(@{ $::form->{variant_property_id} || [] });
   $self->render(\'', { type => 'json' }); # ' emacs happy again
 }
 
-sub action_edit_property_value {
+sub action_reorder_variant_property_values {
   my ($self) = @_;
 
-  $self->render_variant_edit_dialog();
+  SL::DB::VariantPropertyValue->reorder_list(@{ $::form->{variant_property_value_id} || [] });
+  $self->render(\'', { type => 'json' }); # ' emacs happy again
 }
 
-sub action_save_property_value {
+sub action_edit_variant_property_value {
   my ($self) = @_;
 
-  $self->create_or_update_property_value;
-}
-
-
-sub render_variant_edit_dialog {
-  my ($self) = @_;
   $self->js
     ->run(
-      'kivi.VariantProperty.variant_dialog',
-      t8('Variant Value'),
-      $self->render('variant_property/variant_property_value_form', { output => 0 })
+      'kivi.VariantProperty.variant_property_value_dialog',
+      t8('Variant Property Value'),
+      $self->render(
+        'variant_property/variant_property_value_form',
+        { output => 0 },
+        variant_property_value => SL::DB::Manager::VariantPropertyValue->find_by(
+            id => $::form->{variant_property_value_id}
+          ),
+      )
     )
     ->reinit_widgets;
 
   $self->js->render;
 }
 
-#sub action_list_property_values_list {
-#  my ($self) = @_;
-#
-#  $self->_setup_list_action_bar;
-#  $self->render('variant_property/variant_property_list',
-#                title             => t8('Variant Property'),
-#                VARIANTPROPERTIES => SL::DB::Manager::VariantProperty->get_all_sorted,
-#               );
-#}
+sub action_save_variant_property_value {
+  my ($self) = @_;
 
-#sub action_reorder {
-#  my ($self) = @_;
-#
-#  SL::DB::DeliveryTerm->reorder_list(@{ $::form->{delivery_term_id} || [] });
-#
-#  $self->render(\'', { type => 'json' });     # ' make Emacs happy
-#}
-#
+  die "'variant_property_value.id' is needed" unless $::form->{variant_property_value}->{id};
+
+  my $variant_property_value = SL::DB::Manager::VariantPropertyValue->find_by(
+    id => $::form->{variant_property_value}->{id}
+  ) or die t8("Could not find Variant Property Value");
+
+  $variant_property_value->update_attributes(
+    %{$::form->{variant_property_value}}
+  );
+
+  flash_later('info', t8('The Variant Property Value has been saved.'));
+  $self->redirect_to(
+    action => 'edit_variant_property',
+    id     => $variant_property_value->variant_property_id,
+  );
+}
+
+sub action_add_variant_property_value {
+  my ($self) = @_;
+
+  my $new_variant_property_value = SL::DB::VariantPropertyValue->new(
+    %{ $::form->{new_variant_property_value} },
+    variant_property => $self->variant_property,
+  )->save;
+
+  $self->redirect_to(
+    action => 'edit_variant_property',
+    id     => $self->variant_property->id,
+  );
+}
+
 #
 #inits
 #
 
 sub init_variant_property {
   SL::DB::Manager::VariantProperty->find_by_or_create(id => $::form->{id} || 0)->assign_attributes(%{ $::form->{variant_property} });
-}
-
-sub init_variant_property_value {
-  SL::DB::Manager::VariantPropertyValue->find_by_or_create(id => $::form->{variant_property_value_id} || 0)->assign_attributes(%{ $::form->{variant_property_value} });
 }
 
 sub add_javascripts  {
@@ -126,7 +148,7 @@ sub add_javascripts  {
 # helpers
 #
 
-sub create_or_update_property {
+sub create_or_update_variant_property {
   my ($self) = @_;
 
   my $is_new = !$self->variant_property->id;
@@ -134,35 +156,17 @@ sub create_or_update_property {
   my @errors = $self->variant_property->validate;
   if (@errors) {
     flash('error', @errors);
-    $self->action_edit_property();
+    $self->action_edit_variant_property();
     return;
   }
 
   $self->variant_property->save;
 
   flash_later('info', $is_new ? t8('The Variant Property has been created.') : t8('The Variant Property has been saved.'));
-  $self->redirect_to(action => 'list_properties');
-}
-
-sub create_or_update_property_value {
-  my ($self) = @_;
-
-  $main::lxdebug->dump(0, "TST:FORM ", $::form);
-  my $is_new = !$self->variant_property_value->id;
-
-#  my @errors = $self->variant_property_value->validate;
-#  if (@errors) {
-#    flash('error', @errors);
-#    $self->action_edit_property();
-#    return;
-#  }
-  $main::lxdebug->dump(0, "TST:PV ", $self->variant_property_value);
-  $self->variant_property_value->assign_attributes( variant_property_id => $::form->{property_value_id});
-  $self->variant_property_value->save;
-
-  flash_later('info', $is_new ? t8('The Variant Property Value has been created.') : t8('The Variant Property Value has been saved.'));
-  $self->action_edit_property();
-  return;
+  $self->redirect_to(
+    action => 'edit_variant_property',
+    id     => $self->variant_property->id,
+  );
 }
 
 sub _setup_form_action_bar {
@@ -173,17 +177,17 @@ sub _setup_form_action_bar {
       combobox => [
         action => [
           t8('Save'),
-          submit    => [ '#form', { action => "VariantProperty/save_property" } ],
+          submit    => [ '#form', { action => "VariantProperty/save_variant_property" } ],
           accesskey => 'enter',
         ],
          action => [
           t8('Delete'),
-          submit => [ '#form', { action => "VariantProperty/delete_property" } ],
+          submit => [ '#form', { action => "VariantProperty/delete_variant_property" } ],
         ],
       ],
       action => [
         t8('Cancel'),
-        submit => [ '#form', { action => "VariantProperty/list_properties" } ],
+        submit => [ '#form', { action => "VariantProperty/list_variant_properties" } ],
       ],
     );
   }
@@ -196,7 +200,7 @@ sub _setup_list_action_bar {
     $bar->add(
       link => [
         t8('Add'),
-        link => $self->url_for(action => 'edit_property'),
+        link => $self->url_for(action => 'edit_variant_property'),
       ],
     )
   };
