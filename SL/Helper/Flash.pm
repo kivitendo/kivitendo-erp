4a663bf82dec36e05f9e5f7f039a83ad619b5067
Tamino Steinert <tamino.steinert@tamino.st>
2024-02-05 15:35:04 +0100

    SL::Layout::Flash: fliegende Flash-Meldungen (portiert von Odyn)
      TODOs:
      - CSS für Design40 und Material anpassen
      - Fehler von t/flash_migration/deprecated_calls beheben

--- SL/Helper/Flash.pm
+++ SL/Helper/Flash.pm
@@ -5,7 +5,7 @@ use strict;
 require Exporter;
 our @ISA       = qw(Exporter);
 our @EXPORT    = qw(flash flash_later);
-our @EXPORT_OK = qw(render_flash delay_flash);
+our @EXPORT_OK = qw(delay_flash);
 
 my %valid_categories = (
   map({$_ => 'info'} qw(information message)),
@@ -24,13 +24,17 @@ sub flash_later {
   $::auth->set_session_value({ key => "FLASH", value => _store_flash($::auth->get_session_value('FLASH'), @_), auto_restore => 1 });
 }
 
-sub delay_flash {
-  my $store = $::form->{FLASH} || { };
-  flash_later($_ => @{ $store->{$_} || [] }) for keys %$store;
+sub flash_contents {
+  return unless $::form;
+  return unless $::form->{FLASH};
+  return unless 'ARRAY' eq ref $::form->{FLASH};
+
+  @{ $::form->{FLASH} }
 }
 
-sub render_flash {
-  return $::form->parse_html_template('common/flash');
+sub delay_flash {
+  my $store = $::form->{FLASH} || [];
+  flash_later(@{ $_ || [] }) for @$store;
 }
 
 #
@@ -38,11 +42,12 @@ sub render_flash {
 #
 
 sub _store_flash {
-  my $store    = shift || { };
-  my $category = _check_category(+shift);
+  my ($store, $type, $message, $details, $timestamp) = @_;
+  $store     //= [ ];
+  $timestamp //= time();
+  my $category = _check_category($type);
 
-  $store->{ $category } ||= [ ];
-  push @{ $store->{ $category } }, @_;
+  push @{ $store }, [ $type, $message, $details, $timestamp ];
 
   return $store;
 }
@@ -68,50 +73,54 @@ displayed to the user
 
 =head1 SYNOPSIS
 
+  use SL::Helper::Flash qw(flash flash_later delay_flash);
+
+  # display in this request
+  flash('info', 'Customer saved!');
+  flash('error', 'Something went wrong', "details about what went wrong");
+  flash('warning', 'this might not be a good idea');
+
+  # display after a redirect
+  flash_later('info', 'Customer saved!');
+  flash_later('error', 'Something went wrong', "details about what went wrong");
+  flash_later('warning', 'this might not be a good idea');
+
+  # delay flash() calls to next request:
+  delay_flash();
+
+=head1 DESCRIPTION
+
 The flash is a store for messages that should be displayed to the
 user. Each message has a category which is usually C<information>,
 C<warning> or C<error>. The messages in each category are grouped and
 displayed in colors appropriate for their severity (e.g. errors in
 red).
 
-Messages are rendered either by calling the function C<render_flash>
-or by including the flash sub-template from a template with the
-following code:
-
-  [%- INCLUDE 'common/flash.html' %]
+Messages are rendered by including the L<SL::Layout::Flash> sub layout.
 
 =head1 EXPORTS
 
 The functions L</flash> and L</flash_later> are always exported.
 
-The function L</render_flash> is only exported upon request.
-
 =head1 FUNCTIONS
 
 =over 4
 
-=item C<flash $category, @messages>
+=item C<flash $category, $message [, $details ]>
 
-Stores messages for the given category. The category can be either
-C<information>, C<warning> or C<error>. C<info> can also be used as an
-alias for C<information>.
+Store a message with optional details for the given category. The category can
+be either C<information>, C<warning> or C<error>. C<info> can also be used as
+an alias for C<information>.
 
-=item C<flash_later $category, @messages>
+=item C<flash_later $category, $message [, $details ]>
 
-Stores messages for the given category for the next request. The
-category can be either C<information>, C<warning> or C<error>. C<info>
-can also be used as an alias for C<information>.
+Store a message with optional details for the given category for the next
+request. The category can be either C<information>, C<warning> or C<error>.
+C<info> can also be used as an alias for C<information>.
 
-The messages are stored in the user's session and restored upon the
+The message is stored in the user's session and restored upon the
 next request. Can be used for transmitting information over HTTP
 redirects.
-
-=item C<render_flash>
-
-Outputs the flash message by parsing the C<common/flash.html> template
-file.
-
-This function is not exported by default.
 
 =item C<delay_flash>
 
@@ -119,6 +128,10 @@ Delays flash, as if all flash messages in this request would have been
 C<flash_later>
 
 Not exported by default.
+
+=item C<flash_contents>
+
+The contents of the current flash accumulator.
 
 =back
 
---
+++ js/kivi.Flash.js
@@ -0,0 +1,122 @@
+namespace("kivi.Flash", function(ns) {
+  "use strict";
+
+  ns.type_to_title = {
+    error:   kivi.t8('Error'),
+    warning: kivi.t8('Warning'),
+    info:    kivi.t8('Information'),
+    ok:      kivi.t8('Ok')
+  };
+
+  ns.display_flash = function(type, message, details, timestamp) {
+    let $dom = $('<div>');
+    $dom.addClass('layout-flash-' + type);
+    $dom.addClass('layout-flash-message');
+
+    let $header = $('<div>');
+    $header.addClass('layout-flash-header');
+
+    let $remove = $('<span>✘</span>');
+    $remove.addClass('layout-flash-remove').addClass('cursor-pointer');
+    $remove.attr('alt', kivi.t8('Close Flash'));
+    $header.append($remove);
+
+    if (timestamp === undefined) {
+      timestamp = new Date();
+    } else if (timestamp > 0) {
+      timestamp = new Date(timestamp * 1000);
+    }
+
+    let $time = $('<span>');
+    $time.addClass('layout-flash-timestamp');
+    $time.text(kivi.format_time(timestamp));
+    $header.append($time);
+
+    let $type = $('<span>');
+    $type.addClass('layout-flash-type');
+    $type.text(ns.type_to_title[type]);
+    $header.append($type);
+
+    let $body = $('<div>');
+    $body.addClass('layout-flash-body');
+
+    if (message !== undefined && message !== null) {
+      let $message =  $('<span>');
+      $message.addClass('layout-flash-content');
+      $message.html(message);
+      $body.append($message);
+    }
+
+    if (details !== undefined && details !== null) {
+      let $details =  $('<span>');
+      $details.addClass('layout-flash-details');
+      $details.html(details);
+      $body.append($details);
+    }
+
+    $dom.append($header);
+    $dom.append($body);
+
+    $("#layout_flash_container").append($dom);
+
+    // fadeout after 1min
+    $dom.delay(60000).fadeOut('slow');
+
+    ns.show();
+  };
+
+  ns.display_flash_detail = function(type, message) {
+    $('#flash_' + type + '_disp').show();
+  };
+
+  ns.clear_flash = function(category, timeout) {
+    if (timeout === undefined) {
+      ns.clear_flash_now(category);
+    } else {
+      window.setTimeout(function(){
+        ns.clear_flash_now(category);
+      }, timeout);
+    }
+  };
+
+  ns.clear_flash_now = function(category) {
+    if (category) {
+      $('div.layout-flash-' + category).remove();
+    } else {
+      $('div.layout-flash-message').remove();
+    }
+  };
+
+  ns.remove_entry = function(e) {
+    $(e.target).closest('div.layout-flash-message').remove();
+  };
+
+  ns.toggle = function() {
+    $('#layout_flash_container').toggle();
+  };
+  ns.show = function() {
+    $('#layout_flash_container').show();
+  };
+  ns.hide = function() {
+    $('#layout_flash_container').hide();
+  };
+  ns.reload_flash = function() {
+    $.get("controller.pl", { action: "Flash/reload" }, kivi.eval_json_result);
+  };
+
+  ns.reinit_widgets = function() {
+
+  };
+});
+
+$(function() {
+  "use strict";
+  // dispatch to kivi.Flash for compatibility
+  kivi.display_flash        = kivi.Flash.display_flash;
+  kivi.display_flash_detail = kivi.Flash.display_flash_detail;
+  kivi.empty_flash          = kivi.Flash.empty_flash;
+  kivi.clear_flash          = kivi.Flash.clear_flash;
+
+  $('.layout-flash-toggle').click(kivi.Flash.toggle);
+  $('#layout_flash_container').on('click', '.layout-flash-remove', kivi.Flash.remove_entry);
+});
