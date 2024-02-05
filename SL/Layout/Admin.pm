615d1664427d7374e5191fc174b786be614d36d5
Tamino Steinert <tamino.steinert@tamino.st>
2024-02-05 15:35:04 +0100

    SL::Layout::Flash: fliegende Flash-Meldungen (portiert von Odyn)
      TODOs:
      - CSS für Design40 und Material anpassen
      - Fehler von t/flash_migration/deprecated_calls beheben

--- SL/Layout/Admin.pm
+++ SL/Layout/Admin.pm
@@ -7,11 +7,15 @@ use SL::Menu;
 use SL::Layout::None;
 use SL::Layout::Top;
 use SL::Layout::CssMenu;
+use SL::Layout::Flash;
 
 sub init_sub_layouts {
+  $_[0]->sub_layouts_by_name->{flash}     = SL::Layout::Flash->new;
+
   [
     SL::Layout::None->new,
     SL::Layout::CssMenu->new(menu => SL::Menu->new('admin')),
+    $_[0]->sub_layouts_by_name->{flash},
   ]
 }
 
--- SL/Layout/Classic.pm
+++ SL/Layout/Classic.pm
@@ -8,17 +8,23 @@ use SL::Layout::MenuLeft;
 use SL::Layout::None;
 use SL::Layout::Split;
 use SL::Layout::ActionBar;
+use SL::Layout::Flash;
 use SL::Layout::Content;
 
 sub init_sub_layouts {
   $_[0]->sub_layouts_by_name->{actionbar} = SL::Layout::ActionBar->new;
+  $_[0]->sub_layouts_by_name->{flash}     = SL::Layout::Flash->new;
 
   [
     SL::Layout::None->new,
     SL::Layout::Top->new,
     SL::Layout::Split->new(
       left  => [ SL::Layout::MenuLeft->new ],
-      right => [ $_[0]->sub_layouts_by_name->{actionbar}, SL::Layout::Content->new ],
+      right => [
+        $_[0]->sub_layouts_by_name->{actionbar},
+        $_[0]->sub_layouts_by_name->{flash},
+        SL::Layout::Content->new,
+      ],
     )
   ]
 }
