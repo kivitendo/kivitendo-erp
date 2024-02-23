a2cfe3e75dd420a0ee7c7a5e7a4e8223a8951ceb
Tamino Steinert <tamino.steinert@tamino.st>
2024-02-23 10:04:14 +0100

    Füge Reiter Dokumente bei Debitorenbuchungen hinzu

--- js/kivi.AR.js
+++ js/kivi.AR.js
@@ -21,3 +21,7 @@ namespace('kivi.AR', function(ns){
     return false;
   };
 });
+
+$(function() {
+  console.log(kivi.File.doc_tab_init('ar_tabs', 'ui-tabs-docs', $('#id').val(), 'invoice'));
+});
