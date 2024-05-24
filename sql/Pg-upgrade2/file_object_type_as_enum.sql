64a17f70891c1773226ac92a63a189a1da2548db
Jan Büren <jan@kivitendo.de>
2024-05-24 10:03:22 +0200

    korrekte abhängigkeit db-skript, dann gibt es auch keinen Folgekonflikt

--- sql/Pg-upgrade2/file_object_type_as_enum.sql
+++ sql/Pg-upgrade2/file_object_type_as_enum.sql
@@ -1,6 +1,6 @@
 -- @tag: file_object_type_as_enum
 -- @description: Objekttype von Dateien in Enum ändern
--- @depends: files file_storage_purchase_quotation_intake
+-- @depends: files file_storage_purchase_order_confirmation
 -- @ignore: 0
 
 
@@ -53,20 +53,4 @@ ALTER TABLE files ALTER  COLUMN object_type_new SET NOT NULL;
 ALTER TABLE files DROP   COLUMN object_type;
 ALTER TABLE files RENAME COLUMN object_type_new TO object_type;
 
--- constraint aus file_storage_purchase_quotation_intake wiederherstellen:
-ALTER TABLE files
-  ADD  CONSTRAINT valid_type CHECK (
-             (object_type = 'credit_note'                 ) OR (object_type = 'invoice'                   )
-          OR (object_type = 'sales_order'                 ) OR (object_type = 'sales_order_intake'        )
-          OR (object_type = 'sales_quotation'             ) OR (object_type = 'sales_delivery_order'      )
-          OR (object_type = 'request_quotation'           ) OR (object_type = 'purchase_quotation_intake' ) OR (object_type = 'purchase_order'          )
-          OR (object_type = 'purchase_delivery_order'     ) OR (object_type = 'purchase_invoice'          )
-          OR (object_type = 'vendor'                      ) OR (object_type = 'customer'                  ) OR (object_type = 'part'                    )
-          OR (object_type = 'gl_transaction'              ) OR (object_type = 'dunning'                   ) OR (object_type = 'dunning1'                )
-          OR (object_type = 'dunning2'                    ) OR (object_type = 'dunning3'                  ) OR (object_type = 'dunning_orig_invoice'    )
-          OR (object_type = 'dunning_invoice'             ) OR (object_type = 'draft'                     ) OR (object_type = 'statement'               )
-          OR (object_type = 'shop_image'                  ) OR (object_type = 'letter'                    ) OR (object_type = 'project'                 )
-          OR (object_type = 'invoice_for_advance_payment' ) OR (object_type = 'final_invoice'             ) OR (object_type = 'supplier_delivery_order' )
-          OR (object_type = 'sales_reclamation'           ) OR (object_type = 'purchase_reclamation'      ) OR (object_type = 'rma_delivery_order'      )
-  );
 
