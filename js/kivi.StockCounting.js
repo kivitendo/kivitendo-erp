f01e015e8a9bec0782a7a1a6a8836f0bba075cd0
Bernd Bleßmann <bernd@kivitendo-premium.de>
2024-08-07 17:00:06 +0200

    Zwischeninventur: Modale Dialoge für Feedback

--- js/kivi.StockCounting.js
+++ js/kivi.StockCounting.js
@@ -1,3 +1,12 @@
 namespace('kivi.StockCounting', function(ns) {
 
+  ns.count_window_onload = () => {
+    if ($('#successfully_counted').val() === '1') {
+      $('#successfully_counted_modal').modal('open');
+    }
+    if ($('#errors').val() !== '0') {
+      $('#error_modal').modal('open');
+    }
+  };
+
 });
