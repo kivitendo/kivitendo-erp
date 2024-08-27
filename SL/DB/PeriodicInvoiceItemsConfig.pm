3ba87b6075826c67bbf8f7ce7b88359d0f3860cc
Tamino Steinert <tamino.steinert@tamino.st>
2024-08-27 11:53:25 +0200

    S:D:PeriodicInvoiceItemsConfig: nicht aktive hervorheben

--- SL/DB/PeriodicInvoiceItemsConfig.pm
+++ SL/DB/PeriodicInvoiceItemsConfig.pm
@@ -15,4 +15,23 @@ sub get_item_period_length {
   return $ITEM_PERIOD_LENGTHS{ $self->periodicity };
 }
 
+sub active {
+  my ($self) = @_;
+  return 0 if $self->periodicity eq 'n';
+  if ($self->periodicity eq 'o') {
+    return 0 if $self->once_invoice_id;
+    return 1;
+  }
+  return 1 if !$self->end_date || (!$self->terminated && $self->extend_automatically_by);
+  my $order_config = $self->order_item->order->periodic_invoices_config;
+  my $last_billing_date = $order_config->get_previous_billed_period_start_date;
+  if ($last_billing_date) {
+    my $next_billing_date = $order_config->add_months(
+      $last_billing_date, $self->get_item_period_length
+    );
+    return 0 if $next_billing_date > $self->end_date;
+  }
+  return 1;
+}
+
 1;
