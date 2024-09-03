fa41c2817bf91dc99aacf35bfa2272c533bbb765
Sven Schöling <s.schoeling@googlemail.com>
2024-09-03 15:48:48 +0200

    price_rule_macro: Pg-upgrade2 für release 3.9.0 basis

--- sql/Pg-upgrade2/defaults_price_rule_type_order.sql
+++ sql/Pg-upgrade2/defaults_price_rule_type_order.sql
@@ -1,5 +1,5 @@
 -- @tag: defaults_price_rule_type_order
 -- @description: Reiehnfolge der Typspalten in der Preisregelsuche
--- @depends: release_19_05
+-- @depends: release_3_9_0 price_rule_macro_foreign_key
 
 ALTER TABLE defaults ADD COLUMN price_rule_type_order text[];
--- sql/Pg-upgrade2/price_rule_macro_foreign_key.sql
+++ sql/Pg-upgrade2/price_rule_macro_foreign_key.sql
@@ -1,6 +1,6 @@
 -- @tag: price_rule_macro_foreign_key
 -- @description: Preisregeln löschen - Datenbankkonsistenz
--- @depends: release_19_04 price_rules_macros
+-- @depends: release_3_9_0 price_rules_macros price_rule_macro_itime_mtime
 
 ALTER TABLE price_rules DROP CONSTRAINT "price_rules_price_rule_macro_id_fkey";
 ALTER TABLE price_rules ADD FOREIGN KEY (price_rule_macro_id) REFERENCES price_rule_macros(id) ON DELETE CASCADE;
