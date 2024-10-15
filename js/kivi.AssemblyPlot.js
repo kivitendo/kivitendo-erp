32ab9a6977ff7ce24b49806814ba5c3855048d6b
Bernd Bleßmann <bernd@kivitendo-premium.de>
2024-10-15 11:41:17 +0200

    visuelle Darstellung Erzeugnisse: Farbverlauf auch im svg-Tag …
    und wrapper weg, da dieser sonst beim horiz. Scrollen eine Lücke
    hinterlässt.

--- js/kivi.AssemblyPlot.js
+++ js/kivi.AssemblyPlot.js
@@ -168,6 +168,7 @@ namespace('kivi.AssemblyPlot', function(ns) {
           .attr("width", width)
           .attr("height", dx)
           .attr("viewBox", [-marginLeft, -marginTop, width, dx])
+          .attr("class", "bg-content")
           .attr("style", `width: ${width}; height: auto; font: 12px sans-serif; user-select: none;`);
 
     const gLink = svg.append("g")
@@ -313,7 +314,6 @@ namespace('kivi.AssemblyPlot', function(ns) {
     let svg;
     if ($('#alternative').val() == 1) {
       $('#content').css({overflow:'initial'});
-      $('#svg-wrapper').css({overflow:'initial'});
       svg = ns.collapsableChart(root);
     } else {
       svg = ns.chart(root);
