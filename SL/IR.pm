8a2fa03da5f76c913265a2cdccdc078954f033c5
Jan Büren <jan@kivitendo.de>
2025-01-22 10:05:21 +0100

    Preisinformation um Quelle EK-Rechnung und Lieferant erweitert

--- SL/IR.pm
+++ SL/IR.pm
@@ -95,7 +95,7 @@ sub _post_invoice {
   my $ic_cvar_configs = CVar->get_configs(module => 'IC',
                                           dbh    => $dbh);
 
-  my ($query, $sth, @values, $project_id);
+  my ($query, $sth, @values, $project_id, $ph);
   my ($allocated, $taxrate, $taxamount, $taxdiff, $item);
   my ($amount, $linetotal, $last_inventory_accno_tax_id_key, $last_expense_accno_tax_id_key);
   my ($netamount, $invoicediff, $expensediff) = (0, 0, 0);
@@ -266,6 +266,11 @@ sub _post_invoice {
       if ( abs($a->lastcost - $new_lastcost) >= 0.009 ) {
         $a->update_attributes(lastcost => $new_lastcost);
         $a->set_lastcost_assemblies_and_assortiments;
+
+        # trigger hack
+        #    SL::DB::Manager::SomeClass->get_all(where => [ … ], limit => 1)
+        $ph = SL::DB::Manager::PartsPriceHistory->get_all(where => [ part_id => $a->id ], sort_by => 'id DESC', limit => 1)->[0];
+        $ph->update_attributes(vendor_id => $form->{vendor_id}, ap_id => $form->{id});
       }
 
       # check if we sold the item already and
@@ -427,8 +432,13 @@ sub _post_invoice {
       my $part_price_factor = $a->price_factor_id ? $a->price_factor->factor : 1;
       my $new_lastcost      = abs($fxsellprice * $form->{exchangerate} / $basefactor / $price_factor * $part_price_factor);
       if ( abs($a->lastcost - $new_lastcost) >= 0.009 ) {
+        $ph = SL::DB::Manager::PartsPriceHistory->get_all(part_id => $a->id);
         $a->update_attributes(lastcost => $new_lastcost);
         $a->set_lastcost_assemblies_and_assortiments;
+
+        # trigger hack - new entry is created by trigger on the fly CAVEAT: db behaviour might change
+        my $ph = SL::DB::Manager::PartsPriceHistory->get_all(where => [ part_id => $a->id ], sort_by => 'id DESC', limit => 1)->[0];
+        $ph->update_attributes(vendor_id => $form->{vendor_id}, ap_id => $form->{id});
       }
     }
 
