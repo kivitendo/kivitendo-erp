7660bca362659b3c7af87b4b5873c83ad6a3e026
Jan Büren <jan@kivitendo.de>
2025-02-09 16:21:16 +0100

    Neues Feld used_for_assembly_id in Tabelle inventory

---
+++ sql/Pg-upgrade2/inventory_add_used_for_assembly.sql
@@ -0,0 +1,11 @@
+-- @tag: inventory_add_used_for_assembly
+-- @description: parts_id für das erzeugnis, was mit der trans_id gefertigt worden ist
+-- @depends: release_3_9_0
+
+ALTER TABLE inventory ADD COLUMN used_for_assembly_id integer;
+
+ALTER TABLE inventory ADD FOREIGN KEY (used_for_assembly_id) REFERENCES parts (id);
+
+CREATE INDEX inventory_assembly_id_idx ON inventory (used_for_assembly_id);
+
+
