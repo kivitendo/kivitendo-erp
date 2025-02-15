c73836814f74959d4e7ae064de1678725444baf7
Jan Büren <jan@kivitendo.de>
2025-02-15 06:10:14 +0100

    sepa Zahlungslauf per Mandantenkonfig steuern

---
+++ sql/Pg-upgrade2/defaults_add_subtract_credit_notes.sql
@@ -0,0 +1,7 @@
+-- @tag: defaults_add_subtract_credit-notes
+-- @description: Mandantekonfig: SEPA Zahlungen zusammenfassen, Gutschriften abziehen
+-- @depends: release_3_9_1
+
+ALTER TABLE defaults ADD COLUMN sepa_combine_payments boolean DEFAULT TRUE;
+ALTER TABLE defaults ADD COLUMN spea_subtract_credit_notes boolean DEFAULT TRUE;
+
