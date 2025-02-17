4c3d116612ad94ad4d39a5386a812a1ee36702a2
Jan Büren <jan@kivitendo.de>
2025-02-17 09:09:10 +0100

    sepa_exports_acc_trans Gutschriften gegen Zahlungen mit durch. Posten

---
+++ SL/DB/MetaSetup/SepaExportsAccTrans.pm
@@ -0,0 +1,47 @@
+# This file has been auto-generated. Do not modify it; it will be overwritten
+# by rose_auto_create_model.pl automatically.
+package SL::DB::SepaExportsAccTrans;
+
+use strict;
+
+use parent qw(SL::DB::Object);
+
+__PACKAGE__->meta->table('sepa_exports_acc_trans');
+
+__PACKAGE__->meta->columns(
+  acc_trans_id    => { type => 'integer', not_null => 1 },
+  ap_id           => { type => 'integer', not_null => 1 },
+  ar_id           => { type => 'integer', not_null => 1 },
+  itime           => { type => 'timestamp', default => 'now()' },
+  mtime           => { type => 'timestamp' },
+  sepa_exports_id => { type => 'integer', not_null => 1 },
+);
+
+__PACKAGE__->meta->primary_key_columns([ 'sepa_exports_id', 'acc_trans_id', 'ar_id', 'ap_id' ]);
+
+__PACKAGE__->meta->allow_inline_column_values(1);
+
+__PACKAGE__->meta->foreign_keys(
+  acc_transaction => {
+    class       => 'SL::DB::AccTransaction',
+    key_columns => { acc_trans_id => 'acc_trans_id' },
+  },
+
+  ap => {
+    class       => 'SL::DB::PurchaseInvoice',
+    key_columns => { ap_id => 'id' },
+  },
+
+  ar => {
+    class       => 'SL::DB::Invoice',
+    key_columns => { ar_id => 'id' },
+  },
+
+  sepa_exports => {
+    class       => 'SL::DB::SepaExport',
+    key_columns => { sepa_exports_id => 'id' },
+  },
+);
+
+1;
+;
