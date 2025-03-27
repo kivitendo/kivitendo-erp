3410f78035dfba3cbcdcb4316f85d284d844df5a
Tamino Steinert <tamino.steinert@tamino.st>
2025-03-27 15:11:52 +0100

    FIX: fester record_type für Rechnungen

--- sql/Pg-upgrade2/invoice_record_type.sql
+++ sql/Pg-upgrade2/invoice_record_type.sql
@@ -2,6 +2,8 @@
 -- @description: Persistente Typen in Rechnungen
 -- @depends: release_3_9_1
 
+-- invoice
+
 CREATE TYPE invoice_types AS ENUM (
   'ar_transaction',
   'ar_transaction_storno',
@@ -17,31 +19,31 @@ CREATE TYPE invoice_types AS ENUM (
 ALTER TABLE ar ADD COLUMN record_type invoice_types;
 
 UPDATE ar SET record_type = 'ar_transaction_storno'
-  WHERE record_type IS NULL and (invoice = FALSE or invoice IS NULL) and storno = TRUE;
+  WHERE record_type IS NULL and (invoice = FALSE or invoice IS NULL) and storno_id IS NOT NULL;
 UPDATE ar SET record_type = 'ar_transaction'
   WHERE record_type IS NULL and (invoice = FALSE or invoice IS NULL);
 
 UPDATE ar SET record_type = 'invoice_for_advance_payment_storno'
-  WHERE record_type IS NULL and type = 'invoice_for_advance_payment' and amount < 0 and storno = TRUE;
+  WHERE record_type IS NULL and type = 'invoice_for_advance_payment' and amount < 0 and storno_id IS NOT NULL;
 UPDATE ar SET record_type = 'invoice_for_advance_payment'
   WHERE record_type IS NULL and type = 'invoice_for_advance_payment';
 
 UPDATE ar SET record_type = 'final_invoice'
   WHERE record_type IS NULL and type = 'final_invoice';
 
+UPDATE ar SET record_type = 'credit_note_storno'
+  WHERE record_type IS NULL and type = 'credit_note' and amount > 0 and storno_id IS NOT NULL;
 UPDATE ar SET record_type = 'credit_note'
   WHERE record_type IS NULL and type = 'credit_note' and amount < 0;
-UPDATE ar SET record_type = 'credit_note_storno'
-  WHERE record_type IS NULL and type = 'credit_note' and amount > 0 and storno = TRUE;
 
 UPDATE ar SET record_type = 'invoice_storno'
-  WHERE record_type IS NULL and type != 'credit_note' and amount < 0 and storno = TRUE;
-
+  WHERE record_type IS NULL and amount <= 0 and storno_id IS NOT NULL;
 UPDATE ar SET record_type = 'invoice'
   WHERE record_type IS NULL and amount >= 0;
 
-
 ALTER TABLE ar ALTER COLUMN record_type SET NOT NULL;
+
+-- purchase_invoice
 
 CREATE TYPE purchase_invoice_types AS ENUM (
   'ap_transaction',
@@ -54,19 +56,19 @@ CREATE TYPE purchase_invoice_types AS ENUM (
 
 ALTER TABLE ap ADD COLUMN record_type purchase_invoice_types;
 
+UPDATE ap SET record_type = 'ap_transaction_storno'
+  WHERE record_type IS NULL and (invoice = FALSE or invoice IS NULL) and storno_id IS NULL;
 UPDATE ap SET record_type = 'ap_transaction'
   WHERE record_type IS NULL and (invoice = FALSE or invoice IS NULL);
-UPDATE ap SET record_type = 'ap_transaction_storno'
-  WHERE record_type IS NULL and (invoice = FALSE or invoice IS NULL) and storno = TRUE;
 
-UPDATE ap SET record_type = 'purchase_credit_note'
-  WHERE record_type IS NULL and amount < 0 and (storno = FALSE or invoice IS NULL);
 UPDATE ap SET record_type = 'purchase_credit_note_storno'
-  WHERE record_type IS NULL and amount > 0 and storno = TRUE;
+  WHERE record_type IS NULL and amount > 0 and storno_id IS NOT NULL;
+UPDATE ap SET record_type = 'purchase_credit_note'
+  WHERE record_type IS NULL and amount < 0;
 
-UPDATE ap SET record_type = 'purchase_invoice'
-  WHERE record_type IS NULL and amount >= 0 and (storno = FALSE or invoice IS NULL);
 UPDATE ap SET record_type = 'purchase_invoice_storno'
-  WHERE record_type IS NULL and amount <= 0 and storno = TRUE;
+  WHERE record_type IS NULL and amount <= 0 and storno_id IS NOT NULL;
+UPDATE ap SET record_type = 'purchase_invoice'
+  WHERE record_type IS NULL and amount >= 0;
 
-ALTER TABLE ar ALTER COLUMN record_type SET NOT NULL;
+ALTER TABLE ap ALTER COLUMN record_type SET NOT NULL;
