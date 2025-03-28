e9b560172cd9d7fc2b1562e9ec079608abd2a401
Niklas Schmidt <niklas@kivitendo.de>
2025-03-28 12:38:28 +0100

    DB-Upgrade: stabilisiere Sortierreihenfolge
    email_journal_record_types_add_purchase_order_confirmation soll vor
    email_journal_record_types_add_catch_all laufen, denn sonst sind
    möglicherweise zwei Spalten in SL/DB/MetaSetup/EmailJournal.pm vertauscht.

--- sql/Pg-upgrade2/email_journal_record_types_add_chatall.sql
+++ sql/Pg-upgrade2/email_journal_record_types_add_chatall.sql
@@ -1,5 +1,5 @@
 -- @tag: email_journal_record_types_add_catch_all
 -- @description: E-Mail-Journal Beleg Type um generischen Type erweitern
--- @depends: email_journal_record_import_types
+-- @depends: email_journal_record_import_types email_journal_record_types_add_purchase_order_confirmation
 
 ALTER TYPE email_journal_record_type ADD VALUE IF NOT EXISTS 'catch_all';
