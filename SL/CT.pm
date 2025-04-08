0899949411c6d6846da150c7291ce4e8ec282a0d
Jan Büren <jan@kivitendo.de>
2025-04-08 09:20:20 +0200

    Nur aktive Ansprechpartner anzeigen bei gültigen Kunden

--- SL/CT.pm
+++ SL/CT.pm
@@ -519,7 +519,7 @@ sub search_contacts {
     }
     add_token(\@where_tokens, \@values, col => "COALESCE(c.name, v.name)", val => $filter->{vcname}, method => 'ILIKE', esc => 'substr') if $filter->{vcname};
 
-    push @where_tokens, 'cp.cp_cv_id IS NOT NULL' if $filter->{status} eq 'active';
+    push @where_tokens, 'c.obsolete is FALSE ' if $filter->{status} eq 'active';
     push @where_tokens, 'cp.cp_cv_id IS NULL'     if $filter->{status} eq 'orphaned';
   }
 
