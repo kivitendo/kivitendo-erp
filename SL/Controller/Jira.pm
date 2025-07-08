e02f909ee89ce44a5bddcd3b82073e9f80c8ce0e
Sven Schöling <s.schoeling@googlemail.com>
2025-07-08 14:06:17 +0200

    experimenteller Jira controller

---
+++ SL/Controller/Jira.pm
@@ -0,0 +1,137 @@
+package SL::Controller::Jira;
+
+use strict;
+use SL::DB::Customer;
+use SL::DB::Vendor;
+use SL::DB::OauthToken;
+
+
+sub action_ajax_list_jira {
+  my ($self, %params) = @_;
+
+  my $cus =  SL::DB::Manager::Customer->find_by(id => $::form->{id});
+  my $jql = 'textfields ~ "' . $cus->name . '*"';
+
+  my $report   = SL::ReportGenerator->new(\%::myconfig, $::form);
+  my @columns  = qw(key summary priority status creator assignee created updated);
+  my @visible  = qw(key summary priority status creator assignee created updated);
+  my @sortable = qw(key summary priority status creator assignee created updated);
+
+  my $instance_base_url = '';
+
+  my %column_defs = (
+    key         => { text => $::locale->text('Key'),              sub => sub { $_[0]->{key} }, obj_link => sub { $instance_base_url . '/browse/' . $_[0]->{key} } },
+    summary     => { text => $::locale->text('Summary'),          sub => sub { $_[0]->{summary} } },
+    priority    => { text => $::locale->text('Priority'),         sub => sub { $_[0]->{priority} } },
+    status      => { text => $::locale->text('Status'),           sub => sub { $_[0]->{status} } },
+    creator     => { text => $::locale->text('Creator'),          sub => sub { $_[0]->{creator} } },
+    assignee    => { text => $::locale->text('Assignee'),         sub => sub { $_[0]->{assignee} } },
+    created     => { text => $::locale->text('Created'),          sub => sub { $_[0]->{created}  } },
+    updated     => { text => $::locale->text('Updated'),          sub => sub { $_[0]->{updated} } },
+  );
+
+  $::form->{sort_by}  ||= 'partnumber';
+  $::form->{sort_dir} //= 1;
+
+  for my $col (@sortable) {
+    $column_defs{$col}{link} = $self->url_for(
+      action   => 'ajax_list_jira',
+      callback => $::form->{callback},
+      db       => $::form->{db},
+      id       => $self->{cv}->id,
+      sort_by  => $col,
+      sort_dir => ($::form->{sort_by} eq $col ? 1 - $::form->{sort_dir} : $::form->{sort_dir})
+    );
+  }
+
+  map { $column_defs{$_}{visible} = 1 } @visible;
+
+  my $pricegroup;
+  $pricegroup = $self->{cv}->pricegroup->pricegroup if $self->{cv}->pricegroup;
+
+  $report->set_columns(%column_defs);
+  $report->set_column_order(@columns);
+  $report->set_options(allow_pdf_export => 0, allow_csv_export => 0);
+  $report->set_sort_indicator($::form->{sort_by}, $::form->{sort_dir});
+  $report->set_export_options(@{ $params{report_generator_export_options} || [] });
+  $report->set_options(
+    %{ $params{report_generator_options} || {} },
+    output_format        => 'HTML',
+    top_info_text        => $::locale->text('Issues') . ': ' . $pricegroup . 'Atlassian JQL: ' . $jql,
+    title                => $::locale->text('Jira'),
+  );
+
+  my $sort_param = $::form->{sort_by} eq 'price'       ? 'price'             :
+                   $::form->{sort_by} eq 'description' ? 'parts.description' :
+                   'parts.partnumber';
+  $sort_param .= ' ' . ($::form->{sort_dir} ? 'ASC' : 'DESC');
+
+  my $jira_issues = $self->atlassian_jira_cloudid(1, $jql);
+
+  $self->report_generator_list_objects(report => $report, objects => $jira_issues, layout => 0, header => 0);
+}
+
+
+
+### private
+
+sub atlassian_jira_cloudid {
+  my ($self, $token_db_id, $jql) = @_;
+
+  my $bearer = http_bearer_auth_header($token_db_id);
+  my $client = REST::Client->new();
+  $client->addHeader('Authorization', $bearer);
+  $client->addHeader('Accept', 'application/json');
+  my $ret = $client->GET('https://api.atlassian.com/oauth/token/accessible-resources');
+  my $response_code = $ret->responseCode();
+  die "HTTP $response_code" unless $response_code eq '200';
+
+  my $accessible_resources = from_json($ret->responseContent);
+  my $cloudid = $accessible_resources->[0]{id};
+
+  #return $cloudid;
+
+  my $url;
+  #$url = "https://api.atlassian.com/ex/jira/$cloudid/rest/api/3/project/search";
+  #$ret = $client->GET($url);
+  #$response_code = $ret->responseCode();
+  #die "HTTP $response_code" unless $response_code eq '200';
+  #return from_json($ret->responseContent);
+
+  my $maxResults = 100;
+  my $fields = "summary";
+  #my $jql = 'textfields ~ "Test case*"';
+  $url = "https://api.atlassian.com/ex/jira/$cloudid/rest/api/3/search/jql?jql=" . uri_encode($jql) . "&maxResults=100&fields=id%2Cassignee%2Cauthor%2Ccreator%2Csummary%2Cresolution%2Cstatus%2Cpriority%2Ccreated%2Cupdated&expand=&reconcileIssues=";
+  $ret = $client->GET($url);
+  $response_code = $ret->responseCode();
+  die "HTTP $response_code" unless $response_code eq '200';
+  my $c = from_json($ret->responseContent);
+
+  my @a = map({{
+    key        => $_->{key},
+    summary    => $_->{fields}->{summary},
+    creator    => $_->{fields}->{creator}->{displayName},
+    assignee   => $_->{fields}->{assignee}->{displayName},
+    priority   => $_->{fields}->{priority}->{name},
+    created    => $_->{fields}->{created},
+    updated    => $_->{fields}->{updated},
+    status     => $_->{fields}->{status}->{name},
+    resolution => $_->{fields}->{resolution}->{name},
+  }} @{$c->{issues}});
+  return \@a;
+}
+
+
+sub http_bearer_auth_header {
+  my ($db_id) = @_;
+
+  my $tok = SL::DB::Manager::OauthToken->find_by(id => $db_id);
+
+  if (!$tok->is_valid) {
+    SL::Controller::OAuth::refresh($tok);
+  }
+
+  return 'Bearer ' . $tok->access_token;
+}
+
+1;
