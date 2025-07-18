8062178b46b6d2ca8f6914015380d273ae1fa426
Niklas Schmidt <niklas@kivitendo.de>
2025-07-18 15:06:30 +0200

    OAuth: employee für nutzerbezogene Tokens eingebaut

--- SL/DB/MetaSetup/OAuthToken.pm
+++ SL/DB/MetaSetup/OAuthToken.pm
@@ -12,6 +12,7 @@ __PACKAGE__->meta->columns(
   access_token            => { type => 'text' },
   access_token_expiration => { type => 'timestamp' },
   email                   => { type => 'text' },
+  employee_id             => { type => 'integer' },
   id                      => { type => 'serial', not_null => 1 },
   itime                   => { type => 'timestamp', default => 'now()', not_null => 1 },
   mtime                   => { type => 'timestamp', default => 'now()', not_null => 1 },
@@ -24,6 +25,13 @@ __PACKAGE__->meta->columns(
 __PACKAGE__->meta->primary_key_columns([ 'id' ]);
 
 __PACKAGE__->meta->allow_inline_column_values(1);
+
+__PACKAGE__->meta->foreign_keys(
+  employee => {
+    class       => 'SL::DB::Employee',
+    key_columns => { employee_id => 'id' },
+  },
+);
 
 1;
 ;
