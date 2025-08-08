0d66e4a9d86ef9fb18f1a5cdae229959c48f49f9
Bernd Bleßmann <bernd@kivitendo-premium.de>
2025-08-08 15:12:44 +0200

    Ticket System Jira: Beispiel-Provider google/calendar
    simple Aktion zum Zugriff auf google-Kalendar-List über OAuth-Provider
    
    Test google-Kalendar mit developer-Recht geschützt

--- SL/Controller/Test.pm
+++ SL/Controller/Test.pm
@@ -7,6 +7,12 @@ use parent qw(SL::Controller::Base);
 use Data::Dumper;
 use SL::ClientJS;
 
+use SL::Controller::OAuth;
+use REST::Client;
+
+__PACKAGE__->run_before(sub { $::auth->assert('developer') },
+                        only => [ qw(get_google_cal_list) ]);
+
 sub action_dump_form {
   my ($self) = @_;
 
@@ -20,5 +26,25 @@ sub action_ckeditor_test_page {
 
   $self->render("test/ckeditor");
 }
+
+
+sub action_get_google_cal_list {
+  my ($self) = @_;
+
+  my $api_host = 'https://www.googleapis.com';
+
+  my $acctok = SL::Controller::OAuth::access_token_for('google_cal', allow_client_wide => 1) or die "no access token";
+
+  my $client = REST::Client->new(host => $api_host);
+  $client->addHeader('Accept',        'application/json');
+  $client->addHeader('Authorization', 'Bearer ' . $acctok);
+
+  my $ret = $client->GET('/calendar/v3/users/me/calendarList');
+
+  my $output = Dumper($ret);
+  $::lxdebug->message(LXDebug->DEBUG2(), "Test: get_google_cal_list:\n" . $output);
+  $self->render(\$output, { type => 'text' });
+}
+
 
 1;
