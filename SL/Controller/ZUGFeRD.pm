29d7b8bcde2c2c608b33512c00dadd96451d9e2b
Sven Schöling <s.schoeling@googlemail.com>
2025-08-21 17:36:24 +0200

    ZUGFeRD: find_vendor aus Controller nach SL::ZUGFeRD verschoben
    Das macht es einfacher die zirkulären includes zu vermeiden die bisher
    passiert sind:
    
    SL::DB::Helper::ZUGFeRD -> SL::Controller::ZUGFeRD -> SL::DB::Vendor
    
    so ist da jetzt nur noch eine kleine Kaskade in
    
    SL::ZUGFeRD -> SL::DB::Vendor, und die wird jetzt absichtlich mit require eingebunden

--- SL/Controller/ZUGFeRD.pm
+++ SL/Controller/ZUGFeRD.pm
@@ -7,7 +7,6 @@ use SL::DB::RecordTemplate;
 use SL::Locale::String qw(t8);
 use SL::Helper::DateTime;
 use SL::XMLInvoice;
-use SL::VATIDNr;
 use SL::ZUGFeRD;
 use SL::SessionFile;
 use SL::Helper::Number qw(_format_total);
@@ -25,102 +24,6 @@ sub action_upload_zugferd {
   $self->render('zugferd/form', title => $::locale->text('Factur-X/ZUGFeRD import'));
 }
 
-sub find_vendor_by_taxnumber {
-  my $taxnumber = shift @_;
-
-  # 1.1 check if we a have a vendor with this tax number (vendor.taxnumber)
-  my $vendor = SL::DB::Manager::Vendor->find_by(
-    taxnumber => $taxnumber,
-    or    => [
-      obsolete => undef,
-      obsolete => 0,
-    ]);
-
-  if (!$vendor) {
-    # 1.2 If no vendor with the exact VAT ID number is found, the
-    # number might be stored slightly different in the database
-    # (e.g. with spaces breaking up groups of numbers). Iterate over
-    # all existing vendors with VAT ID numbers, normalize their
-    # representation and compare those.
-
-    my $vendors = SL::DB::Manager::Vendor->get_all(
-      where => [
-        '!taxnumber' => undef,
-        '!taxnumber' => '',
-        or       => [
-          obsolete => undef,
-          obsolete => 0,
-        ],
-      ]);
-
-    foreach my $other_vendor (@{ $vendors }) {
-      next unless $other_vendor->taxnumber eq $taxnumber;
-
-      $vendor = $other_vendor;
-      last;
-    }
-  }
-}
-
-sub find_vendor_by_ustid {
-  my $ustid = shift @_;
-
-  $ustid = SL::VATIDNr->normalize($ustid);
-
-  # 1.1 check if we a have a vendor with this VAT-ID (vendor.ustid)
-  my $vendor = SL::DB::Manager::Vendor->find_by(
-    ustid => $ustid,
-    or    => [
-      obsolete => undef,
-      obsolete => 0,
-    ]);
-
-  if (!$vendor) {
-    # 1.2 If no vendor with the exact VAT ID number is found, the
-    # number might be stored slightly different in the database
-    # (e.g. with spaces breaking up groups of numbers). Iterate over
-    # all existing vendors with VAT ID numbers, normalize their
-    # representation and compare those.
-
-    my $vendors = SL::DB::Manager::Vendor->get_all(
-      where => [
-        '!ustid' => undef,
-        '!ustid' => '',
-        or       => [
-          obsolete => undef,
-          obsolete => 0,
-        ],
-      ]);
-
-    foreach my $other_vendor (@{ $vendors }) {
-      next unless SL::VATIDNr->normalize($other_vendor->ustid) eq $ustid;
-
-      $vendor = $other_vendor;
-      last;
-    }
-  }
-
-  return $vendor;
-}
-
-sub find_vendor {
-  my ($ustid, $taxnumber) = @_;
-  my $vendor;
-
-  if ( $ustid ) {
-    $vendor = find_vendor_by_ustid($ustid);
-  }
-
-  if (ref $vendor eq 'SL::DB::Vendor') { return $vendor; }
-
-  if ( $taxnumber ) {
-    $vendor = find_vendor_by_taxnumber($taxnumber);
-  }
-
-  if (ref $vendor eq 'SL::DB::Vendor') { return $vendor; }
-
-  return undef;
-}
 
 sub action_import_zugferd {
   my ($self, %params) = @_;
@@ -188,7 +91,7 @@ sub build_ap_transaction_form_defaults {
       die t8("Cannot process this invoice: neither VAT ID nor tax ID present.");
     }
 
-    $vendor = find_vendor($metadata{'ustid'}, $metadata{'taxnumber'});
+    $vendor = SL::ZUGFeRD::find_vendor($metadata{'ustid'}, $metadata{'taxnumber'});
 
     die t8("Vendor with VAT ID (#1) and/or tax ID (#2) not found. Please check if the vendor " .
             "#3 exists and whether it has the correct tax ID/VAT ID." ,
