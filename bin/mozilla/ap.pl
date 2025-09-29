420282a0be793a0b1451e13f6fd32705fdca2eb7
Niklas Schmidt <niklas@kivitendo.de>
2025-09-29 10:36:31 +0200

    Bericht Ek-Rechnungen Einkäufer als Spalte anzeigen

--- bin/mozilla/ap.pl
+++ bin/mozilla/ap.pl
@@ -1209,7 +1209,7 @@ sub ap_transactions {
 
   my @columns =
     qw(transdate id type invnumber ordnumber name netamount tax amount paid datepaid
-       due duedate transaction_description notes intnotes employee globalprojectnumber department
+       due duedate transaction_description notes intnotes buyer employee globalprojectnumber department
        vendornumber country ustid taxzone payment_terms payment_approved charts debit_chart direct_debit
        insertdate items);
 
@@ -1239,6 +1239,7 @@ sub ap_transactions {
     'transaction_description' => { 'text' => $locale->text('Transaction description'), },
     'notes'                   => { 'text' => $locale->text('Notes'), },
     'intnotes'                => { 'text' => $locale->text('Internal Notes'), },
+    'buyer'                   => { 'text' => $locale->text('Buyer'), },
     'employee'                => { 'text' => $locale->text('Employee'), },
     'globalprojectnumber'     => { 'text' => $locale->text('Document Project Number'), },
     'department'              => { 'text' => $locale->text('Department'), },
