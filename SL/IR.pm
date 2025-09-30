d84e04fbac60b2cc4f7ecfa0aa304cfe5b244839
Jan Büren <jan@kivitendo.de>
2025-09-30 14:09:45 +0200

    Einkäufer auch bei Einkaufsrechnung speichern

--- SL/IR.pm
+++ SL/IR.pm
@@ -811,7 +811,8 @@ SQL
                 cp_id        = ?, employee_id = ?, department_id = ?, delivery_term_id = ?,
                 payment_id   = ?, transaction_description        = ?,
                 currency_id = (SELECT id FROM currencies WHERE name = ?),
-                globalproject_id = ?, direct_debit = ?
+                globalproject_id = ?, direct_debit = ?,
+                buyer_id = ?
               WHERE id = ?|;
   @values = (
                 $form->{invnumber},          $form->{ordnumber},           $form->{quonumber},      conv_date($form->{invdate}),
@@ -824,6 +825,7 @@ SQL
                 $form->{"currency"},
          conv_i($form->{globalproject_id}),
                 $form->{direct_debit} ? 't' : 'f',
+         conv_i($form->{buyer_id}),
          conv_i($form->{id})
   );
   do_query($form, $dbh, $query, @values);
