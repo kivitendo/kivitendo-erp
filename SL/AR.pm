63ebe307e8f52492ebf4c5cf73c6bc2615ff09af
Niklas Schmidt <niklas@kivitendo.de>
2025-11-06 14:45:19 +0100

    S:AR fix: amount und netamount sind Sortierspalten
    Zuvor waren diese Spalten zwar als sortierbar im Report anklickbar,
    die Sorteriung wurde aber nicht entsprechend durchgeführt.

--- SL/AR.pm
+++ SL/AR.pm
@@ -806,7 +806,7 @@ SQL
   my $sortdir   = !defined $form->{sortdir} ? 'ASC' : $form->{sortdir} ? 'ASC' : 'DESC';
   my $sortorder = join(', ', map { "$_ $sortdir" } @a);
 
-  if (grep({ $_ eq $form->{sort} } qw(id transdate duedate invnumber ordnumber cusordnumber donumber deliverydate name datepaid employee shippingpoint shipvia transaction_description department taxzone globalprojectdescription globalprojectnumber))) {
+  if (grep({ $_ eq $form->{sort} } qw(id transdate duedate invnumber ordnumber cusordnumber donumber deliverydate name netamount amount datepaid employee shippingpoint shipvia transaction_description department taxzone globalprojectdescription globalprojectnumber))) {
     $sortorder = $form->{sort} . " $sortdir";
   }
 
