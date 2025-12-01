34e1981a4097aa3e003d3164a4d8baf8fc3c5203
Cem Aydin <cem.aydin@gmx.ch>
2025-12-01 18:19:01 +0100

    css flex layout: Layout für Seitentitel erstellt

---
+++ SL/Layout/Title.pm
@@ -0,0 +1,13 @@
+package SL::Layout::Title;
+
+use strict;
+use parent qw(SL::Layout::Base);
+use SL::Presenter::EscapedText qw(escape);
+
+sub pre_content {
+  my $title = escape($::locale->text($::form->{title} || ""));
+
+  "<h1>${title}</h1>";
+}
+
+1;
