0f0b9569fa1e5538eeb71cabef2191a8bf57cea9
Cem Aydin <cem.aydin@gmx.ch>
2025-12-08 17:24:22 +0100

    css flex layout: Titel Layout Zeilenumbruch hinzufügen

--- SL/Layout/Title.pm
+++ SL/Layout/Title.pm
@@ -7,7 +7,7 @@ use SL::Presenter::EscapedText qw(escape);
 sub pre_content {
   my $title = escape($::locale->text($::form->{title} || ""));
 
-  "<h1>${title}</h1>";
+  "<h1>${title}</h1>\n";
 }
 
 1;
