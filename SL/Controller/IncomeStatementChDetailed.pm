2aeeb07d93f7d47a3aea1a6a679ddc8e1d6e6c4f
Cem Aydin <cem.aydin@gmx.ch>
2025-12-18 18:20:24 +0100

    Swiss Income Statement (Erfolgsrechnung): use base class for common functionality

--- SL/Controller/IncomeStatementChDetailed.pm
+++ SL/Controller/IncomeStatementChDetailed.pm
@@ -1,24 +1,19 @@
 package SL::Controller::IncomeStatementChDetailed;
 
 use strict;
-use parent qw(SL::Controller::Base);
+use parent qw(SL::Controller::IncomeStatementChBase);
 
 use POSIX qw(strftime);
 
 use SL::DBUtils;
-use SL::DB::Chart;
-use SL::DB::Department;
 use SL::DB::IncomeStatementChDetailedGroups;
 use SL::DB::IncomeStatementChDetailedCategories;
 
-use SL::ReportGenerator;
-use SL::Controller::Helper::ReportGenerator; # for PDF and CSV export
 use SL::Locale::String; # for t8()
-use SL::Presenter::DatePeriodAdder qw(get_date_periods get_hidden_variables_for_report);
+use SL::Presenter::DatePeriodAdder qw(get_date_periods);
 
 use Rose::Object::MakeMethods::Generic (
-  scalar => [ qw(title report date_periods show_accounts include_zero_amounts department_id) ],
-  'scalar --get_set_init' => [ qw(departments) ],
+  scalar => [ qw(show_accounts include_zero_amounts) ],
 );
 
 __PACKAGE__->run_before(sub { $::auth->assert('report'); });
@@ -28,8 +23,13 @@ __PACKAGE__->run_before(sub { $::auth->assert('report'); });
 sub action_report_settings {
   my ($self) = @_;
 
+  $self->actionname('IncomeStatementChDetailed/list');
   $self->setup_report_settings_action_bar;
-  $self->render('income_statement_ch_detailed/report_settings', title => t8('Detailed Swiss Income Statement'));
+
+  $self->render(
+    'income_statement_ch_detailed/report_settings',
+    title => t8('Detailed Swiss Income Statement'
+  ));
 }
 
 sub action_list {
@@ -41,16 +41,7 @@ sub action_list {
 
   # get date periods
   $self->date_periods(get_date_periods($::form, 'dateperiod'));
-
-  # simple date validation
-  for my $date_period (@{ $self->date_periods }) {
-    if (!$date_period->{from} || !$date_period->{to}) {
-      $::form->error(t8('Invalid date period specified.'));
-    }
-    if ($date_period->{from} gt $date_period->{to}) {
-      $::form->error(t8('Invalid date period specified: From date is after To date.'));
-    }
-  }
+  $self->validate_date_periods;
 
   $self->show_accounts($::form->{show_accounts} ? 1 : 0);
   $self->include_zero_amounts($::form->{include_zero_amounts} ? 1 : 0);
@@ -58,6 +49,9 @@ sub action_list {
   $self->department_id($::form->{department_id} || undef);
 
   ### prepare report
+  $self->controller_class('IncomeStatementChDetailed');
+  $self->attachment_basename_prefix(t8('income_statement_ch_detailed'));
+  $self->additional_hidden_variables(qw(show_accounts include_zero_amounts));
   $self->prepare_report;
 
   ### get report data and generate report
@@ -65,72 +59,6 @@ sub action_list {
   $self->report->add_data($report_data);
 
   $self->report->generate_with_headers;
-}
-
-# report preparation
-
-sub prepare_report {
-  my ($self) = @_;
-
-  ### setup report generator
-
-  $self->report(SL::ReportGenerator->new(\%::myconfig, $::form));
-
-  my @columns     = qw(account description);
-  # dynamically create columns for the balance depending on the date periods selected
-  my @balance_columns = map { "balance_$_->{index}" } @{ $self->date_periods };
-  @columns        = (@columns, @balance_columns);
-
-  # NOTE: the contents of the header are set in set_report_custom_headers,
-  # it seems that column definitions are still needed for things to work tho
-  my %column_defs = (
-    account   => {},
-    description => {},
-  );
-  $column_defs{"balance_$_->{index}"} = {} for (@{ $self->date_periods });
-
-  # NOTE: the pdf export does not look very good, so disabling it for now,
-  # printing from the web page does look good and can be used instead
-  $self->report->set_options(
-    std_column_visibility => 1,
-    controller_class      => 'IncomeStatementChDetailed',
-    output_format         => 'HTML',
-    title                 => $self->title,
-    allow_pdf_export      => 0,
-    allow_csv_export      => 1,
-    allow_chart_export    => 1,
-    attachment_basename   => t8('income_statement_ch_detailed') . strftime('_%Y%m%d', localtime time),
-    top_info_text         => $self->get_top_info_text,
-  );
-  $self->report->set_columns(%column_defs);
-  $self->report->set_column_order(@columns);
-
-  $self->set_report_custom_headers();
-
-  my @hidden_variables = get_hidden_variables_for_report($::form, 'dateperiod');
-
-  $self->report->set_export_options(qw(list), @hidden_variables, qw(show_accounts include_zero_amounts));
-  $self->report->set_options_from_form;
-}
-
-sub set_report_custom_headers {
-  my ($self) = @_;
-
-  ### this set the date periods in the header as well as the column descriptions
-
-  my @custom_headers = ();
-  push @custom_headers, [
-    { text => "", },
-    { text => "", },
-    map {{ text => ($_->{from}//'') . " - " . ($_->{to}//'') }} @{ $self->date_periods },
-  ];
-
-  push @custom_headers, [
-    { text => t8('Account'), },
-    { text => t8('Description'), },
-    map {{ text => t8('Saldo'), align => 'right' }} @{ $self->date_periods },
-  ];
-  $self->report->set_custom_headers(@custom_headers);
 }
 
 # report data calculation
@@ -190,7 +118,7 @@ sub get_report_data {
 
         ### prepare individual accounts display
         # only show accounts with non zero amounts, unless include_zero_amounts is set
-        if ($self->show_accounts && (has_nonzero_amount(@account_totals) || $self->include_zero_amounts)) {
+        if ($self->show_accounts && ($self->has_nonzero_amount(@account_totals) || $self->include_zero_amounts)) {
           my %data;
           $self->insert_description_columns(\%data, $account->accno, $account->description);
           $self->insert_date_period_columns_data(\%data, \@account_totals, 'numeric');
@@ -220,102 +148,6 @@ sub get_report_data {
     push @row_set, \%data;
   }
   return \@row_set;
-}
-
-# helper
-
-sub get_top_info_text {
-  my ($self) = @_;
-  my @text;
-  push @text, join " ", t8('Report date:'), $::locale->format_date_object(DateTime->now_local);
-  push @text, join " ", t8('Company:'), $::instance_conf->get_company;
-  if ($self->department_id) {
-    my ($department_ref) = grep { $_->[0] == $self->department_id } @{ $self->departments };
-    push @text, $::locale->text('Department') . ": $department_ref->[1]";
-  }
-  return join "\n", @text;
-}
-
-sub insert_description_columns {
-  my ($self, $data, $account, $description, $class) = @_;
-  $data->{account} = {
-    data  => $account,
-    class => $class // '',
-  };
-  $data->{description} = {
-    data  => $description,
-    class => $class // '',
-  };
-}
-
-sub insert_date_period_columns_data {
-  my ($self, $data, $columns_data, $class) = @_;
-
-  for my $date_period (@{ $self->date_periods }) {
-    $data->{"balance_$date_period->{index}"} = {
-      data  => $::form->format_amount(\%::myconfig, $columns_data->[ $date_period->{index} ], 2),
-      class => $class,
-    };
-  }
-}
-
-sub get_account_totals {
-  my ($self, $account) = @_;
-
-  my @account_totals = map { 0.0 } @{ $self->date_periods };
-
-  for my $date_period (@{ $self->date_periods }) {
-    # NOTE: this uses SL::DB::Chart::get_balance
-    my $account_sum = $account->get_balance(
-      fromdate => $date_period->{from_dateobj},
-      todate   => $date_period->{to_dateobj},
-      department_id => $self->department_id,
-    );
-
-    $account_totals[ $date_period->{index} ] += $account_sum;
-  }
-  return @account_totals;
-}
-
-sub has_nonzero_amount {
-  my @account_totals = @_;
-  return 1 if grep { $_ != 0.0 } @account_totals;
-  return 0;
-}
-
-# action bar
-
-sub setup_report_settings_action_bar {
-  my ($self, %params) = @_;
-
-  for my $bar ($::request->layout->get('actionbar')) {
-    $bar->add(
-      action => [
-        t8('Show'),
-        submit    => [ '#report_settings', { action => 'IncomeStatementChDetailed/list' } ],
-        accesskey => 'enter',
-      ],
-    );
-  }
-}
-
-# initializers
-
-sub init_departments {
-  my ($self) = @_;
-
-  my @departments = map {
-    [
-      $_->id,
-      $_->description,
-      0,
-    ]
-  } @{ SL::DB::Manager::Department->get_all_sorted };
-
-  # add empty option
-  unshift @departments, [ '', '-- ' . t8('All') . ' --', 1 ];
-
-  \@departments;
 }
 
 1;
