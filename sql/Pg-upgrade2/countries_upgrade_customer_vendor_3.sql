2bc6fd3f0b44e98ccc781234d5879affcfaed74e
Niklas Schmidt <niklas@kivitendo.de>
2026-01-22 17:28:19 +0100

    Countries: entferne NOT NULL Constraint, der viele Unit-Tests bricht

--- sql/Pg-upgrade2/countries_upgrade_customer_vendor_3.sql
+++
@@ -1,7 +0,0 @@
--- @tag: countries_upgrade_customer_vendor_3
--- @description: Table for countries
--- @depends: release_3_2_0 countries_upgrade_customer_vendor_2
-
-
-ALTER TABLE customer ALTER COLUMN country_id SET NOT NULL;
-ALTER TABLE vendor   ALTER COLUMN country_id SET NOT NULL;
