697380e5eeca8cc03c1748928352cc0935280bdb
Niklas Schmidt <niklas@kivitendo.de>
2026-01-29 10:52:03 +0100

    Shared Contacts: Suche nach Zugewiesenen korrigiert

--- SL/CT.pm
+++ SL/CT.pm
@@ -522,12 +522,10 @@ sub search_contacts {
     }
 
     push @where_tokens, '
-      (EXISTS(SELECT COUNT(*) FROM customer_contacts WHERE contact_id = cp_id) OR
-       EXISTS(SELECT COUNT(*) FROM vendor_contacts WHERE contact_id = cp_id))
+      (cc.id IS NOT NULL OR vc.id IS NOT NULL)
     ' if $filter->{status} eq 'active';
     push @where_tokens, '
-      (NOT EXISTS(SELECT COUNT(*) FROM customer_contacts WHERE contact_id = cp_id) AND
-       NOT EXISTS(SELECT COUNT(*) FROM vendor_contacts WHERE contact_id = cp_id))
+      (cc.id IS NULL AND vc.id IS NULL)
     ' if $filter->{status} eq 'orphaned';
   }
 
