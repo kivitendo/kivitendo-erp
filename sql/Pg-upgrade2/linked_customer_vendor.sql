24b5cbccd9bbb3ccd62eba09e5e2d90e403c0efe
Sven Schöling <s.schoeling@googlemail.com>
2026-02-16 16:25:13 +0100

    linked customer/vendor: pg-upgrade dependencies auf relesae_4_0_0

--- sql/Pg-upgrade2/linked_customer_vendor.sql
+++ sql/Pg-upgrade2/linked_customer_vendor.sql
@@ -1,6 +1,6 @@
 -- @tag: linked_customer_vendor
 -- @description: Verbundene Kunden/Lieferanten
--- @depends: release_3_9_2
+-- @depends: release_4_0_0
 
 CREATE TABLE customer_vendor_links (
   customer_id INTEGER NOT NULL REFERENCES customer(id) ON DELETE CASCADE UNIQUE,
