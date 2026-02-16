f51e29645c29d311e85aa1c9840de148ef7a9065
Sven Schöling <s.schoeling@googlemail.com>
2026-02-16 16:25:13 +0100

    linked customer/vendor: pg-upgrade dependencies auf relesae_4_0_0

--- sql/Pg-upgrade2/linked_customer_vendor_custom_variables_config.sql
+++ sql/Pg-upgrade2/linked_customer_vendor_custom_variables_config.sql
@@ -1,5 +1,5 @@
 -- @tag: linked_customer_vendor_custom_variables_config
 -- @description: BDVs zwischen verbundenem Kunden/Lieferanten synchronisieren
--- @depends: release_3_9_2
+-- @depends: release_4_0_0 linked_customer_vendor
 
 ALTER TABLE public.custom_variable_configs ADD COLUMN sync_linked_cv BOOLEAN default false;
