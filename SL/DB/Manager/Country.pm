319b2b689511fb9594e396da3e87545fa73a88fc
Niklas Schmidt <niklas@kivitendo.de>
2026-02-17 10:57:17 +0100

    Countries: S:B:M:Country: Helfer find_by_name erledigt Regexp-Lookup

--- SL/DB/Manager/Country.pm
+++ SL/DB/Manager/Country.pm
@@ -5,11 +5,24 @@ package SL::DB::Manager::Country;
 
 use strict;
 
+use SL::Helper::ISO3166 qw(map_name_to_alpha_2_code);
 use SL::DB::Helper::Manager;
 use base qw(SL::DB::Helper::Manager);
 
 sub object_class { 'SL::DB::Country' }
 
 __PACKAGE__->make_manager_methods;
+
+sub find_by_name {
+  my ($country_name) = @_;
+
+  my $country_code = map_name_to_alpha_2_code($country_name);
+
+  return unless $country_code;
+
+  my $countries_by_iso2 = $::request->cache('::SL::DB::Manager::Country', { map { $_->iso2 => $_ } @{ SL::DB::Manager::Country->get_all } });
+
+  return $countries_by_iso2->{$country_code};
+}
 
 1;
