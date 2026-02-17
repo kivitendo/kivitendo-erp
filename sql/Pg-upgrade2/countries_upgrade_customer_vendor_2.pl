c45c80e72f47b9c76b2957cba74bafc77d874191
Bernd Bleßmann <bernd@kivitendo-premium.de>
2026-02-17 16:17:13 +0100

    Countries: DB-Update: with_transaction entfernt, da …
    … die Upgrades schon in einer Transaktion laufen.

--- sql/Pg-upgrade2/countries_upgrade_customer_vendor_2.pl
+++ sql/Pg-upgrade2/countries_upgrade_customer_vendor_2.pl
@@ -67,83 +67,81 @@ sub run {
   my %missing = ();
   $missing{$_->{name}} = $_->{id} for @{$::form->{missing} // []};
 
-  SL::DB->client->with_transaction(sub {
-    my ($query, $sth);
+  my ($query, $sth);
 
-    $query = 'SELECT id, iso2 FROM countries;';
-    $sth = $self->dbh->prepare($query);
-    $sth->execute || $self->db_error($query);
-    my $countries = $sth->fetchall_arrayref();
-    my %country_id_by_iso2 = map { $_->[1] => $_->[0] } @$countries;
+  $query = 'SELECT id, iso2 FROM countries;';
+  $sth = $self->dbh->prepare($query);
+  $sth->execute || $self->db_error($query);
+  my $countries = $sth->fetchall_arrayref();
+  my %country_id_by_iso2 = map { $_->[1] => $_->[0] } @$countries;
 
-    $query = "SELECT distinct(country) FROM customer
-              UNION ALL
-              SELECT distinct(country) FROM vendor
-              UNION ALL
-              SELECT distinct(shiptocountry) AS country FROM shipto WHERE shiptocountry != ''
-              UNION ALL
-              SELECT distinct(country) FROM additional_billing_addresses WHERE country != '';";
-    $sth = $self->dbh->prepare($query);
-    $sth->execute || $self->db_error($query);
-    my %country_id_by_country_name = ();
-    while (my $cv = $sth->fetchrow_hashref()) {
-      my $country_id = $cv->{country} ? $country_id_by_iso2{ SL::Helper::ISO3166::map_name_to_alpha_2_code($cv->{country}) } : undef;
-      unless ($country_id) {
-        if (defined $missing{$cv->{country}}) {
-          $country_id = $missing{$cv->{country}};
-        } else {
-          $missing{$cv->{country}} = '';
-          next;
-        }
+  $query = "SELECT distinct(country) FROM customer
+            UNION ALL
+            SELECT distinct(country) FROM vendor
+            UNION ALL
+            SELECT distinct(shiptocountry) AS country FROM shipto WHERE shiptocountry != ''
+            UNION ALL
+            SELECT distinct(country) FROM additional_billing_addresses WHERE country != '';";
+  $sth = $self->dbh->prepare($query);
+  $sth->execute || $self->db_error($query);
+  my %country_id_by_country_name = ();
+  while (my $cv = $sth->fetchrow_hashref()) {
+    my $country_id = $cv->{country} ? $country_id_by_iso2{ SL::Helper::ISO3166::map_name_to_alpha_2_code($cv->{country}) } : undef;
+    unless ($country_id) {
+      if (defined $missing{$cv->{country}}) {
+        $country_id = $missing{$cv->{country}};
+      } else {
+        $missing{$cv->{country}} = '';
+        next;
       }
-
-      $country_id_by_country_name{$cv->{country}} = $country_id;
     }
 
-    if (scalar (grep { !$missing{$_} } keys %missing)) {
-      $self->print_errors(\%missing);
-      return 2;
-    }
-    return 0 if scalar @errors;
+    $country_id_by_country_name{$cv->{country}} = $country_id;
+  }
 
-    $query = "UPDATE customer SET country_id = ? WHERE COALESCE(country, '') = ?;";
-    $sth = $self->dbh->prepare($query);
-    foreach my $name (keys %country_id_by_country_name) {
-      do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
-    }
+  if (scalar (grep { !$missing{$_} } keys %missing)) {
+    $self->print_errors(\%missing);
+    return 2;
+  }
+  return 0 if scalar @errors;
 
-    $query = "UPDATE vendor SET country_id = ? WHERE COALESCE(country, '') = ?;";
-    $sth = $self->dbh->prepare($query);
-    foreach my $name (keys %country_id_by_country_name) {
-      do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
-    }
+  $query = "UPDATE customer SET country_id = ? WHERE COALESCE(country, '') = ?;";
+  $sth = $self->dbh->prepare($query);
+  foreach my $name (keys %country_id_by_country_name) {
+    do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
+  }
 
-    $query = 'UPDATE shipto SET shiptocountry_id = ? WHERE shiptocountry = ?;';
-    $sth = $self->dbh->prepare($query);
-    foreach my $name (keys %country_id_by_country_name) {
-      do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
-    }
+  $query = "UPDATE vendor SET country_id = ? WHERE COALESCE(country, '') = ?;";
+  $sth = $self->dbh->prepare($query);
+  foreach my $name (keys %country_id_by_country_name) {
+    do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
+  }
 
-    $query = 'UPDATE additional_billing_addresses SET country_id = ? WHERE country = ?;';
-    $sth = $self->dbh->prepare($query);
-    foreach my $name (keys %country_id_by_country_name) {
-      do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
-    }
+  $query = 'UPDATE shipto SET shiptocountry_id = ? WHERE shiptocountry = ?;';
+  $sth = $self->dbh->prepare($query);
+  foreach my $name (keys %country_id_by_country_name) {
+    do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
+  }
 
-    $query = 'ALTER TABLE customer ALTER COLUMN country_id SET NOT NULL;
-              ALTER TABLE vendor   ALTER COLUMN country_id SET NOT NULL;';
-    $sth = $self->dbh->prepare($query);
-    $sth->execute || $self->db_error($query);
+  $query = 'UPDATE additional_billing_addresses SET country_id = ? WHERE country = ?;';
+  $sth = $self->dbh->prepare($query);
+  foreach my $name (keys %country_id_by_country_name) {
+    do_statement($::form, $sth, $query, $country_id_by_country_name{$name}, $name);
+  }
 
-    $query = 'ALTER TABLE customer DROP COLUMN country;
-              ALTER TABLE vendor   DROP COLUMN country;
-              ALTER TABLE shipto   DROP COLUMN shiptocountry;
-              ALTER TABLE additional_billing_addresses DROP COLUMN country;';
-    $sth = $self->dbh->prepare($query);
-    $sth->execute || $self->db_error($query);
+  $query = 'ALTER TABLE customer ALTER COLUMN country_id SET NOT NULL;
+            ALTER TABLE vendor   ALTER COLUMN country_id SET NOT NULL;';
+  $sth = $self->dbh->prepare($query);
+  $sth->execute || $self->db_error($query);
 
-    1;
-  }) ;#or do { die SL::DB->client->error };
+  $query = 'ALTER TABLE customer DROP COLUMN country;
+            ALTER TABLE vendor   DROP COLUMN country;
+            ALTER TABLE shipto   DROP COLUMN shiptocountry;
+            ALTER TABLE additional_billing_addresses DROP COLUMN country;';
+  $sth = $self->dbh->prepare($query);
+  $sth->execute || $self->db_error($query);
+
+  return 1;
 }
 
 1;
