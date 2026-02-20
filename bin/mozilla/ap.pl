0d005ec20f1c46143bf6f442a3ecb157c089e8f3
Bernd Bleßmann <bernd@kivitendo-premium.de>
2026-02-20 09:28:00 +0100

    Kosmetik: Einrückung

--- bin/mozilla/ap.pl
+++ bin/mozilla/ap.pl
@@ -1237,7 +1237,7 @@ sub ap_transactions {
     'items'                   => { 'text' => $locale->text('Positions'), },
   );
 
-foreach my $name (qw(id transdate duedate invnumber ordnumber name netamount amount datepaid employee shipvia transaction_description direct_debit department taxzone insertdate intnotes globalprojectdescription globalprojectnumber)) {
+  foreach my $name (qw(id transdate duedate invnumber ordnumber name netamount amount datepaid employee shipvia transaction_description direct_debit department taxzone insertdate intnotes globalprojectdescription globalprojectnumber)) {
     my $sortdir                 = $form->{sort} eq $name ? 1 - $form->{sortdir} : $form->{sortdir};
     $column_defs{$name}->{link} = $href . "&sort=$name&sortdir=$sortdir";
   }
