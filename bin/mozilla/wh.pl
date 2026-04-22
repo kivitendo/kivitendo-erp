671321dd622904d374e8c76d32e4a2fd7c6d63ee
Niklas Schmidt <niklas@kivitendo.de>
2026-04-22 17:11:13 +0200

    CreateAssembly: Meldung Chargenauswahl mit Einheit und präziser

--- bin/mozilla/wh.pl
+++ bin/mozilla/wh.pl
@@ -547,7 +547,9 @@ sub create_assembly_chargenumbers {
   my $hidden_vars = { map { $_ => $form->{$_} } qw(parts_id warehouse_id bin_id bestbefore chargenumber unit comment) };
   $hidden_vars->{$_} = $form->format_amount(\%main::myconfig, $form->{$_}) for qw(qty);
 
-  $form->{title} = $::locale->text('Select Chargenumbers to produce quantity #1 of #2 #3', $form->{qty}, $assembly->partnumber, $assembly->description);
+  $form->{title} = $::locale->text('Select charge numbers to produce quantity #1 of #2',
+                     $form->format_amount(\%main::myconfig, $form->{qty}) . ' ' . $form->{unit},
+                     $assembly->displayable_name);
   $form->header;
 
   print $form->parse_html_template(
