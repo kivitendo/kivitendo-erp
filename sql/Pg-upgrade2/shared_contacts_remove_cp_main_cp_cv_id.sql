66797d954bcd5d294160c911c6a9ea6dea679cd4
Niklas Schmidt <niklas@kivitendo.de>
2026-06-04 19:30:18 +0200

    Shared Contacts: DB cp_main und cp_cv_id entfernt

---
+++ sql/Pg-upgrade2/shared_contacts_remove_cp_main_cp_cv_id.sql
@@ -0,0 +1,6 @@
+-- @tag: shared_contacts_remove_cp_main_cp_cv_id
+-- @description: Mehrfachzuordnung on Ansprechpartnern zu Kunden und Lieferanten: lösche alte Spalten cp_main und cp_cv_id
+-- @depends: shared_contacts
+
+alter table contacts drop column cp_main;
+alter table contacts drop column cp_cv_id;
