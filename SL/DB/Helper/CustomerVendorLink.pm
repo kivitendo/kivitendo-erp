237eeddf6e704dfb522570aa47526b7d8ded3329
Niklas Schmidt <niklas@kivitendo.de>
2026-06-29 13:15:13 +0200

    linked customer/vendor: synchronisiere BDVs: fix für Booleans

--- SL/DB/Helper/CustomerVendorLink.pm
+++ SL/DB/Helper/CustomerVendorLink.pm
@@ -83,10 +83,9 @@ sub make_sync_customer_vendor {
 
     $other->assign_attributes($_ => $self->$_) for qw(city country currency_id email fax homepage language_id name obsolete phone street taxnumber ustid zipcode);
 
-    my $own_cvars = $self->cvar_as_hashref;
     foreach my $other_cvar (@{$other->cvars_by_config}) {
       next unless $other_cvar->config->sync_linked_cv;
-      $other_cvar->value($own_cvars->{$other_cvar->config->name}{value});
+      $other_cvar->value($self->cvar_by_name($other_cvar->config->name)->value);
     }
   };
 }
