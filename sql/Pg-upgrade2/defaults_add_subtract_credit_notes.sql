d0c9e16b8d2af63e7c52794da6953de2dde5926e
Jan Büren <jan@kivitendo.de>
2026-07-09 19:13:56 +0200

    db-skripte auf 4.0.0 gesetzt

--- sql/Pg-upgrade2/defaults_add_subtract_credit_notes.sql
+++ sql/Pg-upgrade2/defaults_add_subtract_credit_notes.sql
@@ -1,6 +1,6 @@
 -- @tag: defaults_add_subtract_credit-notes
 -- @description: Mandantekonfig: SEPA Zahlungen zusammenfassen, Gutschriften abziehen
--- @depends: release_3_9_1
+-- @depends: release_4_0_0
 
 ALTER TABLE defaults ADD COLUMN sepa_combine_payments boolean DEFAULT TRUE;
 ALTER TABLE defaults ADD COLUMN sepa_subtract_credit_notes boolean DEFAULT TRUE;
--- sql/Pg-upgrade2/sepa_exports_add_gl.sql
+++ sql/Pg-upgrade2/sepa_exports_add_gl.sql
@@ -1,5 +1,5 @@
 -- @tag: sepa_exports_add_gl
 -- @description: Verrechnete Gutschriften können auch Skonto Steuerkorrektur
--- @depends: release_3_9_1 sepa_exports_add_acc_trans
+-- @depends: release_4_0_0 sepa_exports_add_acc_trans
 ALTER TABLE sepa_exports_acc_trans ADD gl_id INTEGER REFERENCES gl(id);
 
