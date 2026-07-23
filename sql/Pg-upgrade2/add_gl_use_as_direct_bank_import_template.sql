f74012701165e1d53be591173ef460ab3b1ac543
Jan Büren <jan@kivitendo.de>
2026-07-23 13:17:23 +0200

    sql-upgrade-skript von 3_9_1 nach 4_0_0

--- sql/Pg-upgrade2/add_gl_use_as_direct_bank_import_template.sql
+++ sql/Pg-upgrade2/add_gl_use_as_direct_bank_import_template.sql
@@ -1,6 +1,6 @@
 -- @tag: add_gl_use_as_direct_bank_import_template
 -- @description: Dialogbuchungsvorlagen kennzeichnen die direkt als Quelle vom Bankimport bebucht werden dürfen
--- @depends: release_3_9_1
+-- @depends: release_4_0_0
 
 ALTER TABLE gl ADD bank_import_template boolean DEFAULT false;
 ALTER TABLE record_templates ADD bank_import_template boolean DEFAULT false;
