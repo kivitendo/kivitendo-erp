ebc8e44f4e9748e41b76013dd150d765088c1e62
Niklas Schmidt <niklas@kivitendo.de>
2026-07-23 14:36:42 +0200

    invoicing period: DB-Upgrade: durch Lieferscheinen schleifen

--- sql/Pg-upgrade2/invoicing_period.sql
+++ sql/Pg-upgrade2/invoicing_period.sql
@@ -2,11 +2,13 @@
 -- @description: Abrechnungszeitraum als Intervall statt Zeitpunkt Leistungsdatum
 -- @depends: release_4_0_0
 
---ALTER TABLE ap         ADD COLUMN tax_point_start        date;
-ALTER TABLE ar         ADD COLUMN tax_point_start        date;
-ALTER TABLE invoice    ADD COLUMN invoicing_period_start date,
-                       ADD COLUMN invoicing_period_end   date;
+--ALTER TABLE ap              ADD COLUMN tax_point_start        date;
+ALTER TABLE ar              ADD COLUMN tax_point_start        date;
+ALTER TABLE invoice         ADD COLUMN invoicing_period_start date,
+                            ADD COLUMN invoicing_period_end   date;
 
-ALTER TABLE oe         ADD COLUMN tax_point_start        date;
-ALTER TABLE orderitems ADD COLUMN invoicing_period_start date,
-                       ADD COLUMN invoicing_period_end   date;
+ALTER TABLE oe              ADD COLUMN tax_point_start        date;
+ALTER TABLE orderitems      ADD COLUMN invoicing_period_start date,
+                            ADD COLUMN invoicing_period_end   date;
+
+ALTER TABLE delivery_orders ADD COLUMN tax_point_start        date;
