d645941917c8e22ce79cd0acfbfa7a3c4e641ba9
Niklas Schmidt <niklas@kivitendo.de>
2026-07-23 15:23:32 +0200

    invoicing period: DB-Upgrade: PurchaseInvoice

--- sql/Pg-upgrade2/invoicing_period.sql
+++ sql/Pg-upgrade2/invoicing_period.sql
@@ -2,8 +2,8 @@
 -- @description: Abrechnungszeitraum als Intervall statt Zeitpunkt Leistungsdatum
 -- @depends: release_4_0_0
 
---ALTER TABLE ap              ADD COLUMN tax_point_start        date;
 ALTER TABLE ar              ADD COLUMN tax_point_start        date;
+ALTER TABLE ap              ADD COLUMN tax_point_start        date;
 ALTER TABLE invoice         ADD COLUMN invoicing_period_start date,
                             ADD COLUMN invoicing_period_end   date;
 
