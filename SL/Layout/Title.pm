d4ae1dcd0fc7050d465f8b1f0353990dd073a4b9
Niklas Schmidt <niklas@kivitendo.de>
2026-08-28 16:50:05 +0200

    Layout: Titel in ActionBar integriert mittels flex Layout
    Vermeidet das negativen margin, um die ActionBar und die H1-Überschrift
    visuell auf derselben Höhe zu platzieren.
    
    Vorteil: die H1-Überschrift wird nicht überdeckt und lässt sich im Browser
    als Text kopieren.

--- SL/Layout/Title.pm
+++ SL/Layout/Title.pm
@@ -8,7 +8,7 @@ use SL::Presenter::Tag qw(html_tag);
 sub pre_content {
   my $title = escape($::locale->text($::form->{title} || ""));
 
-  html_tag('h1', $title, id => 'title-headline', 'data-base-title' => $title)
+  '';#html_tag('h1', $title, id => 'title-headline', 'data-base-title' => $title)
 }
 
 1;
