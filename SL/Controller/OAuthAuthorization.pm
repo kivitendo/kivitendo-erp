19d37e035f3f3e01666200823b570725c9d4f96c
Niklas Schmidt <niklas@kivitendo.de>
2026-09-08 14:09:29 +0200

    Ticket System Jira: OAuth2 Token {Abruf,Refresh} Fehlerabhandlung
    Fehler-Queryparameter auswerten
    
    erforderliche Queryparameter prüfen
    
    tolerant für text/plain Responses mit Fehler-Text statt JSON:
    Bernds Test mit Google Calender ergab, dass Google Fehler als
    text/plain statt JSON schickt.

--- SL/Controller/OAuthAuthorization.pm
+++ SL/Controller/OAuthAuthorization.pm
@@ -12,6 +12,7 @@ use SL::Controller::OAuth::Atlassian;
 use SL::Controller::OAuth::GoogleCal;
 use SL::Helper::Flash qw(flash_later);
 use SL::OAuth;
+use Try::Tiny;
 
 
 #
@@ -21,8 +22,20 @@ use SL::OAuth;
 sub action_authcode {
   my ($self) = @_;
 
-  my $search_state = $::form->{state} or die 'Request has no state parameter';
-  my $auth_code    = $::form->{code}  or die 'Request has no code parameter';
+  my @errors;
+  my $auth_code    = $::form->{code};
+  my $search_state = $::form->{state};
+
+  push @errors, t8('Provider returned error: #1=#2', 'error', $::form->{oaerror})          if     $::form->{oaerror};
+  push @errors, t8('Provider returned error: #1=#2', 'error_description', $::form->{error_description}) if $::form->{error_description};
+  push @errors, t8('Provider did not send required parameter: #1', 'code')                 unless $auth_code;
+  push @errors, t8('Provider did not send required parameter: #1', 'state')                unless $search_state;
+
+  if (@errors) {
+    flash_later('error', $_) foreach (@errors);
+    $self->redirect_to(controller => 'OAuth', action => 'list');
+    return;
+  }
 
   my $tok = SL::DB::Manager::OAuthToken->find_by(tokenstate => $search_state) or die 'unknown state';
   my $provider = SL::OAuth::providers()->{$tok->registration} or die 'unknown provider';
@@ -30,17 +43,23 @@ sub action_authcode {
   my $ret = $provider->access_token($tok, $auth_code);
 
   my $response_code = $ret->responseCode();
+  my $content = try {
+    return from_json($ret->responseContent);
+  } catch {
+    push @errors, t8('invalid JSON format received: #1', $ret->responseContent());
+    return {};
+  };
+  push @errors, t8('Provider returned error: #1=#2', 'HTTP Status', $response_code)        unless ($response_code >= 200 && $response_code <= 299);
+  push @errors, t8('Provider returned error: #1=#2', 'error',      $content->{error})      if     ($content->{error});
+  push @errors, t8('Provider returned error: #1=#2', 'error_code', $content->{error_code}) if     ($content->{error_code});
+  push @errors, t8('Provider returned error: #1=#2', 'error_description', $content->{error_description}) if ($content->{error_description});
+  push @errors, t8('Provider did not send required parameter: #1', 'access_token')         unless ($content->{access_token});
+  push @errors, t8('Provider did not send required parameter: #1', 'expires_in')           unless ($content->{expires_in} =~ m/^\d+$/);
 
-  unless ($response_code >= 200 && $response_code <= 299) {
-    flash_later('error', t8('Provider returned HTTP #1', $response_code));
-    return $self->redirect_to(controller => 'OAuth', action => 'list');
-  }
-
-  my $content = from_json($ret->responseContent);
-
-  if (exists $content->{error_code}) {
-    flash_later('error', t8('Provider returned error code #1', $content->{error_code}));
-    return $self->redirect_to(controller => 'OAuth', action => 'list');
+  if (@errors) {
+    flash_later('error', $_) foreach (@errors);
+    $self->redirect_to(controller => 'OAuth', action => 'list');
+    return;
   }
 
   $tok->set_access_refresh_token($content);
