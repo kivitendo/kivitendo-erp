2aae3374506311631de133a109d3adb2637eab9f
Niklas Schmidt <niklas@kivitendo.de>
2026-09-14 17:08:27 +0200

    Auftrags-Controller: Aufträge auch mit neuem Controller zusammenfassen können: Fallback entfernt
    Wurde nicht benötigt und kollidiert mit neuem Feature "mehrere Preisanfragen
    zu 1 Lieferantenauftrag zusammenfassen".

--- SL/Controller/Order.pm
+++ SL/Controller/Order.pm
@@ -207,13 +207,6 @@ sub action_edit_collective {
     return;
   }
 
-  # fall back to save as new if only one id is given
-  if (scalar @multi_ids == 1) {
-    $self->order(SL::DB::Order->new(id => $multi_ids[0])->load);
-    $self->action_save_as_new();
-    return;
-  }
-
   # make new order from given orders
   my @multi_orders = map { SL::DB::Order->new(id => $_)->load } @multi_ids;
   my $order = SL::Model::Record->new_from_workflow_multi(\@multi_orders, $target_order_type, sort_sources_by => 'transdate');
