bc883cf720e6f89b23e656681ea58dab84ee8e5a
Niklas Schmidt <niklas@kivitendo.de>
2026-09-14 17:01:52 +0200

    mehrere Preisanfragen zu 1 Lieferantenauftrag zusammenfassen

--- SL/DB/Order.pm
+++ SL/DB/Order.pm
@@ -685,8 +685,14 @@ sub new_from {
 sub new_from_multi {
   my ($class, $sources, %params) = @_;
 
+  my $is_purchase = any { !$_->is_sales } @$sources;
+
   croak("Unsupported object type in sources")                             if any { ref($_) !~ m{SL::DB::Order} }                   @$sources;
-  croak("Cannot create order for purchase records")                       if any { !$_->is_sales }                                 @$sources;
+  if ($is_purchase) {
+    croak("Cannot create order for purchase records")                     if any {  $_->is_sales }                                 @$sources;
+  } else {
+    croak("Cannot create order for purchase records")                     if any { !$_->is_sales }                                 @$sources;
+  }
   croak("Cannot create order from source records of different customers") if any { $_->customer_id != $sources->[0]->customer_id } @$sources;
 
   # bb: todo: check shipto: is it enough to check the ids or do we have to compare the entries?
@@ -746,7 +752,7 @@ sub new_from_multi {
   push @items, @{$_->items_sorted} for @$sources;
   # make order from first source and all items
   my $order = $class->new_from($sources->[0],
-                               destination_type => SALES_ORDER_TYPE(),
+                               destination_type => $is_purchase ? PURCHASE_ORDER_TYPE() : SALES_ORDER_TYPE(),
                                attributes       => \%attributes,
                                items            => \@items,
                                %params);
@@ -906,12 +912,20 @@ sub preceding_request_quotations {
     @lrs = grep { 'SL::DB::Order' eq ref($_) && $_->record_type eq REQUEST_QUOTATION_TYPE() } @{$self->linked_records(from => 'SL::DB::Order', recursive => 1)};
   } else {
     if ('SL::DB::Order' eq $self->{RECORD_TYPE_REF()}) {
-      my $order = SL::DB::Order->load_cached($self->{RECORD_ID()});
-      if ($order->record_type eq REQUEST_QUOTATION_TYPE()) {
-        push @lrs, $order;
+      my @from_record_ids;
+      unless ($self->{RECORD_ID()} =~ m/^[0-9]*$/) {
+        @from_record_ids = split / /, $self->{RECORD_ID()};
+      } else {
+        @from_record_ids = ($self->{RECORD_ID()});
+      }
+      foreach my $id (@from_record_ids) {
+        my $order = SL::DB::Order->load_cached($id);
+        if ($order->record_type eq REQUEST_QUOTATION_TYPE()) {
+          push @lrs, $order;
 
-      } elsif (any { $order->record_type eq $_ } (PURCHASE_ORDER_TYPE(), PURCHASE_QUOTATION_INTAKE_TYPE())) {
-        @lrs = @{ $order->preceding_request_quotations() || [] };
+        } elsif (any { $order->record_type eq $_ } (PURCHASE_ORDER_TYPE(), PURCHASE_QUOTATION_INTAKE_TYPE())) {
+          @lrs = @{ $order->preceding_request_quotations() || [] };
+        }
       }
     }
   }
