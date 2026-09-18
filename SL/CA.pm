ba1849ce2b09fe56e8413a895018b1fcf0364cf4
Jan Büren <jan@kivitendo.de>
2026-09-18 18:49:39 +0200

    Buchungsliste: Kunden- oder Lieferantennummer als Spalte anzeigen

--- SL/CA.pm
+++ SL/CA.pm
@@ -265,7 +265,7 @@ sub all_transactions {
 
     # get all transactions
     $query =
-      qq|SELECT ac.itime, a.id, a.reference, a.description, ac.transdate, ac.chart_id, | .
+      qq|SELECT ac.itime, a.id, a.reference, a.description, '' as number, ac.transdate, ac.chart_id, | .
       qq|  FALSE AS invoice, ac.amount, 'gl' as module, | .
       qq§(SELECT accno||'--'||rate FROM tax LEFT JOIN chart ON (tax.chart_id=chart.id) WHERE tax.id = (SELECT tax_id FROM taxkeys WHERE taxkey_id = ac.taxkey AND taxkeys.startdate <= ac.transdate ORDER BY taxkeys.startdate DESC LIMIT 1)) AS taxinfo, ac.source || ' ' || ac.memo AS memo § .
       qq|FROM acc_trans ac, gl a | .
@@ -277,7 +277,7 @@ sub all_transactions {
 
       qq|UNION ALL | .
 
-      qq|SELECT ac.itime, a.id, a.invnumber, c.name, ac.transdate, ac.chart_id, | .
+      qq|SELECT ac.itime, a.id, a.invnumber, c.name, c.customernumber as number, ac.transdate, ac.chart_id, | .
       qq|  a.invoice, ac.amount, 'ar' as module, | .
       qq§(SELECT accno||'--'||rate FROM tax LEFT JOIN chart ON (tax.chart_id=chart.id) WHERE tax.id = (SELECT tax_id FROM taxkeys WHERE taxkey_id = ac.taxkey AND taxkeys.startdate <= ac.transdate ORDER BY taxkeys.startdate DESC LIMIT 1)) AS taxinfo, ac.source || ' ' || ac.memo AS memo  § .
       qq|FROM acc_trans ac, customer c, ar a | .
@@ -290,7 +290,7 @@ sub all_transactions {
 
       qq|UNION ALL | .
 
-      qq|SELECT ac.itime, a.id, a.invnumber, v.name, ac.transdate, ac.chart_id, | .
+      qq|SELECT ac.itime, a.id, a.invnumber, v.name, v.vendornumber as number, ac.transdate, ac.chart_id, | .
       qq|  a.invoice, ac.amount, 'ap' as module, | .
       qq§(SELECT accno||'--'||rate FROM tax LEFT JOIN chart ON (tax.chart_id=chart.id) WHERE tax.id = (SELECT tax_id FROM taxkeys WHERE taxkey_id = ac.taxkey AND taxkeys.startdate <= ac.transdate ORDER BY taxkeys.startdate DESC LIMIT 1)) AS taxinfo, ac.source || ' ' || ac.memo AS memo  § .
       qq|FROM acc_trans ac, vendor v, ap a | .
@@ -326,7 +326,7 @@ sub all_transactions {
       $query .=
         qq|UNION ALL | .
 
-        qq|SELECT ac.itime, a.id, a.invnumber, c.name, a.transdate, | .
+        qq|SELECT ac.itime, a.id, a.invnumber, c.name, c.customernumber as number, a.transdate, | .
         qq|  a.invoice, ac.qty * ac.sellprice AS sellprice, 'ar' as module, | .
         qq§(SELECT accno||'--'||rate FROM tax LEFT JOIN chart ON (tax.chart_id=chart.id) WHERE tax.id = (SELECT tax_id FROM taxkeys WHERE taxkey_id = ac.taxkey AND taxkeys.startdate <= ac.transdate ORDER BY taxkeys.startdate DESC LIMIT 1)) AS taxinfo § .
         qq|FROM ar a | .
@@ -341,7 +341,7 @@ sub all_transactions {
         $project .
         qq|UNION ALL | .
 
-        qq|SELECT ac.itime, a.id, a.invnumber, v.name, a.transdate, | .
+        qq|SELECT ac.itime, a.id, a.invnumber, v.name, v.vendornumber as number, a.transdate, | .
         qq|  a.invoice, ac.qty * ac.sellprice AS sellprice, 'ap' as module, | .
         qq§(SELECT accno||'--'||rate FROM tax LEFT JOIN chart ON (tax.chart_id=chart.id) WHERE tax.id = (SELECT tax_id FROM taxkeys WHERE taxkey_id = ac.taxkey AND taxkeys.startdate <= ac.transdate ORDER BY taxkeys.startdate DESC LIMIT 1)) AS taxinfo § .
         qq|FROM ap a | .
