ef455b1dd860532f7a131e510d3da832e909420d
Niklas Schmidt <niklas@kivitendo.de>
2026-09-23 16:11:55 +0200

    LeadTime: integriert in Order-Controller inkl. Druck

--- SL/DB/Helper/FlattenToForm.pm
+++ SL/DB/Helper/FlattenToForm.pm
@@ -16,7 +16,7 @@ sub flatten_to_form {
                                     employee_id salesman_id closed department_id language_id payment_id delivery_customer_id delivery_vendor_id shipto_id proforma
                                     globalproject_id delivered transaction_description container_type accepted_by_customer invoice storno storno_id dunning_config_id
                                     orddate quodate reqdate gldate duedate deliverydate datepaid transdate tax_point_start tax_point delivery_term_id billing_address_id
-                                    vendor_confirmation_number));
+                                    vendor_confirmation_number lead_time_id));
   $form->{currency} = $form->{curr} = $self->currency_id ? $self->currency->name || '' : '';
 
   if ( $vc eq 'customer' ) {
